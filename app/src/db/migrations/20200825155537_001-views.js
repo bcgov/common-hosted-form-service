e5c5a7fe60837fb24efd3efc62684fa10f6854af
Jason Sherman <jsherman@parcsystems.ca>
2020-09-04 10:20:38 -0700

    add submissions metadata view to fetch all submissions for a form.

--- app/src/db/migrations/20200825155537_001-views.js
+++ app/src/db/migrations/20200825155537_001-views.js
@@ -50,11 +50,18 @@ order by lower(u."lastName"::text), lower(u."firstName"::text), lower(f.name::te
 select null as "userId", null as "keycloakId", 'public' as username, 'public' as "fullName", null as "firstName", null as "lastName", null as email, f.id as "formId", f.name as "formName", f.labels, f."identityProviders", f.idps, f.active, f."formVersionId", f.version, '{}'::varchar[] as roles, '{submission_create,form_read}'::varchar[] as permissions
 from form_vw f
 where 'public' = ANY(f."idps")
-order by lower(f.name::text)`));
+order by lower(f.name::text)`))
+    .then(() => knex.schema.raw(`create view submissions_vw as 
+select s.id as "submissionId", s."confirmationId", s.draft, s.deleted, s."createdBy", s."createdAt", f.id as "formId", f.name as "formName", fv.id as"formVersionId", fv.version
+from form_submission as s
+    inner join form_version fv on s."formVersionId" = fv.id
+    inner join form f on fv."formId" = f.id
+order by s."createdAt" desc`));
 };
 
 exports.down = function(knex) {
   return Promise.resolve()
+    .then(() => knex.schema.raw('DROP VIEW IF EXISTS submissions_vw'))
     .then(() => knex.schema.raw('DROP VIEW IF EXISTS public_form_access_vw'))
     .then(() => knex.schema.raw('DROP VIEW IF EXISTS user_form_access_vw'))
     .then(() => knex.schema.raw('DROP VIEW IF EXISTS user_form_roles_vw'))
