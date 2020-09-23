48b8dc5a22bbe3dc7002ed1fa9f8e53518008ad6
Jeremy Ho <jujaga@gmail.com>
2020-09-23 16:19:46 -0700

    Complete TeamManagement component implementation
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/components/forms/TeamManagement.vue
+++ app/frontend/src/components/forms/TeamManagement.vue
@@ -1,39 +1,60 @@
 <template>
   <div>
-    <v-card outlined>
-      <v-skeleton-loader :boilerplate="false" class="mx-auto" :loading="loading" type="table">
-        <v-simple-table>
-          <template v-slot:default>
-            <thead>
-              <tr>
-                <th>
-                  <div>User</div>
-                </th>
-                <th v-for="role in roleList" :key="role.code">
-                  <div v-if="role.active">{{ role.display }}</div>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr v-for="(user, index) in tableData" :key="user.userId">
-                <td>{{ user.fullName }} ({{ user.username }})</td>
-                <td v-for="role in Object.keys(user.roles)" :key="role">
-                  <v-checkbox v-model="tableData[index].roles[role]" />
-                </td>
-              </tr>
-            </tbody>
-          </template>
-        </v-simple-table>
-      </v-skeleton-loader>
-    </v-card>
-    <v-btn color="primary" class="my-2" @click="submitFormSchema">
-      <span>Update Permissions</span>
+    <!-- search input -->
+    <div class="team-search mt-6 mt-sm-0">
+      <v-text-field
+        v-model="search"
+        append-icon="mdi-magnify"
+        label="Search"
+        single-line
+        hide-details
+        class="pb-5"
+      />
+    </div>
+
+    <!-- team table -->
+    <v-data-table
+      class="team-table"
+      :headers="headers"
+      :items="tableData"
+      :loading="loading || updating"
+      loading-text="Loading... Please wait"
+      no-data-text="Failed to load team role data"
+      :search="search"
+    >
+      <template v-slot:item="{ item, index }">
+        <tr>
+          <td>{{ item.fullName }}</td>
+          <td>{{ item.username }}</td>
+          <td
+            v-for="role in Object.keys(item.roles).sort((a, b) =>
+              roleOrder.indexOf(a) > roleOrder.indexOf(b) ? 1 : -1
+            )"
+            :key="role"
+          >
+            <v-checkbox v-model="tableData[index].roles[role]" @click="edited = true" />
+          </td>
+        </tr>
+      </template>
+    </v-data-table>
+    <v-btn
+      color="primary"
+      class="my-2"
+      :disabled="!edited"
+      :loading="updating"
+      @click="setFormUsers"
+    >
+      <span>UPDATE</span>
+    </v-btn>
+    <v-btn color="secondary" class="my-2 ml-2" :disabled="!edited" @click="createTableData">
+      <span>ROLLBACK</span>
     </v-btn>
   </div>
 </template>
 
 <script>
 import { rbacService, roleService } from '@/services';
+import { FormRoleCodes } from '@/utils/constants';
 
 export default {
   name: 'TeamManagement',
@@ -43,17 +64,51 @@ export default {
       required: true,
     },
   },
+  computed: {
+    roleOrder: () => Object.values(FormRoleCodes),
+  },
   data() {
     return {
+      edited: false,
+      headers: [],
       formUsers: [],
       loading: true,
       roleList: [],
+      search: '',
       tableData: [],
+      updating: false,
     };
   },
   methods: {
+    createHeaders() {
+      const headers = [
+        { text: 'Full Name', value: 'fullName' },
+        { text: 'Username', value: 'username' },
+      ];
+      this.headers = headers
+        .concat(
+          this.roleList
+            .map((role) => ({
+              align: 'start',
+              filterable: false,
+              sortable: false,
+              text: role.display,
+              value: role.code,
+            }))
+            .sort((a, b) =>
+              this.roleOrder.indexOf(a.value) > this.roleOrder.indexOf(b.value)
+                ? 1
+                : -1
+            )
+            .map((role) => ({ ...role, value: `roles.${role.value}` }))
+        )
+        .map((header) => ({
+          ...header,
+          align: 'start',
+          sortable: false,
+        }));
+    },
     createTableData() {
-      const roleCodes = this.roleList.map((role) => role.code);
       this.tableData = this.formUsers.map((user) => {
         const row = {
           formId: this.formId,
@@ -62,13 +117,29 @@ export default {
           userId: user.userId,
           username: user.username,
         };
-        roleCodes.forEach((role) => {
-          row.roles[role] = user.roles.includes(role);
-        });
-
-        console.log('tableData', row); // eslint-disable-line no-console
+        this.roleList
+          .map((role) => role.code)
+          .forEach((role) => {
+            row.roles[role] = user.roles.includes(role);
+          });
         return row;
       });
+      this.edited = false;
+    },
+    generateUserRoles() {
+      const body = [];
+      this.tableData.forEach((user) => {
+        Object.keys(user.roles).forEach((role) => {
+          if (user.roles[role]) {
+            body.push({
+              formId: user.formId,
+              role: role,
+              userId: user.userId,
+            });
+          }
+        });
+      });
+      return body;
     },
     async getFormUsers() {
       try {
@@ -89,20 +160,62 @@ export default {
       }
     },
     async setFormUsers() {
+      this.updating = true;
       try {
-        const response = await rbacService.setFormUsers({
+        const userRoles = this.generateUserRoles();
+        const response = await rbacService.setFormUsers(userRoles, {
           formId: this.formId,
         });
-        console.log('setFormUsers', response.data); // eslint-disable-line no-console
+        this.formUsers = response.data;
+        this.createTableData();
+        this.edited = false;
       } catch (error) {
         console.error(`Error setting form users: ${error}`); // eslint-disable-line no-console
       }
+      this.updating = false;
     },
   },
   async mounted() {
     await Promise.all([this.getFormUsers(), this.getRolesList()]);
+    this.createHeaders();
     this.createTableData();
     this.loading = false;
   },
 };
 </script>
+
+<style scoped>
+.team-search {
+  width: 100%;
+}
+@media (min-width: 600px) {
+  .team-search {
+    max-width: 20em;
+    float: right;
+  }
+}
+@media (max-width: 599px) {
+  .team-search {
+    padding-left: 16px;
+    padding-right: 16px;
+  }
+}
+
+.team-table {
+  clear: both;
+}
+@media (max-width: 1263px) {
+  .team-table >>> th {
+    vertical-align: top;
+  }
+}
+/* Want to use scss but the world hates me */
+.team-table >>> tbody tr:nth-of-type(odd) {
+  background-color: #f5f5f5;
+}
+.team-table >>> thead tr th {
+  font-weight: normal;
+  color: #003366 !important;
+  font-size: 1.1em;
+}
+</style>
