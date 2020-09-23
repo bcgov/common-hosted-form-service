802d42657fb9ff3fa4a4b8dfeac1a71db675bb86
Jeremy Ho <jujaga@gmail.com>
2020-09-23 11:11:36 -0700

    WIP tabular team management component
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/components/forms/TeamManagement.vue
+++ app/frontend/src/components/forms/TeamManagement.vue
@@ -1,27 +1,35 @@
 <template>
-  <v-card outlined>
-    <v-skeleton-loader
-      :boilerplate="false"
-      class="mx-auto"
-      :loading="loading"
-      type="card-heading, list-item-three-line@4"
-      tile
-    >
-      <v-list-item three-line>
-        <v-list-item-content>
-          <div class="overline mb-4">TEAM MANGEMENT</div>
-          <div v-for="role in roleList" :key="role.code">
-            <div v-if="role.active">
-              <v-list-item-title class="headline my-2">{{ role.display }}</v-list-item-title>
-              <ul v-for="user in formUsers" :key="user.userId">
-                <li v-if="user.roles.includes(role.code)">{{ user.fullName }} ({{ user.username }})</li>
-              </ul>
-            </div>
-          </div>
-        </v-list-item-content>
-      </v-list-item>
-    </v-skeleton-loader>
-  </v-card>
+  <div>
+    <v-card outlined>
+      <v-skeleton-loader :boilerplate="false" class="mx-auto" :loading="loading" type="table">
+        <v-simple-table>
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th>
+                  <div>User</div>
+                </th>
+                <th v-for="role in roleList" :key="role.code">
+                  <div v-if="role.active">{{ role.display }}</div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(user, index) in tableData" :key="user.userId">
+                <td>{{ user.fullName }} ({{ user.username }})</td>
+                <td v-for="role in Object.keys(user.roles)" :key="role">
+                  <v-checkbox v-model="tableData[index].roles[role]" />
+                </td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </v-skeleton-loader>
+    </v-card>
+    <v-btn color="primary" class="my-2" @click="submitFormSchema">
+      <span>Update Permissions</span>
+    </v-btn>
+  </div>
 </template>
 
 <script>
@@ -40,9 +48,28 @@ export default {
       formUsers: [],
       loading: true,
       roleList: [],
+      tableData: [],
     };
   },
   methods: {
+    createTableData() {
+      const roleCodes = this.roleList.map((role) => role.code);
+      this.tableData = this.formUsers.map((user) => {
+        const row = {
+          formId: this.formId,
+          fullName: user.fullName,
+          roles: {},
+          userId: user.userId,
+          username: user.username,
+        };
+        roleCodes.forEach((role) => {
+          row.roles[role] = user.roles.includes(role);
+        });
+
+        console.log('tableData', row); // eslint-disable-line no-console
+        return row;
+      });
+    },
     async getFormUsers() {
       try {
         const response = await rbacService.getFormUsers({
@@ -61,9 +88,20 @@ export default {
         console.error(`Error getting list of roles: ${error}`); // eslint-disable-line no-console
       }
     },
+    async setFormUsers() {
+      try {
+        const response = await rbacService.setFormUsers({
+          formId: this.formId,
+        });
+        console.log('setFormUsers', response.data); // eslint-disable-line no-console
+      } catch (error) {
+        console.error(`Error setting form users: ${error}`); // eslint-disable-line no-console
+      }
+    },
   },
   async mounted() {
     await Promise.all([this.getFormUsers(), this.getRolesList()]);
+    this.createTableData();
     this.loading = false;
   },
 };
