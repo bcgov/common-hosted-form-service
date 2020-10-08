e0b6aa172bded114b2b2ab79d46acf86569c0072
Jeremy Ho <jujaga@gmail.com>
2020-10-08 15:52:15 -0700

    Rename FormEditor to FormSettings Begin route refactor for create flow and existing edit flows - Settings route is temporary - will be integrated into Manage eventually
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/frontend/src/views/form/Create.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="my-6 text-center">Create New Form</h1>
+    <FormSettings />
+  </div>
+</template>
+
+<script>
+import FormSettings from '@/components/designer/FormSettings.vue';
+
+export default {
+  name: 'FormCreate',
+  components: {
+    FormSettings,
+  },
+};
+</script>
