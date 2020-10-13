e6d82a94176570df09969899365f1f187b92a7e4
Jeremy Ho <jujaga@gmail.com>
2020-10-13 13:37:02 -0700

    Update FormSettings component to only be the form fields
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/views/form/Settings.vue
+++ app/frontend/src/views/form/Settings.vue
@@ -2,11 +2,24 @@
 <template>
   <div>
     <h1 class="my-6 text-center">Form Edit</h1>
-    <FormSettings :formId="f" />
+    <v-form ref="settingsForm" v-model="settingsFormValid" lazy-validation>
+      <FormSettings :formId="f" />
+    </v-form>
+    <v-btn
+      color="primary"
+      :disabled="!settingsFormValid"
+      :loading="submitting"
+      @click="updateForm"
+    >
+      <span>Save</span>
+    </v-btn>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex';
+import { mapFields } from 'vuex-map-fields';
+
 import FormSettings from '@/components/designer/FormSettings.vue';
 
 export default {
@@ -15,7 +28,19 @@ export default {
     FormSettings,
   },
   props: {
-    f: String
+    f: String,
+  },
+  computed: mapFields('form', ['form.idps']),
+  data() {
+    return {
+      settingsFormValid: false,
+    };
+  },
+  methods: mapActions('form', ['updateForm']),
+  watch: {
+    idps() {
+      if (this.$refs.settingsForm) this.$refs.settingsForm.validate();
+    },
   },
 };
 </script>
