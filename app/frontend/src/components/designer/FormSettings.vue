0e30692f99641d2f301fb9bb2c098f77c4ac6605
Lucas O'Neil <lucasoneil@gmail.com>
2020-10-13 22:07:08 -0700

    WIP

--- app/frontend/src/components/designer/FormSettings.vue
+++ app/frontend/src/components/designer/FormSettings.vue
@@ -38,10 +38,10 @@
         <v-radio
           disabled
           label="Public (annonymous)"
-          :value="ID_MODE.PUBLIC"
+          :value="ID_PROVIDERS.PUBLIC"
         />
         <v-radio label="Log-in Required" value="login"></v-radio>
-        <v-row v-if="userType === ID_MODE.LOGIN" class="pl-6 mb-2">
+        <v-row v-if="userType === 'login'" class="pl-6 mb-2">
           <v-checkbox
             v-model="idps"
             class="mx-4"
@@ -71,7 +71,7 @@
           />
         </v-row>
         <v-radio label="Specific Team Members" value="team" />
-        <v-row v-if="userType === ID_MODE.TEAM" class="pl-6 mb-2">
+        <v-row v-if="userType === 'team'" class="pl-6 mb-2">
           You can specify users on the form's management screen once created.
         </v-row>
       </v-radio-group>
@@ -82,11 +82,15 @@
 <script>
 import { mapActions } from 'vuex';
 import { mapFields } from 'vuex-map-fields';
-import { IdentityMode, IdentityProviders } from '@/utils/constants';
+import { IdentityProviders } from '@/utils/constants';
 
 export default {
   name: 'FormSettings',
   props: {
+    disabled: {
+      default: false,
+      type: Boolean,
+    },
     formId: String,
   },
   data() {
