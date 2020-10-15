8e93b69127ff6fd4c391a68593a867317156498d
Jeremy Ho <jujaga@gmail.com>
2020-10-15 12:40:12 -0700

    Use minified versions of font-awesome
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/components/designer/FormDesigner.vue
+++ app/frontend/src/components/designer/FormDesigner.vue
@@ -381,6 +381,6 @@ export default {
 <style lang="scss" scoped>
 // include bootstrap and formio styles for the form builder
 @import '~bootstrap/dist/css/bootstrap.min.css';
-@import "~font-awesome/css/font-awesome.css";
+@import "~font-awesome/css/font-awesome.min.css";
 @import 'https://unpkg.com/formiojs@4.11.2/dist/formio.builder.min.css';
 </style>
