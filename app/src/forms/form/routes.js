7eb60329c6b498cd4d545797961365036082c088
Jason Sherman <jsherman@parcsystems.ca>
2020-10-16 10:41:22 -0700

    Add formId filtering to currentUser middleware and /rbac/currentUser. Will respect url param formId or query param formId.

--- app/src/forms/form/routes.js
+++ app/src/forms/form/routes.js
@@ -9,93 +9,91 @@ const P = require('../common/constants').Permissions;
 const controller = require('./controller');
 const keycloak = require('../../components/keycloak');
 
-routes.use(currentUser);
-
-routes.get('/', async (req, res, next) => {
+routes.get('/', currentUser, async (req, res, next) => {
   await controller.listForms(req, res, next);
 });
 
-routes.post('/', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.post('/', currentUser, keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
   await controller.createForm(req, res, next);
 });
 
-routes.get('/:formId', hasFormPermissions(P.FORM_READ), async (req, res, next) => {
+routes.get('/:formId', currentUser, hasFormPermissions(P.FORM_READ), async (req, res, next) => {
   await controller.readForm(req, res, next);
 });
 
-routes.get('/:formId/export', hasFormPermissions(P.FORM_READ), async (req, res, next) => {
+routes.get('/:formId/export', currentUser, hasFormPermissions(P.FORM_READ), async (req, res, next) => {
   await controller.export(req, res, next);
 });
 
-routes.get('/:formId/version', hasFormPermissions(P.FORM_READ), async (req, res, next) => {
+routes.get('/:formId/version', currentUser, hasFormPermissions(P.FORM_READ), async (req, res, next) => {
   await controller.readPublishedForm(req, res, next);
 });
 
-routes.put('/:formId', hasFormPermissions([P.FORM_READ, P.FORM_UPDATE]), async (req, res, next) => {
+routes.put('/:formId', currentUser, hasFormPermissions([P.FORM_READ, P.FORM_UPDATE]), async (req, res, next) => {
   await controller.updateForm(req, res, next);
 });
 
-routes.get('/:formId/submissions', hasFormPermissions([P.FORM_READ, P.SUBMISSION_READ]), async (req, res, next) => {
+routes.get('/:formId/submissions', currentUser, hasFormPermissions([P.FORM_READ, P.SUBMISSION_READ]), async (req, res, next) => {
   await controller.listFormSubmissions(req, res, next);
 });
 
-routes.get('/:formId/versions', hasFormPermissions([P.FORM_READ, P.DESIGN_READ]), async (req, res, next) => {
+routes.get('/:formId/versions', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_READ]), async (req, res, next) => {
   await controller.listVersions(req, res, next);
 });
 
-routes.post('/:formId/versions', hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
+routes.post('/:formId/versions', currentUser, hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
   next(new Problem(410, {detail: 'This method is deprecated, use /forms/id/drafts to create form versions.'}));
 });
 
-routes.get('/:formId/versions/:formVersionId', hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
+routes.get('/:formId/versions/:formVersionId', currentUser, hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
   await controller.readVersion(req, res, next);
 });
 
-routes.put('/:formId/versions/:formVersionId', hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
+routes.put('/:formId/versions/:formVersionId', currentUser, hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
   next(new Problem(410, {detail: 'This method is deprecated, use /forms/id/drafts to modify form versions.'}));
 });
 
-routes.post('/:formId/versions/:formVersionId/publish', hasFormPermissions([P.FORM_READ, P.DESIGN_CREATE]), async (req, res, next) => {
+routes.post('/:formId/versions/:formVersionId/publish', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_CREATE]), async (req, res, next) => {
   await controller.publishVersion(req, res, next);
 });
 
-routes.get('/:formId/versions/:formVersionId/submissions', hasFormPermissions([P.FORM_READ, P.SUBMISSION_READ]), async (req, res, next) => {
+routes.get('/:formId/versions/:formVersionId/submissions', currentUser, hasFormPermissions([P.FORM_READ, P.SUBMISSION_READ]), async (req, res, next) => {
   await controller.listSubmissions(req, res, next);
 });
 
-routes.post('/:formId/versions/:formVersionId/submissions', hasFormPermissions([P.FORM_READ, P.SUBMISSION_CREATE]), async (req, res, next) => {
+routes.post('/:formId/versions/:formVersionId/submissions', currentUser, hasFormPermissions([P.FORM_READ, P.SUBMISSION_CREATE]), async (req, res, next) => {
   await controller.createSubmission(req, res, next);
 });
 
-routes.get('/:formId/versions/:formVersionId/submissions/:formSubmissionId', hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
+routes.get('/:formId/versions/:formVersionId/submissions/:formSubmissionId', currentUser, hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
   next(new Problem(410, {detail: 'This method is deprecated, use /submissions to read a submission.'}));
 });
 
-routes.put('/:formId/versions/:formVersionId/submissions/:formSubmissionId', hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
+routes.put('/:formId/versions/:formVersionId/submissions/:formSubmissionId', currentUser, hasFormPermissions([P.FORM_READ]), async (req, res, next) => {
   next(new Problem(410, {detail: 'This method is deprecated, use /submissions to modify a submission.'}));
 });
 
-routes.get('/:formId/drafts', hasFormPermissions([P.FORM_READ, P.DESIGN_READ]), async (req, res, next) => {
+routes.get('/:formId/drafts', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_READ]), async (req, res, next) => {
   await controller.listDrafts(req, res, next);
 });
 
-routes.post('/:formId/drafts', hasFormPermissions([P.FORM_READ, P.DESIGN_CREATE]), async (req, res, next) => {
+routes.post('/:formId/drafts', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_CREATE]), async (req, res, next) => {
   await controller.createDraft(req, res, next);
 });
 
-routes.get('/:formId/drafts/:formVersionDraftId', hasFormPermissions([P.FORM_READ, P.DESIGN_READ]), async (req, res, next) => {
+routes.get('/:formId/drafts/:formVersionDraftId', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_READ]), async (req, res, next) => {
   await controller.readDraft(req, res, next);
 });
 
-routes.put('/:formId/drafts/:formVersionDraftId', hasFormPermissions([P.FORM_READ, P.DESIGN_UPDATE]), async (req, res, next) => {
+routes.put('/:formId/drafts/:formVersionDraftId', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_UPDATE]), async (req, res, next) => {
   await controller.updateDraft(req, res, next);
 });
 
-routes.delete('/:formId/drafts/:formVersionDraftId', hasFormPermissions([P.FORM_READ, P.DESIGN_DELETE]), async (req, res, next) => {
+routes.delete('/:formId/drafts/:formVersionDraftId', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_DELETE]), async (req, res, next) => {
   await controller.deleteDraft(req, res, next);
 });
 
-routes.post('/:formId/drafts/:formVersionDraftId/publish', hasFormPermissions([P.FORM_READ, P.DESIGN_CREATE]), async (req, res, next) => {
+routes.post('/:formId/drafts/:formVersionDraftId/publish', currentUser, hasFormPermissions([P.FORM_READ, P.DESIGN_CREATE]), async (req, res, next) => {
   await controller.publishDraft(req, res, next);
 });
 
