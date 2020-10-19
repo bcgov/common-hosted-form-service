8ef5011c5c83696954c4a8ff9735cbe506184de3
Lucas O'Neil <lucasoneil@gmail.com>
2020-10-19 10:10:25 -0700

    Adjust create new button

--- app/frontend/src/views/user/Forms.vue
+++ app/frontend/src/views/user/Forms.vue
@@ -1,11 +1,6 @@
 <template>
   <div>
     <h1 class="my-6 text-center">My Forms</h1>
-    <div class="text-right">
-      <strong>
-        <router-link :to="{ name: 'FormCreate' }">Create a New Form</router-link>
-      </strong>
-    </div>
     <FormsTable />
   </div>
 </template>
