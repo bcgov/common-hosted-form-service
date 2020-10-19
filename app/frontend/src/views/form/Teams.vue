c7cfff168e0cecff6aa6fa5920f01e40cd969e5d
Lucas O'Neil <lucasoneil@gmail.com>
2020-10-18 21:58:22 -0700

    Breadcrumbs out

--- app/frontend/src/views/form/Teams.vue
+++ app/frontend/src/views/form/Teams.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <v-breadcrumbs :items="breadcrumbs" />
     <h1 class="my-6 text-center">Team Management</h1>
     <TeamManagement :formId="f" />
   </div>
@@ -19,24 +18,6 @@ export default {
       type: String,
       required: true,
     },
-  },
-  computed: {
-    breadcrumbs() {
-      const path = [
-        {
-          text: 'Form',
-        },
-        {
-          text: 'Manage Form'
-        }
-      ];
-      if (this.$route.meta.breadcrumbTitle) {
-        path.push({
-          text: this.$route.meta.breadcrumbTitle,
-        });
-      }
-      return path;
-    },
-  },
+  }
 };
 </script>
