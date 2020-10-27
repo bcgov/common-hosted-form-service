48e9a2af0427ca181b66ae7cb004b0efe1233a25
Jeremy Ho <jujaga@gmail.com>
2020-10-27 12:59:20 -0700

    Add base component unit tests
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/frontend/tests/unit/components/base/BaseNotificationContainer.spec.js
@@ -0,0 +1,34 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+
+import BaseNotificationContainer from '@/components/base/BaseNotificationContainer.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('BaseNotificationContainer.vue', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store({
+      modules: {
+        notifications: {
+          namespaced: true,
+          state: {
+            notifications: []
+          }
+        }
+      }
+    });
+  });
+
+  it('renders', () => {
+    const wrapper = shallowMount(BaseNotificationContainer, {
+      localVue,
+      store,
+      stubs: ['BaseNotificationBar']
+    });
+
+    expect(wrapper.html()).toMatch('notification-container');
+  });
+});
