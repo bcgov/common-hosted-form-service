6d818c88c82bd8cf4b1991e16d745d0db9f36fcd
Jeremy Ho <jujaga@gmail.com>
2020-10-27 16:00:34 -0700

    Add top level views unit tests
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/tests/unit/views/NotFound.spec.js
+++ app/frontend/tests/unit/views/NotFound.spec.js
@@ -1,23 +1,16 @@
-import { shallowMount } from '@vue/test-utils';
-import Vuetify from 'vuetify';
+import { createLocalVue, shallowMount } from '@vue/test-utils';
 import VueRouter from 'vue-router';
 
 import NotFound from '@/views/NotFound.vue';
 
+const localVue = createLocalVue();
+localVue.use(VueRouter);
+
 describe('NotFound.vue', () => {
-  let router;
-  let vuetify;
-
-  beforeEach(() => {
-    router = new VueRouter();
-    vuetify = new Vuetify();
-  });
-
   it('renders', () => {
     const wrapper = shallowMount(NotFound, {
-      vuetify,
-      router,
-      stubs: ['router-link', 'router-view']
+      localVue,
+      stubs: ['router-link']
     });
 
     expect(wrapper.text()).toMatch('404: Page not found. :(');
