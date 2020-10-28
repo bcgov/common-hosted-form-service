2ff89e8bbd5ba6533bff0ed35c6d8650b4fd52ce
Jeremy Ho <jujaga@gmail.com>
2020-10-28 12:17:28 -0700

    Add most views/form unit tests
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/frontend/tests/unit/views/form/Preview.spec.js
@@ -0,0 +1,16 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+
+import Preview from '@/views/form/Preview.vue';
+
+const localVue = createLocalVue();
+
+describe('Preview.vue', () => {
+  it('renders', () => {
+    const wrapper = shallowMount(Preview, {
+      localVue,
+      stubs: ['BaseSecure', 'FormViewer']
+    });
+
+    expect(wrapper.html()).toMatch('basesecure');
+  });
+});
