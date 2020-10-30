3ae115982034550dea86ab51cead6c786bd2ecd8
Lucas O'Neil <lucasoneil@gmail.com>
2020-10-30 15:38:24 -0700

    email receipt ui

--- app/frontend/src/views/form/Success.vue
+++ app/frontend/src/views/form/Success.vue
@@ -2,16 +2,21 @@
   <div>
     <FormViewer :submissionId="s">
       <template #alert="{ form }">
-        <div class="mb-6">
-          <h1>
+        <div class="mb-5">
+          <h1 class="mb-5">
             <v-icon large color="success">check_circle</v-icon>
             Your form has been submitted successfully
           </h1>
           <h3 v-if="form.showSubmissionConfirmation">
             If you wish to keep a record of this submission, you can keep the
             following Confirmation ID:
-            <strong>{{ s.substring(0, 8).toUpperCase() }}</strong>
+            <mark>{{ s.substring(0, 8).toUpperCase() }}</mark>
           </h3>
+          <RequestReceipt
+            :email="email"
+            :formName="form.name"
+            :submissionId="s"
+          />
           <hr />
         </div>
       </template>
@@ -20,7 +25,10 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 import FormViewer from '@/components/designer/FormViewer.vue';
+import RequestReceipt from '@/components/forms/RequestReceipt.vue';
 
 export default {
   name: 'FormView',
@@ -29,6 +37,8 @@ export default {
   },
   components: {
     FormViewer,
+    RequestReceipt,
   },
+  computed: mapGetters('auth', ['email'])
 };
 </script>
