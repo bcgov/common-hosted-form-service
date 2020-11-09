e49c693ef0375bc8376cf6bb15504d8335ae4f6d
Lucas O'Neil <lucasoneil@gmail.com>
2020-11-09 09:41:58 -0800

    wip

--- app/src/forms/rbac/routes.js
+++ app/src/forms/rbac/routes.js
@@ -15,39 +15,39 @@ routes.get('/idps', async (req, res, next) => {
   await controller.getIdentityProviders(req, res, next);
 });
 
-routes.get('/forms', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.get('/forms', async (req, res, next) => {
   await controller.getFormUsers(req, res, next);
 });
 
-routes.put('/forms', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.put('/forms', async (req, res, next) => {
   await controller.setFormUsers(req, res, next);
 });
 
-routes.get('/users', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.get('/users', async (req, res, next) => {
   await controller.getUserForms(req, res, next);
 });
 
-routes.put('/users', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.put('/users', async (req, res, next) => {
   await controller.setUserForms(req, res, next);
 });
 
-routes.get('/', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.get('/', async (req, res, next) => {
   await controller.list(req, res, next);
 });
 
-routes.post('/', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.post('/', async (req, res, next) => {
   await controller.create(req, res, next);
 });
 
-routes.get('/:id', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.get('/:id', async (req, res, next) => {
   await controller.read(req, res, next);
 });
 
-routes.put('/:id', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.put('/:id', async (req, res, next) => {
   await controller.update(req, res, next);
 });
 
-routes.delete('/:id', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
+routes.delete('/:id', async (req, res, next) => {
   await controller.delete(req, res, next);
 });
 
