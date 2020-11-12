59ac4257e245e0c3cd3e6aa3cf43e53b92c24b44
Lucas O'Neil <lucasoneil@gmail.com>
2020-11-12 11:49:49 -0800

    conditional

--- app/frontend/src/components/bcgov/BCGovHeader.vue
+++ app/frontend/src/components/bcgov/BCGovHeader.vue
@@ -23,6 +23,9 @@
       <h1 data-test="btn-header-title" class="font-weight-bold title">
         {{ appTitle }}
       </h1>
+      <v-chip v-if="!hideBetaChip" class="ma-2" color="green" text-color="white">
+        Beta
+      </v-chip>
       <v-spacer />
       <BaseAuthButton />
     </v-toolbar>
@@ -38,6 +41,10 @@ export default {
         ? this.$route.meta.title
         : process.env.VUE_APP_TITLE;
     },
+    hideBetaChip() {
+      // Temp while we have this chip in, hide it for form submitter pages
+      return this.$route && this.$route.meta && this.$route.meta.hideNavBar;
+    }
   },
 };
 </script>
