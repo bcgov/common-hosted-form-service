13ecc8df4cc2836bc0bc44e9ee254807af36942f
Lucas O'Neil <lucasoneil@gmail.com>
2020-11-12 09:18:54 -0800

    Hide chip

--- app/frontend/src/components/bcgov/BCGovHeader.vue
+++ app/frontend/src/components/bcgov/BCGovHeader.vue
@@ -23,7 +23,7 @@
       <h1 data-test="btn-header-title" class="font-weight-bold title">
         {{ appTitle }}
       </h1>
-      <v-chip class="ma-2" color="green" text-color="white">
+      <v-chip v-if="!hideNavBar" class="ma-2" color="green" text-color="white">
         Beta
       </v-chip>
       <v-spacer />
