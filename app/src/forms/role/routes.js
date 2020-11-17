3795add08fa4d3ff5b16b33f4d6368e19ffc6bbd
Lucas O'Neil <lucasoneil@gmail.com>
2020-11-16 21:23:56 -0800

    Allow roles to be fetched by non-admins

--- app/src/forms/role/routes.js
+++ app/src/forms/role/routes.js
@@ -6,22 +6,21 @@ const currentUser = require('../auth/middleware/userAccess').currentUser;
 const controller = require('./controller');
 const keycloak = require('../../components/keycloak');
 
-routes.use(keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`));
 routes.use(currentUser);
 
-routes.get('/', async (req, res, next) => {
+routes.get('/', keycloak.protect(), async (req, res, next) => {
   await controller.list(req, res, next);
 });
 
-routes.post('/', async (req, res, next) => {
+routes.post('/', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
   await controller.create(req, res, next);
 });
 
-routes.get('/:code', async (req, res, next) => {
+routes.get('/:code', keycloak.protect(), async (req, res, next) => {
   await controller.read(req, res, next);
 });
 
-routes.put('/:code', async (req, res, next) => {
+routes.put('/:code', keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`), async (req, res, next) => {
   await controller.update(req, res, next);
 });
 
