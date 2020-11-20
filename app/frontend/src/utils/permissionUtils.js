58b6afa916500436d1c24504157445bfa8f2e3bb
Lucas O'Neil <lucasoneil@gmail.com>
2020-11-19 22:10:28 -0800

    Check if auth needed for form routes

--- app/frontend/src/utils/permissionUtils.js
+++ app/frontend/src/utils/permissionUtils.js
@@ -1,4 +1,6 @@
 import { FormPermissions, IdentityProviders } from '@/utils/constants';
+import { formService } from '@/services';
+import store from '@/store';
 
 //
 // Utility Functions for determining permissions
@@ -44,4 +46,42 @@ export function checkSubmissionView(userForm) {
     FormPermissions.SUBMISSION_UPDATE
   ];
   return userForm && userForm.permissions && userForm.permissions.some(p => perms.includes(p));
+}
+
+/**
+ * @function determineFormNeedsAuth
+ * When loading a form to fill out, determine if the user needs to log in to submit it
+ * @param {Object} store The vuex store reference
+ * @param {String} formId The form guid
+ * @param {String} formId The submissionId guid
+ * @param {Object} next The routing next object
+ */
+export async function determineFormNeedsAuth(formId, submissionId, next) {
+  // before this view is loaded, determine if this is a public form
+  // if it IS, they don't need to log in. If it's secured, go through auth loop
+  // If authed already skip all this
+  if (!store.getters['auth/authenticated']) {
+    try {
+      // Get this form or submission
+      if (formId) {
+        await formService.readForm(formId);
+      } else if (submissionId) {
+        await formService.getSubmission(submissionId);
+      }
+    } catch (error) {
+      // If there's a 401 trying to get this form, make that user log in
+      if (error.response && error.response.status === 401) {
+        window.location.replace(
+          store.getters['auth/createLoginUrl']({ idpHint: 'idir' })
+        );
+      } else {
+        // Other errors raise an issue
+        store.dispatch('notifications/addNotification', {
+          message: 'An error occurred while loading this form.',
+          consoleError: `Error loading form ${formId} or submission ${submissionId}: ${error}`,
+        });
+      }
+    }
+  }
+  next();
 }
