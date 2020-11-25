af48b59454f6dc7c793ac832127dbb87a60ee246
Csaky <Tim.Csaky@gov.bc.ca>
2020-11-24 16:49:35 -0800

    add side padding to navbar and header

--- app/frontend/src/components/bcgov/BCGovHeader.vue
+++ app/frontend/src/components/bcgov/BCGovHeader.vue
@@ -1,29 +1,21 @@
 <template>
   <header :class="{'gov-header': true, 'd-print-none': formSubmitMode}">
-    <v-toolbar color="#003366" flat>
+    <v-toolbar color="#003366" flat class="px-md-12">
       <!-- Navbar content -->
       <a href="https://www2.gov.bc.ca" data-test="btn-header-logo">
         <v-img
           alt="B.C. Government Logo"
-          class="d-none d-sm-flex d-md-none"
-          contain
-          height="3.5rem"
-          src="@/assets/images/bc_logo_square.svg"
-          width="3.5rem"
-        />
-        <v-img
-          alt="B.C. Government Logo"
-          class="d-none d-md-flex"
+          class="d-flex"
           contain
           height="3.5rem"
           src="@/assets/images/bc_logo.svg"
           width="10rem"
         />
       </a>
-      <h1 v-if="!formSubmitMode" data-test="btn-header-title" class="font-weight-bold title">
+      <h1 v-if="!formSubmitMode" data-test="btn-header-title" class="font-weight-bold title d-none d-md-flex pl-4">
         {{ appTitle }}
       </h1>
-      <v-chip v-if="!formSubmitMode" class="ma-2" color="secondary" text-color="white">Beta</v-chip>
+      <v-chip v-if="!formSubmitMode" class="d-none d-md-flex ma-2" color="secondary" text-color="white">Beta</v-chip>
       <v-spacer />
       <BaseAuthButton />
     </v-toolbar>
@@ -56,7 +48,6 @@ export default {
     font-family: inherit !important;
     color: #ffffff;
     overflow: hidden;
-    padding: 1rem;
     margin-bottom: 0;
     @media #{map-get($display-breakpoints, 'sm-and-down')} {
       font-size: 1rem !important;
