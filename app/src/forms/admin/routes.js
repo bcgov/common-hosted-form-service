50f69ca6567f4d95576310bc58b4246ddc913e74
Lucas O'Neil <lucasoneil@gmail.com>
2020-12-11 14:07:32 -0800

    WIP

--- app/src/forms/admin/routes.js
+++ app/src/forms/admin/routes.js
@@ -6,9 +6,26 @@ const currentUser = require('../auth/middleware/userAccess').currentUser;
 const controller = require('./controller');
 const keycloak = require('../../components/keycloak');
 
+// Always have this applied to all routes here
 routes.use(keycloak.protect(`${config.get('server.keycloak.clientId')}:admin`));
 routes.use(currentUser);
 
+// Routes under the /admin pathing will fetch data without doing Form permission checks in the database
+// As such, this should ALWAYS remain under the :admin role check and that KC role should not be given out
+// other than to people who have permission to read all data
+
+
+//
+// Forms
+//
+routes.get('/forms', async (req, res, next) => {
+  await controller.listForms(req, res, next);
+});
+
+
+//
+// Users
+//
 routes.get('/users', async (req, res, next) => {
   await controller.getUsers(req, res, next);
 });
