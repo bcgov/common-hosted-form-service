f8d6bbab35d71366a141e2576643c688ce4c61f9
Lucas O'Neil <lucasoneil@gmail.com>
2020-12-16 22:19:32 -0800

    Shortcut to KC

--- app/frontend/src/components/admin/AdministerUser.vue
+++ app/frontend/src/components/admin/AdministerUser.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
-    UI TBD
+    <h3>{{ user.fullName }}</h3>
+    <h4>User Details</h4>
     <pre>{{ user }}</pre>
+
+    <v-btn
+      color="primary"
+      text
+      small
+      :href="userUrl"
+      target="_blank"
+    >
+      <span>Open SSO console</span>
+    </v-btn>
   </div>
 </template>
 
@@ -18,6 +29,9 @@ export default {
   },
   computed: {
     ...mapGetters('admin', ['user']),
+    userUrl() {
+      return `${this.$config.keycloak.serverUrl}/admin/${this.$config.keycloak.realm}/console/#/realms/${this.$config.keycloak.realm}/users/${this.user.keycloakId}`;
+    },
   },
   methods: {
     ...mapActions('admin', ['readUser']),
