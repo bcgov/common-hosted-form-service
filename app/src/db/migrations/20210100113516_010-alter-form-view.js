87985cbc8e76380534fe795f589455fa66306c1d
Csaky <Tim.Csaky@gov.bc.ca>
2021-01-14 15:09:45 -0800

    update migration file syntax

--- app/src/db/migrations/20210100113516_010-alter-form-view.js
+++ app/src/db/migrations/20210100113516_010-alter-form-view.js
@@ -2,7 +2,7 @@
 exports.up = function (knex) {
   return Promise.resolve()
     .then(() => knex.schema.raw(`create or replace view user_form_access_vw as
-    select r."userId", u."keycloakId", u.username, u."fullName", u."firstName", u."lastName", u.email, r."formId", f.name as "formName", f.description as "formDescription", f.labels, f."identityProviders", f.idps, f.active, f."formVersionId", f.version, r.roles, p.permissions
+    select r."userId", u."keycloakId", u.username, u."fullName", u."firstName", u."lastName", u.email, r."formId", f.name as "formName", f.labels, f."identityProviders", f.idps, f.active, f."formVersionId", f.version, r.roles, p.permissions, f.description as "formDescription"
     from "user" u join user_form_roles_vw r on u.id = r."userId" join user_form_permissions_vw p on r."userId" = p."userId" and r."formId" = p."formId" join form_vw f on f.id = p."formId"
     order by lower(u."lastName"::text), lower(u."firstName"::text), lower(f.name::text)`));
 };
