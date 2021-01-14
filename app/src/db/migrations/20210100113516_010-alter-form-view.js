e58dc67d0eddc20df9d2f1f9be2d2aa96204f2f5
Csaky <Tim.Csaky@gov.bc.ca>
2021-01-13 17:35:28 -0800

    update the right sql view

--- app/src/db/migrations/20210100113516_010-alter-form-view.js
+++ app/src/db/migrations/20210100113516_010-alter-form-view.js
@@ -1,19 +1,17 @@
 
 exports.up = function (knex) {
   return Promise.resolve()
-    .then(() => knex.schema.raw(`create or replace view public_form_access_vw as
-    select null as "userId", null as "keycloakId", 'public' as username, 'public' as "fullName", null as "firstName", null as "lastName", null as email, f.id as "formId", f.name as "formName", f.description as "formDescription", f.labels, f."identityProviders", f.idps, f.active, f."formVersionId", f.version, '{}'::varchar[] as roles, '{submission_create,form_read}'::varchar[] as permissions
-    from form_vw f
-    where 'public' = ANY(f."idps")
-    order by lower(f.name::text)`));
+    .then(() => knex.schema.raw(`create or replace view user_form_access_vw as
+    select r."userId", u."keycloakId", u.username, u."fullName", u."firstName", u."lastName", u.email, r."formId", f.name as "formName", f.description as "formDescription", f.labels, f."identityProviders", f.idps, f.active, f."formVersionId", f.version, r.roles, p.permissions
+    from "user" u join user_form_roles_vw r on u.id = r."userId" join user_form_permissions_vw p on r."userId" = p."userId" and r."formId" = p."formId" join form_vw f on f.id = p."formId"
+    order by lower(u."lastName"::text), lower(u."firstName"::text), lower(f.name::text)`));
 };
 
 exports.down = function (knex) {
   // Restore original definition of this view from 001-views
   return Promise.resolve()
-    .then(() => knex.schema.raw(`create or replace view public_form_access_vw as
-    select null as "userId", null as "keycloakId", 'public' as username, 'public' as "fullName", null as "firstName", null as "lastName", null as email, f.id as "formId", f.name as "formName", f.labels, f."identityProviders", f.idps, f.active, f."formVersionId", f.version, '{}'::varchar[] as roles, '{submission_create,form_read}'::varchar[] as permissions
-    from form_vw f
-    where 'public' = ANY(f."idps")
-    order by lower(f.name::text)`));
+    .then(() => knex.schema.raw(`create or replace user_form_access_vw as
+    select r."userId", u."keycloakId", u.username, u."fullName", u."firstName", u."lastName", u.email, r."formId", f.name as "formName", f.labels, f."identityProviders", f.idps, f.active, f."formVersionId", f.version, r.roles, p.permissions
+    from "user" u join user_form_roles_vw r on u.id = r."userId" join user_form_permissions_vw p on r."userId" = p."userId" and r."formId" = p."formId" join form_vw f on f.id = p."formId"
+    order by lower(u."lastName"::text), lower(u."firstName"::text), lower(f.name::text)`));
 };
