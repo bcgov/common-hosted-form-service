db3fe3e324a0aac7eb4263a2108e0f0d2d74e69e
Jeremy Ho <jujaga@gmail.com>
2021-02-12 17:08:41 -0800

    Improve submission and permissionUtils coverage
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/tests/unit/utils/permissionUtils.spec.js
+++ app/frontend/tests/unit/utils/permissionUtils.spec.js
@@ -1,3 +1,4 @@
+import { formService } from '@/services';
 import { FormPermissions, IdentityProviders } from '@/utils/constants';
 import * as permissionUtils from '@/utils/permissionUtils';
 
@@ -53,5 +54,56 @@ describe('checkSubmissionView', () => {
   it('should be true when at least one appropriate permission exists', () => {
     expect(permissionUtils.checkSubmissionView({ permissions: [FormPermissions.SUBMISSION_READ] })).toBeTruthy();
     expect(permissionUtils.checkSubmissionView({ permissions: [FormPermissions.SUBMISSION_UPDATE] })).toBeTruthy();
+  });
+});
+
+describe('determineFormNeedsAuth', () => {
+  const mockNext = jest.fn();
+  const getSubmissionSpy = jest.spyOn(formService, 'getSubmission');
+  const readFormSpy = jest.spyOn(formService, 'readForm');
+
+  beforeEach(() => {
+    mockNext.mockReset();
+    getSubmissionSpy.mockReset();
+    readFormSpy.mockReset();
+  });
+
+  afterEach(() => {
+    mockNext.mockRestore();
+    getSubmissionSpy.mockReset();
+    readFormSpy.mockRestore();
+  });
+
+  it('should call readForm service', async () => {
+    readFormSpy.mockImplementation(() => {});
+    await permissionUtils.determineFormNeedsAuth('f', undefined, mockNext);
+    expect(readFormSpy).toHaveBeenCalledTimes(1);
+    expect(mockNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call getSubmission service', async () => {
+    getSubmissionSpy.mockImplementation(() => {});
+    await permissionUtils.determineFormNeedsAuth(undefined, 's', mockNext);
+    expect(getSubmissionSpy).toHaveBeenCalledTimes(1);
+    expect(mockNext).toHaveBeenCalledTimes(1);
+  });
+
+  it('should passthrough when nothing specified', async () => {
+    await permissionUtils.determineFormNeedsAuth(undefined, undefined, mockNext);
+    expect(mockNext).toHaveBeenCalledTimes(1);
+  });
+
+  it.skip('should force a user login', async () => {
+    // delete window.location;
+    // window.location = { replace: jest.fn() };
+    readFormSpy.mockImplementation(() => {
+      const error = new Error();
+      error.response = { status: 401 };
+      throw error;
+    });
+
+    await permissionUtils.determineFormNeedsAuth('f', undefined, mockNext);
+    expect(window.location.replace).toHaveBeenCalledTimes(1);
+    expect(mockNext).toHaveBeenCalledTimes(1);
   });
 });
