ac16d3cdc0a4164290fa0666bb65db0eac653490
Jeremy Ho <jujaga@gmail.com>
2021-02-12 17:08:41 -0800

    Improve submission and permissionUtils coverage
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/tests/unit/views/form/Submit.spec.js
+++ app/frontend/tests/unit/views/form/Submit.spec.js
@@ -1,16 +1,33 @@
 import { createLocalVue, shallowMount } from '@vue/test-utils';
 
+import * as permissionUtils from '@/utils/permissionUtils';
 import Submit from '@/views/form/Submit.vue';
 
 const localVue = createLocalVue();
 
 describe('Submit.vue', () => {
-  it('renders', () => {
+  const determineFormNeedsAuthSpy = jest.spyOn(permissionUtils, 'determineFormNeedsAuth');
+
+  beforeEach(() => {
+    determineFormNeedsAuthSpy.mockReset();
+  });
+
+  afterEach(() => {
+    determineFormNeedsAuthSpy.mockRestore();
+  });
+
+  it('renders', async () => {
+    determineFormNeedsAuthSpy.mockImplementation(() => {});
+
     const wrapper = shallowMount(Submit, {
       localVue,
       stubs: ['FormViewer']
     });
 
+    Submit.beforeRouteEnter.call(wrapper.vm, { query: 'q' }, undefined, undefined);
+    await wrapper.vm.$nextTick();
+
     expect(wrapper.html()).toMatch('formviewer');
+    expect(determineFormNeedsAuthSpy).toHaveBeenCalledTimes(1);
   });
 });
