b310bc0519413aef7afbff184b03822a5043c6bf
Jeremy Ho <jujaga@gmail.com>
2021-04-27 17:17:04 -0700

    Add Vuex logger to non-production environments
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/store/index.js
+++ app/frontend/src/store/index.js
@@ -6,9 +6,16 @@ import notifications from '@/store/modules/notifications.js';
 
 Vue.use(Vuex);
 
+const plugins = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  plugins.push(Vuex.createLogger());
+}
+
 export default new Vuex.Store({
   // Modules not specified below are expected to dynamically register when needed
   modules: { form, notifications },
+  plugins: plugins,
   state: {},
   mutations: {},
   actions: {}
