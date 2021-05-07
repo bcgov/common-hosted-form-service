d61b31eef5abbf17166633da947b2c3c3231b52c
Lucas O'Neil <lucasoneil@gmail.com>
2021-05-07 16:48:33 -0700

    Link to metabase on admin

--- app/frontend/src/views/admin/Root.vue
+++ app/frontend/src/views/admin/Root.vue
@@ -1,6 +1,28 @@
 <template>
   <v-container>
-    <h1 class="mt-6">Admin</h1>
+    <v-row class="mt-6" no-gutters>
+      <v-col cols="12" sm="6">
+        <h1>Admin</h1>
+      </v-col>
+      <v-spacer />
+      <v-col class="text-sm-right" cols="12" sm="6">
+        <v-tooltip bottom>
+          <template #activator="{ on, attrs }">
+            <v-btn
+              class="mx-1"
+              color="primary"
+              icon
+              v-bind="attrs"
+              v-on="on"
+              href="https://chefs-metabase.apps.silver.devops.gov.bc.ca/public/dashboard/876f86e4-7c09-4c8d-9205-97dc20267f5f"
+            >
+              <v-icon>insert_chart</v-icon>
+            </v-btn>
+          </template>
+          <span>Go to Metabase Dashboard</span>
+        </v-tooltip>
+      </v-col>
+    </v-row>
     <AdminPage />
   </v-container>
 </template>
