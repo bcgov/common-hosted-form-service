f3c5fafc156fd71d3ada8fa8896349a2f692f689
Lucas O'Neil <lucasoneil@gmail.com>
2021-06-03 16:26:59 -0700

    Email temporary revert

--- app/frontend/src/views/user/SubmissionView.vue
+++ app/frontend/src/views/user/SubmissionView.vue
@@ -8,7 +8,10 @@ import FormSubmission from '@/components/forms/submission/UserSubmission.vue';
 export default {
   name: 'UserSubmissionView',
   props: {
-    s: String,
+    s: {
+      type: String,
+      required: true,
+    },
   },
   components: {
     FormSubmission,
