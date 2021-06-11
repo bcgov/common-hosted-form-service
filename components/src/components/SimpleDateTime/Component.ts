e0139044da539a093dc7293c91298c376edaa19f
MichaelChung <michaelchung4567@gmail.com>
2021-06-11 15:22:51 -0700

    Linting fixes

--- components/src/components/SimpleDateTime/Component.ts
+++ components/src/components/SimpleDateTime/Component.ts
@@ -1,13 +1,12 @@
 /* tslint:disable */
-import _ from "lodash";
-import { Components } from "formiojs";
+import { Components } from 'formiojs';
 const ParentComponent = (Components as any).components.datetime;
-import editForm from "./Component.form";
+import editForm from './Component.form';
 
-import { Constants } from "../Common/Constants";
+import { Constants } from '../Common/Constants';
 
-const ID = "simpledatetime";
-const DISPLAY = "Date / Time";
+const ID = 'simpledatetime';
+const DISPLAY = 'Date / Time';
 
 export default class Component extends (ParentComponent as any) {
   static schema(...extend) {
@@ -16,22 +15,22 @@ export default class Component extends (ParentComponent as any) {
         type: ID,
         label: DISPLAY,
         key: ID,
-        format: "yyyy-MM-dd hh:mm a",
+        format: 'yyyy-MM-dd hh:mm a',
         useLocaleSettings: false,
         allowInput: true,
         enableDate: true,
         enableTime: false,
-        defaultValue: "",
-        defaultDate: "",
-        displayInTimezone: "viewer",
-        timezone: "",
-        datepickerMode: "day",
+        defaultValue: '',
+        defaultDate: '',
+        displayInTimezone: 'viewer',
+        timezone: '',
+        datepickerMode: 'day',
         datePicker: {
           showWeeks: true,
           startingDay: 0,
-          initDate: "",
-          minMode: "day",
-          maxMode: "year",
+          initDate: '',
+          minMode: 'day',
+          maxMode: 'year',
           yearRows: 4,
           yearColumns: 5,
           minDate: null,
@@ -56,8 +55,8 @@ export default class Component extends (ParentComponent as any) {
   static get builderInfo() {
     return {
       title: DISPLAY,
-      group: "simple",
-      icon: "calendar",
+      group: 'simple',
+      icon: 'calendar',
       weight: 20,
       documentation: Constants.DEFAULT_HELP_LINK,
       schema: Component.schema(),
@@ -68,7 +67,7 @@ export default class Component extends (ParentComponent as any) {
     super(component, options, data);
     // if enableTime is set to false, manually add time fields since it get removed later on through Form IO
     if (!this.component.enableTime) {
-      this.component.format = this.component.format.concat(" hh:mm a");
+      this.component.format = this.component.format.concat(' hh:mm a');
     }
   }
 }
