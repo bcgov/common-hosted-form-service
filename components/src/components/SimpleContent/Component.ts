7661ad48d83f25b72a14462b9335d420edd46437
Jeremy Ho <jujaga@gmail.com>
2021-06-16 10:01:46 -0700

    Revert "Merge pull request #187 from MichaelChung123/dev-1765"
    This reverts commit bef8a999bb00100f694b0136efd6c144493f0176, reversing
    changes made to 1ee0e430543e4cf331f87386be8a9f75b86f8eeb.
    
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- components/src/components/SimpleContent/Component.ts
+++ components/src/components/SimpleContent/Component.ts
@@ -9,46 +9,26 @@ const ID = 'simplecontent';
 const DISPLAY = 'Static Text';
 
 export default class Component extends (ParentComponent as any) {
-  static schema(...extend) {
-    return ParentComponent.schema({
-      type: ID,
-      label: DISPLAY,
-      key: ID,
-      input: false,
-      html: ''
-    }, ...extend);
-  }
-
-  public static editForm = editForm;
-
-  static get builderInfo() {
-    return {
-      title: DISPLAY,
-      group: 'simple',
-      icon: 'pencil-square-o',
-      weight: 40,
-      documentation: Constants.DEFAULT_HELP_LINK,
-      schema: Component.schema()
-    };
-  }
-
-  addTargetBlankToLinks = (html) => {
-    if (html.includes('<a')) {
-      html = html.replace(/<a(?![^>]+target=)/g, '<a target="_blank"');
+    static schema(...extend) {
+        return ParentComponent.schema({
+            type: ID,
+            label: DISPLAY,
+            key: ID,
+            input: false,
+            html: ''
+        }, ...extend);
     }
-    return html;
-  }
 
-  attach(element) {
-    this.loadRefs(element, { html: 'single' });
-    this.refs.html.innerHTML = this.addTargetBlankToLinks(this.refs.html.innerHTML);
-    if (this.component.refreshOnChange) {
-      this.on('change', () => {
-        if (this.refs.html) {
-          this.setContent(this.refs.html, this.content);
-        }
-      }, true);
+    public static editForm = editForm;
+
+    static get builderInfo() {
+        return {
+            title: DISPLAY,
+            group: 'simple',
+            icon: 'pencil-square-o',
+            weight: 40,
+            documentation: Constants.DEFAULT_HELP_LINK,
+            schema: Component.schema()
+        };
     }
-    return super.attach(element);
-  }
 }
