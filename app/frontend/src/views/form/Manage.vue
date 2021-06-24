e6cf3cd283669b6d270ff37be8e890c981a7bc2b
MichaelChung123 <michaelchung4567@gmail.com>
2021-06-24 14:04:19 -0700

    consistency change for Manage view

--- app/frontend/src/views/form/Manage.vue
+++ app/frontend/src/views/form/Manage.vue
@@ -1,15 +1,19 @@
 <template>
   <BaseSecure>
-    <v-row class="my-6" no-gutters>
-      <v-col cols="12" sm="6">
+    <v-row class="mt-6" no-gutters>
+      <!-- page title -->
+      <v-col cols="12" sm="6" md="6" order="2" order-sm="1">
         <h1>Manage Form</h1>
-        <h3>{{ this.form.name }}</h3>
       </v-col>
-      <v-spacer />
-      <v-col class="text-sm-right" cols="12" sm="6">
+      <!-- buttons -->
+      <v-col class="text-left text-sm-right" cols="12" sm="6" md="6" order="1" order-sm="2">
         <v-skeleton-loader :loading="loading" type="actions">
           <ManageFormActions />
         </v-skeleton-loader>
+      </v-col>
+      <!-- form name -->
+      <v-col cols="12" order="3">
+        <h3>{{ this.form.name }}</h3>
       </v-col>
     </v-row>
     <v-skeleton-loader :loading="loading" type="list-item-two-line">
