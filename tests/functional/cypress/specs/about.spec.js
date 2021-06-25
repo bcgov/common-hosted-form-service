b7fc9a9a6ff4cc5c85b7795ebca41fcc8f567a14
Jeremy Ho <jujaga@gmail.com>
2021-06-25 14:24:24 -0700

    Change tests to use relative baseUrl
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- tests/functional/cypress/specs/about.spec.js
+++ tests/functional/cypress/specs/about.spec.js
@@ -1,6 +1,6 @@
 describe('Application About Page', () => {
   it('Visits the app about page', () => {
-    cy.visit('https://chefs-test.apps.silver.devops.gov.bc.ca/app/');
+    cy.visit('/');
     cy.contains('h1', 'Create and publish forms, and receive submissions from your users');
   });
 });
