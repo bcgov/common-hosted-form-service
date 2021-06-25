516ce8203779756e100ca2a9e3150b5bd59f2149
Jeremy Ho <jujaga@gmail.com>
2021-06-25 14:24:24 -0700

    Change tests to use relative baseUrl
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- tests/functional/cypress/specs/forms/kitchen-sink.spec.js
+++ tests/functional/cypress/specs/forms/kitchen-sink.spec.js
@@ -1,6 +1,7 @@
 describe('Kitchen Sink Example Form', () => {
   it('Visits the kitchen sink form', () => {
-    cy.visit('https://chefs-test.apps.silver.devops.gov.bc.ca/app/form/submit?f=897e2ca1-d81c-4079-a135-63b930f98590');
+    cy.visit('/form/submit?f=897e2ca1-d81c-4079-a135-63b930f98590');
+    cy.contains('h1', 'Kitchen Sink Simple');
     cy.contains('button', 'Submit');
   });
 });
