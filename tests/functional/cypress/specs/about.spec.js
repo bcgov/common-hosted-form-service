3d6707b8b6ed871c57e090c367cf3ac329aa7614
Jeremy Ho <jujaga@gmail.com>
2021-06-30 17:09:20 -0700

    Implement network mock intercepts for loading
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- tests/functional/cypress/specs/about.spec.js
+++ tests/functional/cypress/specs/about.spec.js
@@ -1,6 +1,8 @@
+const depEnv = 'app';
+
 describe('Application About Page', () => {
   it('Visits the app about page', () => {
-    cy.visit('/');
+    cy.visit(`/${depEnv}`);
     cy.contains('h1', 'Create and publish forms, and receive submissions from your users');
   });
 });
