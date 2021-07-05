0c8b6ddf79443be1ef4c2de261dbfb22eb83332b
MichaelChung123 <michaelchung4567@gmail.com>
2021-07-05 11:33:50 -0700

    revisions 1

--- components/src/components/SimpleRadios/editForm/Component.edit.validation.ts
+++ components/src/components/SimpleRadios/editForm/Component.edit.validation.ts
@@ -1,19 +1,5 @@
+import common from '../../Common/Simple.edit.validation';
+
 export default [
-    {
-        weight: 10,
-        type: 'checkbox',
-        label: 'Required',
-        tooltip: 'A required field must be filled in before the form can be submitted.',
-        key: 'validate.required',
-        input: true
-    },
-    {
-        weight: 200,
-        key: 'validate.customMessage',
-        label: 'Custom Error Message',
-        placeholder: 'Custom Error Message',
-        type: 'textfield',
-        tooltip: 'Error message displayed if any error occurred.',
-        input: true
-    }
+  ...common
 ];
