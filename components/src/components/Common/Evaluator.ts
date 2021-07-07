8fbf362eec1ce4840ae218f04c971483358b3f17
MichaelChung123 <michaelchung4567@gmail.com>
2021-07-06 23:31:44 -0700

    initial advanced conditionals

---
+++ components/src/components/Common/Evaluator.ts
@@ -0,0 +1,7 @@
+import _ from 'lodash';
+const Evaluator = {
+  noeval: false,
+  protectedEval: false, // This property can be customized only by plugins
+};
+
+export default Evaluator;
--- components/src/components/Common/Simple.edit.conditional.ts
+++ components/src/components/Common/Simple.edit.conditional.ts
@@ -1,4 +1,5 @@
 import { getContextComponents } from 'formiojs/utils/utils';
+import EditFormUtils from './utils';
 /* eslint-disable quotes, max-len */
 export default [
   {
@@ -40,6 +41,18 @@ export default [
         key: 'conditional.eq'
       }
     ]
-  }
+  },
+  EditFormUtils.javaScriptValue(
+    'Advanced Conditions',
+    'customConditional',
+    'conditional.json',
+    110,
+    '<p>You must assign the <strong>show</strong> variable a boolean result.</p>' +
+    '<p><strong>Note: Advanced Conditional logic will override the results of the Simple Conditional logic.</strong></p>' +
+    '<h5>Example</h5><pre>show = !!data.showMe;</pre>',
+    '<p><a href="http://formio.github.io/formio.js/app/examples/conditions.html" target="_blank">Click here for an example</a></p>',
+    '',
+    ''
+  )
 ];
 /* eslint-enable quotes, max-len */
