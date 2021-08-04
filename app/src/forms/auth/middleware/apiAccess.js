c916eddc4c2bb2233221104dc0730f43387cf12e
Jeremy Ho <jujaga@gmail.com>
2021-08-04 12:26:47 -0700

    Integrate Basic auth support to forms routes Make apiAccess middleware only act on Basic authorization types Remove redundant currentUser middleware declarations
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/forms/auth/middleware/apiAccess.js
+++ app/src/forms/auth/middleware/apiAccess.js
@@ -5,26 +5,32 @@ const { validate: uuidValidate } = require('uuid');
 const { readApiKey } = require('../../form/service');
 
 module.exports = async (req, res, next) => {
-  // URL params should override query string params of the same attribute
-  const params = { ...req.query, ...req.params };
-  let secret = ''; // Must be initialized as a string
+  // Check if authorization header is basic auth
+  if (req.headers && req.headers.authorization && req.headers.authorization.startsWith('Basic ')) {
+    // URL params should override query string params of the same attribute
+    const params = { ...req.query, ...req.params };
+    let secret = ''; // Must be initialized as a string
 
-  if (params.formId && uuidValidate(params.formId)) {
-    const result = await readApiKey(params.formId);
-    secret = result.secret;
-  }
-
-  const checkCredentials = basicAuth({
-    // Must be a synchronous function
-    authorizer: (username, password) => {
-      const userMatch = basicAuth.safeCompare(username, params.formId);
-      const pwMatch = basicAuth.safeCompare(password, secret);
-      return userMatch & pwMatch;
-    },
-    unauthorizedResponse: () => {
-      return new Problem(401, { detail: 'Invalid authorization credentials.' });
+    if (params.formId && uuidValidate(params.formId)) {
+      const result = await readApiKey(params.formId);
+      secret = result.secret;
     }
-  });
 
-  return checkCredentials(req, res, next);
+    const checkCredentials = basicAuth({
+      // Must be a synchronous function
+      authorizer: (username, password) => {
+        const userMatch = params.formId && basicAuth.safeCompare(username, params.formId);
+        const pwMatch = secret && basicAuth.safeCompare(password, secret);
+        return userMatch & pwMatch;
+      },
+      unauthorizedResponse: () => {
+        return new Problem(401, { detail: 'Invalid authorization credentials.' });
+      }
+    });
+
+    req.apiUser = true; // Flag current request as an API entity
+    return checkCredentials(req, res, next);
+  } else {
+    next();
+  }
 };
