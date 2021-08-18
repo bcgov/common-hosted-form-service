41f49599db3f75b2a6ca3643203a48c70c5755a0
Jeremy Ho <jujaga@gmail.com>
2021-08-18 12:07:52 -0700

    Implement minimally viable single user form preferences endpoint set
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/src/forms/common/models/tables/userFormPreferences.js
@@ -0,0 +1,31 @@
+const { Model } = require('objection');
+const { Timestamps } = require('../mixins');
+const { Regex } = require('../../constants');
+const stamps = require('../jsonSchema').stamps;
+
+class UserFormPreferences extends Timestamps(Model) {
+  static get tableName() {
+    return 'user_form_preferences';
+  }
+
+  // This is a composite key - order of attributes is important
+  static get idColumn() {
+    return ['userId', 'formId'];
+  }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      required: ['formId', 'userId'],
+      properties: {
+        userId: { type: 'string', pattern: Regex.UUID },
+        formId: { type: 'string', pattern: Regex.UUID },
+        preferences: { type: 'jsonb' },
+        ...stamps
+      },
+      additionalProperties: false
+    };
+  }
+}
+
+module.exports = UserFormPreferences;
--- app/src/forms/user/service.js
+++ app/src/forms/user/service.js
@@ -1,8 +1,9 @@
-const { User } = require('../common/models');
+const { transaction } = require('objection');
+
+const { User, UserFormPreferences } = require('../common/models');
 
 const service = {
-
-  list: async (params) => {
+  list: (params) => {
     return User.query()
       .skipUndefined()
       .modify('filterKeycloakId', params.keycloakId)
@@ -15,12 +16,55 @@ const service = {
       .modify('orderLastFirstAscending');
   },
 
-  read: async (userId) => {
+  read: (userId) => {
     return User.query()
       .findById(userId)
       .throwIfNotFound();
-  }
+  },
 
+  deleteUserFormPreferences: (currentUser, formId) => {
+    return UserFormPreferences.query()
+      .deleteById([currentUser.id, formId])
+      .throwIfNotFound();
+  },
+
+  readUserFormPreferences: (currentUser, formId) => {
+    return UserFormPreferences.query()
+      .findById([currentUser.id, formId])
+      .first();
+  },
+
+  updateUserFormPreferences: async (currentUser, formId, preferences) => {
+    let trx;
+    try {
+      let result;
+
+      const current = await service.readUserFormPreferences(currentUser, formId);
+      trx = await transaction.start(UserFormPreferences.knex());
+
+      if (current) {
+        result = await UserFormPreferences.query(trx)
+          .patchAndFetchById([currentUser.id, formId], {
+            preferences: preferences,
+            updatedBy: currentUser.username
+          });
+      } else {
+        result = await UserFormPreferences.query(trx)
+          .insertAndFetch({
+            userId: currentUser.id,
+            formId: formId,
+            preferences: preferences,
+            createdBy: currentUser.username
+          });
+      }
+
+      await trx.commit();
+      return result;
+    } catch (err) {
+      if (trx) await trx.rollback();
+      throw err;
+    }
+  }
 };
 
 module.exports = service;
