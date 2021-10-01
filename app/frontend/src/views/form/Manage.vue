b2d4b35054e27b82009d192778b1f82f9689d48f
Jeremy Ho <jujaga@gmail.com>
2021-10-01 15:29:54 -0700

    Restrict staff page views to only IDIR users Shift manage page contents into separate view
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/frontend/src/views/form/Manage.vue
+++ app/frontend/src/views/form/Manage.vue
@@ -1,67 +1,24 @@
 <template>
-  <BaseSecure>
-    <v-row class="mt-6" no-gutters>
-      <!-- page title -->
-      <v-col cols="12" sm="6" order="2" order-sm="1">
-        <h1>Manage Form</h1>
-      </v-col>
-      <!-- buttons -->
-      <v-col class="text-right" cols="12" sm="6" order="1" order-sm="2">
-        <v-skeleton-loader :loading="loading" type="actions">
-          <ManageFormActions />
-        </v-skeleton-loader>
-      </v-col>
-      <!-- form name -->
-      <v-col cols="12" order="3">
-        <h3>{{ this.form.name }}</h3>
-      </v-col>
-    </v-row>
-    <v-skeleton-loader :loading="loading" type="list-item-two-line">
-      <ManageForm />
-    </v-skeleton-loader>
+  <BaseSecure :idp="IDP.IDIR">
+    <ManageLayout :f="f" />
   </BaseSecure>
 </template>
 
 <script>
-import { mapActions, mapGetters } from 'vuex';
-
-import ManageForm from '@/components/forms/manage/ManageForm.vue';
-import ManageFormActions from '@/components/forms/manage/ManageFormActions.vue';
+import ManageLayout from '@/components/forms/manage/ManageLayout.vue';
+import { IdentityProviders } from '@/utils/constants';
 
 export default {
   name: 'FormManage',
-  components: { ManageForm, ManageFormActions },
+  components: { ManageLayout },
   props: {
     f: {
       type: String,
       required: true,
     },
   },
-  data() {
-    return {
-      loading: true,
-    };
-  },
   computed: {
-    ...mapGetters('form', ['form']),
-  },
-  methods: {
-    ...mapActions('form', [
-      'fetchDrafts',
-      'fetchForm',
-      'getFormPermissionsForUser',
-    ]),
-  },
-  async mounted() {
-    this.loading = true;
-    await Promise.all([
-      // Get the form for this management page
-      this.fetchForm(this.f),
-      this.fetchDrafts(this.f),
-      // Get the permissions for this form
-      this.getFormPermissionsForUser(this.f),
-    ]);
-    this.loading = false;
+    IDP: () => IdentityProviders,
   },
 };
 </script>
