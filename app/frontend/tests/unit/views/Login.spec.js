81cb6b75ece60b5f9c61fa3db70e8d59d80db582
Jeremy Ho <jujaga@gmail.com>
2021-10-01 16:50:27 -0700

    Update misplaced/broken FE unit tests
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

---
+++ app/frontend/tests/unit/views/Login.spec.js
@@ -0,0 +1,37 @@
+import { createLocalVue, shallowMount } from '@vue/test-utils';
+import Vuex from 'vuex';
+
+import Login from '@/views/Login.vue';
+
+const localVue = createLocalVue();
+localVue.use(Vuex);
+
+describe('Login.vue', () => {
+  let store;
+
+  beforeEach(() => {
+    store = new Vuex.Store();
+    store.registerModule('auth', {
+      namespaced: true,
+      getters: {
+        authenticated: () => true,
+        keycloakReady: () => true,
+        createLoginUrl: () => () => 'testurl'
+      },
+      actions: {
+        login: () => jest.fn()
+      }
+    });
+  });
+
+  it('renders without error', async () => {
+    const wrapper = shallowMount(Login, {
+      localVue,
+      store,
+      stubs: ['router-link']
+    });
+    await localVue.nextTick();
+
+    expect(wrapper.html()).toMatch('router-link');
+  });
+});
