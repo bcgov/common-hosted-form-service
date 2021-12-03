a039cdb2a58d0c12f9ae8733fd775efccfdd5b9e
Naomi Aro <naomi.aro@gov.bc.ca>
2021-12-02 16:42:48 -0800

    give additional a default

--- components/src/overrides/editform/utils.ts
+++ components/src/overrides/editform/utils.ts
@@ -3,7 +3,7 @@ import EditFormUtils from 'formiojs/components/_classes/component/editForm/utils
 const originalLogicVariablesTable = EditFormUtils.logicVariablesTable;
 
 // decorate this function to inclue global updates to evalContext.
-function logicVariablesTable(additional) {
+function logicVariablesTable(additional = '') {
   const customEval =
     '<tr><th>token</th><td>The parsed JWT token</td></tr>' +
     '<tr><th>user</th><td>The currently logged in user</td></tr>';
