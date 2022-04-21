43bf78675bf2b8f57eac5b3df0871bf63a630481
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-04-20 20:17:45 -0600

    fixed code climate fixes

--- components/src/components/Common/simple.texfield.data.ts
+++ components/src/components/Common/simple.texfield.data.ts
@@ -9,4 +9,3 @@ export default  {
       return !context.data.allowMultipleMasks;
     }
   }
-  
--- components/src/components/SimpleCity/editForm/Component.edit.display.ts
+++
@@ -1,9 +0,0 @@
-import common from '../../Common/Simple.edit.display';
-import commonv2 from '../../Common/Simple.edit.display.v2'
-export default {
-  key: 'display',
-  components: [
-    ...common,
-    ...commonv2
-  ]
-}
