b7947f6c2b45ccad974b3e84fd979e21de3a46e4
jasonchung1871 <jason.chung@gov.bc.ca>
2022-04-21 14:26:54 -0700

    Fix version draft duplication
    This is caused by setting the draft version to be equal to the number of
    form versions + 1. This is resolved by setting the version to the number
    of form versions + the number of drafts - the number of drafts we've
    looked at.

--- app/frontend/src/components/forms/manage/ManageVersions.vue
+++ app/frontend/src/components/forms/manage/ManageVersions.vue
@@ -275,9 +275,9 @@ export default {
     versionList() {
       if (this.hasDraft) {
         // reformat draft object and then join with versions array
-        const reDraft = this.drafts.map((obj) => {
+        const reDraft = this.drafts.map((obj, idx) => {
           obj.published = false;
-          obj.version = this.form.versions.length + 1;
+          obj.version = this.form.versions.length + this.drafts.length - idx;
           obj.isDraft = true;
           delete obj.formVersionId;
           delete obj.schema;
