3efa33712076efa288daab7b5de31c43eaa9b341
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-04-21 18:59:19 -0600

    Restructured FormDesigner.vue
    I removed the FormActionsButton.vue
    I made the export, import, save and setting icons/buttons to be sticky/fixed

--- app/frontend/src/components/designer/FormDesigner.vue
+++ app/frontend/src/components/designer/FormDesigner.vue
@@ -1,161 +1,131 @@
 <template>
-  <div>
-    <v-row class="mt-6" no-gutters>
-      <!-- page title -->
-      <v-col cols="12" sm="6" order="2" order-sm="1">
-        <h1>Form Design</h1>
-      </v-col>
-      <!-- buttons -->
-      <v-col class="text-right" cols="12" sm="6" order="1" order-sm="2">
-        <v-tooltip bottom>
-          <template #activator="{ on, attrs }">
-            <v-btn
-              class="mx-md-1 mx-0"
-              @click="submitFormSchema"
-              color="primary"
-              icon
-              v-bind="attrs"
-              v-on="on"
-            >
-              <v-icon>save</v-icon>
-            </v-btn>
-          </template>
-          <span>Save Design</span>
-        </v-tooltip>
-        <v-tooltip bottom>
-          <template #activator="{ on, attrs }">
-            <v-btn
-              class="mx-1"
-              @click="onExportClick"
-              color="primary"
-              icon
-              v-bind="attrs"
-              v-on="on"
-            >
-              <v-icon>get_app</v-icon>
-            </v-btn>
-          </template>
-          <span>Export Design</span>
-        </v-tooltip>
-        <v-tooltip bottom>
-          <template #activator="{ on, attrs }">
-            <v-btn
-              class="mx-1"
-              @click="$refs.uploader.click()"
-              color="primary"
-              icon
-              v-bind="attrs"
-              v-on="on"
-            >
-              <v-icon>publish</v-icon>
-              <input
-                class="d-none"
-                @change="loadFile"
-                ref="uploader"
-                type="file"
-                accept=".json"
-              />
-            </v-btn>
-          </template>
-          <span>Import Design</span>
-        </v-tooltip>
-        <v-tooltip bottom>
-          <template #activator="{ on, attrs }">
-            <router-link
-              :to="{ name: 'FormManage', query: { f: formId } }"
-              :class="{ 'disabled-router': !formId }"
-            >
-              <v-btn
-                class="mx-1"
-                color="primary"
-                :disabled="!formId"
-                icon
-                v-bind="attrs"
-                v-on="on"
-              >
-                <v-icon>settings</v-icon>
-              </v-btn>
-            </router-link>
-          </template>
-          <span>Manage Form</span>
-        </v-tooltip>
-      </v-col>
-      <!-- form name -->
-      <v-col cols="12" order="3">
-        <h3 v-if="name">{{ name }}</h3>
-      </v-col>
-      <!-- version number-->
-      <v-col class="mb-3" cols="12" order="4">
-        <em>Version: {{ this.displayVersion }}</em>
-      </v-col>
-    </v-row>
-    <v-alert
-      :value="saved || saving"
-      :class="
-        saving
-          ? NOTIFICATIONS_TYPES.INFO.class
-          : NOTIFICATIONS_TYPES.SUCCESS.class
-      "
-      :color="
-        saving
-          ? NOTIFICATIONS_TYPES.INFO.color
-          : NOTIFICATIONS_TYPES.SUCCESS.color
-      "
-      :icon="
-        saving
-          ? NOTIFICATIONS_TYPES.INFO.icon
-          : NOTIFICATIONS_TYPES.SUCCESS.icon
-      "
-      transition="scale-transition"
-    >
-      <div v-if="saving">
-        <v-progress-linear indeterminate />
-        Saving
-      </div>
-      <div v-else>
-        Your form has been successfully saved
-        <router-link
-          :to="{ name: 'FormPreview', query: { f: formId, d: draftId } }"
-          target="_blank"
-          class="mx-5"
-        >
-          Preview
-        </router-link>
-        <router-link :to="{ name: 'FormManage', query: { f: formId } }">
-          Go to Manage Form to Publish
-        </router-link>
-      </div>
-    </v-alert>
-
-    <BaseInfoCard class="my-6">
-      <h4 class="primary--text">
-        <v-icon class="mr-1" color="primary">info</v-icon>IMPORTANT!
-      </h4>
-      <p class="my-0">
-        Use the <strong>SAVE DESIGN</strong> button when you are done building
-        this form.
-      </p>
-      <p class="my-0">
-        The <strong>SUBMIT</strong> button is provided for your user to submit
-        this form and will be activated after it is saved.
-      </p>
-    </BaseInfoCard>
-    <FormBuilder
-      :form="formSchema"
-      :key="reRenderFormIo"
-      :options="designerOptions"
-      @change="onChangeMethod"
-      @render="onRenderMethod"
-      @initialized="init"
-      class="form-designer"
-    />
-    <FormActionButtons 
-      @form-schema-submit="submitFormSchema"
-      @form-schema-export="onExportClick"
-      @load-form-schema="loadFile"
-
-    />
-  </div>
+  <v-row>
+    <v-col>
+      <v-container fluid>
+        <v-row class="mt-6 fixed" no-gutters>
+          <v-col>
+            <v-row>
+              <v-col cols="12" sm="6" order="2" order-sm="1">
+                <div>
+                  <h1>Form Design</h1>
+                </div>
+                <div>
+                  <h3 v-if="name">{{ name }}</h3>
+                </div>
+                <div>
+                  <em>Version: {{ this.displayVersion }}</em>
+                </div>
+              </v-col>
+            </v-row>
+          </v-col>
+          <v-col class="text-right" cols="12" sm="6" order="1" order-sm="2">
+            <v-tooltip bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  class="formSubmit"
+                  @click="submitFormSchema"
+                  color="primary"
+                  icon
+                  v-bind="attrs"
+                  v-on="on"
+                  fixed
+                  right
+                >
+                  <v-icon>save</v-icon>
+                </v-btn>
+              </template>
+              <span>Save Design</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  class="formExport"
+                  @click="onExportClick"
+                  color="primary"
+                  icon
+                  v-bind="attrs"
+                  v-on="on"
+                  fixed
+                  right
+                >
+                  <v-icon>get_app</v-icon>
+                </v-btn>
+              </template>
+              <span>Export Design</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template #activator="{ on, attrs }">
+                <v-btn
+                  class="formImport"
+                  @click="$refs.uploader.click()"
+                  color="primary"
+                  icon
+                  v-bind="attrs"
+                  v-on="on"
+                  fixed
+                  right
+                >
+                  <v-icon>publish</v-icon>
+                  <input
+                    class="d-none"
+                    @change="loadFile"
+                    ref="uploader"
+                    type="file"
+                    accept=".json"
+                  />
+                </v-btn>
+              </template>
+              <span>Import Design</span>
+            </v-tooltip>
+            <v-tooltip bottom>
+              <template #activator="{ on, attrs }">
+                <router-link
+                  :to="{ name: 'FormManage', query: { f: formId } }"
+                  :class="{ 'disabled-router': !formId }"
+                >
+                  <v-btn
+                    class="formSetting"
+                    color="primary"
+                    :disabled="!formId"
+                    icon
+                    v-bind="attrs"
+                    v-on="on"
+                    fixed
+                    right
+                  >
+                    <v-icon>settings</v-icon>
+                  </v-btn>
+                </router-link>
+              </template>
+              <span>Manage Form</span>
+            </v-tooltip>
+          </v-col>    
+        </v-row>
+      </v-container>
+      <BaseInfoCard class="my-6">
+        <h4 class="primary--text">
+          <v-icon class="mr-1" color="primary">info</v-icon>IMPORTANT!
+        </h4>
+        <p class="my-0">
+          Use the <strong>SAVE DESIGN</strong> button when you are done building
+          this form.
+        </p>
+        <p class="my-0">
+          The <strong>SUBMIT</strong> button is provided for your user to submit
+          this form and will be activated after it is saved.
+        </p>
+      </BaseInfoCard>
+      <FormBuilder
+        :form="formSchema"
+        :key="reRenderFormIo"
+        :options="designerOptions"
+        @change="onChangeMethod"
+        @render="onRenderMethod"
+        @initialized="init"
+        class="form-designer"
+      /> 
+    </v-col>
+  </v-row>
 </template>
 
 <script>
@@ -167,13 +137,11 @@ import templateExtensions from '@/plugins/templateExtensions';
 import { formService } from '@/services';
 import { IdentityMode, NotificationTypes } from '@/utils/constants';
 import { generateIdps } from '@/utils/transformUtils';
-import FormActionButtons from '@/components/designer/FormActionButtons';
 
 export default {
   name: 'FormDesigner',
   components: {
     FormBuilder,
-    FormActionButtons
   },
   props: {
     draftId: String,
@@ -502,4 +470,31 @@ export default {
 .disabled-router {
   pointer-events: none;
 }
+
+
+.formSubmit{
+
+  margin-top: 0px;
+  margin-right:230px;
+  z-index:1000;
+}
+
+.formExport{
+  margin-top:0px;
+  margin-right:190px;
+  z-index:1000;
+}
+
+.formImport{
+  margin-top:0px;
+  margin-right:150px;
+  z-index:1000;
+}
+
+.formSetting{
+  margin-top:0px;
+  margin-right:110px;
+  z-index:1000;
+}
+
 </style>
