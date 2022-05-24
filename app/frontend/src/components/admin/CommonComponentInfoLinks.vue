74d6a5d1b5d758edb5b6cecbf6ee11efb3485f88
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-05-24 00:19:42 -0600

    remove hand-coded component names so that they will be dynamically read from the store
    -removed hand-coded component names so that they will be dynamically read from the store
    -improved on styling
    - remove hand-coded link in InformationLinkPreviewDialog.uve

--- app/frontend/src/components/admin/CommonComponentInfoLinks.vue
+++ app/frontend/src/components/admin/CommonComponentInfoLinks.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="mt-5">
-    <v-expansion-panels flat
-                        class="nrmc-expand-collapse">
+    <v-expansion-panels flat class="nrmc-expand-collapse">
       <v-expansion-panel
+        class="mb-1"
         flat
+        v-for="(group, index) in groupList" :key="index"
+        @click="onExpansionPanelClick(group)"
       >
-        <v-expansion-panel-header>
+        <v-expansion-panel-header class="panel" :style="panelHeadStyle.get(group)">
           <div class="header">
-            <strong>Basic Layout</strong>
+            <strong>{{group}}</strong>
           </div>
         </v-expansion-panel-header>
-        <v-expansion-panel-content>
-          <BasicLayout :layoutList="basicLayoutList" :itemsList="ccHelpInfoList.basicLayout?ccHelpInfoList.basicLayout:[]"/>
+        <v-expansion-panel-content >
+          <GeneralLayout :layoutList="groupComponentsList" :itemsList="ccHelpInfoList.basicLayout?ccHelpInfoList.basicLayout:[]"/>
         </v-expansion-panel-content>
       </v-expansion-panel>
     </v-expansion-panels>
@@ -19,30 +21,85 @@
 </template>
 
 <script>
-import BasicLayout from '@/components/infolinks/BasicLayout.vue';
+import GeneralLayout from '@/components/infolinks/GeneralLayout.vue';
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
   name: 'CommonComponentInfoLinks',
-  components:{BasicLayout},
+  components:{GeneralLayout},
   data(){
     return{
-      basicLayoutList:[
-        {name:'Basic Layout',id:1},
-        {name:'Text/Images',id:2},
-        {name:'Columns-2',id:3},
-        {name:'Columns-3',id:4},
-        {name:'Columns-4',id:5},
-        {name:'Tabs',id:6},
-        {name:'Panel',id:7}
-      ]
+      isPanelOpened : new Map(),
+      groupComponentsList:[],
+      panelHeadStyle:new Map(),
+      notActivePanelHead:{background: '#BFBDBD14 0% 0% no-repeat padding-box',
+        border: '1px solid #7070703F'},
+      activePanelHead:{background: '#F1F8FF 0% 0% no-repeat padding-box',
+        border: '1px solid #7070703F'}
     };
   },
   methods:{
-    ...mapActions('admin',['listCommonCompsHelpInfo'])
+    ...mapActions('admin',['listCommonCompsHelpInfo']),
+    isObject(obj) {
+      var type = typeof obj;
+      return type === 'function' || (type === 'object' && !!obj);
+    },
+
+    onExpansionPanelClick(group) 
+    {
+      if(this.isPanelOpened.get(group)===undefined || !this.isPanelOpened.get(group))
+      {
+        this.isPanelOpened.set(group,true);
+        this.groupComponentsList=this.extractGroupComponents(group);
+        this.panelHeadStyle.set(group,this.activePanelHead);
+      }
+      else{
+        this.isPanelOpened.set(group,false);
+        this.panelHeadStyle.set(group,this.notActivePanelHead);
+      }
+
+      for(let key of this.isPanelOpened.keys()){
+        if(key!==group) {
+          this.isPanelOpened.set(key,false);
+          this.panelHeadStyle.set(key,this.notActivePanelHead);
+        }
+      }
+    },
+    extractGroups(){
+      let allgroups=[];
+      for (const key in this.builder) {
+        if(this.isObject(this.builder[key])){
+          if(this.builder[key].title){
+            allgroups.push(this.builder[key].title);
+            this.panelHeadStyle.set(this.builder[key].title,this.notActivePanelHead);
+          } 
+          
+        }
+      }
+      return allgroups;
+    },
+    extractGroupComponents(group){
+      let groupComponents = [];
+      for (const key in this.builder) {
+        if(this.isObject(this.builder[key])){
+          if(this.builder[key].title===group && this.builder[key].components) 
+          {
+            for(let componentName of Object.keys(this.builder[key].components)){
+              groupComponents.push({'name':componentName});
+            }
+          }
+        }
+      }
+      return groupComponents;
+    }
   },
   computed:{
     ...mapGetters('admin',['ccHelpInfo','ccHelpInfoList']),
+    ...mapGetters('form', ['builder']),
+    groupList(){
+      return this.extractGroups();
+    },
+
   },
   watch:{
     ccHelpInfo(){
@@ -54,3 +111,18 @@ export default {
   }
 };
 </script>
+<style>
+  
+
+.panel {
+  font-weight: 600;
+  font-family:Open Sans;
+  font-style: normal;
+  font-size: 16px;
+  letter-spacing: 0px;
+  color: #313132;
+  text-transform: uppercase;
+}
+
+
+</style>
