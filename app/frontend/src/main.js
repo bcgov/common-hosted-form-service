d47b4df3645ac53886ea35e16c52045f03127878
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-06-08 14:02:00 -0600

    Re-design ColumnPererences.vue  page
    - install font-awesome icons
    - re-design ColumnPererences.vue  page using the prototype from the design guy.

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -20,12 +20,20 @@ import BcGovFormioComponents from '@/formio/lib';
 import { Formio } from 'vue-formio';
 Formio.use(BcGovFormioComponents);
 
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+/* add font awesome icon component */
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+
 import VueKeycloakJs from '@/plugins/keycloak';
 import vuetify from '@/plugins/vuetify';
 Vue.config.productionTip = false;
 
 NProgress.configure({ showSpinner: false });
 NProgress.start();
+
+
 
 // Globally register all components with base in the name
 const requireComponent = require.context('@/components', true, /Base[A-Z]\w+\.(vue|js)$/);
