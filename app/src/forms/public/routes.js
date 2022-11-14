19d6c4208a007529d503b995393e6fe232b6208d
bigvaimarcella <111837621+bigvaimarcella@users.noreply.github.com>
2022-11-14 14:05:09 -0500

    Fix form error urlin email

--- app/src/forms/public/routes.js
+++ app/src/forms/public/routes.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const controller = require('./controller');
+
 routes.get('/reminder', async (req, res, next) => {
   await controller.sendReminderToSubmitter(req, res, next);
 });
