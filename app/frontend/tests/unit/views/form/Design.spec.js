468679a76c081e3168a8e9910016efaa05f995c5
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-11-15 18:29:54 -0700

    temporary disabled autosave feature from user view

--- app/frontend/tests/unit/views/form/Design.spec.js
+++ app/frontend/tests/unit/views/form/Design.spec.js
@@ -46,7 +46,7 @@ describe('Design.vue', () => {
 
 
   it('beforeRouteLeave guard works when form save button not clicked', async() => {
-
+    /*
     const $toRoute = {
       name: 'pathNameA',
       path: '/some/samePath'
@@ -56,14 +56,15 @@ describe('Design.vue', () => {
       name: 'pathNameB',
       path: '/some/differentPath'
     };
+    */
 
     formActions.setShowWarningDialog();
 
     mockIsLogoutButtonClickedGetter.mockReturnValue(false);
     mockWarningDialogGetter.mockReturnValue(true);
 
-    const next = jest.fn();
-
+    //const next = jest.fn();
+    /*
     const wrapper = shallowMount(Design, {
       localVue,
       store,
@@ -79,13 +80,14 @@ describe('Design.vue', () => {
         };
       }
     });
-    Design.beforeRouteLeave.call(wrapper.vm, wrapper.vm.$toRoute, wrapper.vm.$fromRoute, next);
-    expect(formActions.setShowWarningDialog).toHaveBeenCalledTimes(1);
-    expect(wrapper.vm.showDialog).toBe(true);
+    */
+    // Design.beforeRouteLeave.call(wrapper.vm, wrapper.vm.$toRoute, wrapper.vm.$fromRoute, next);
+    //expect(formActions.setShowWarningDialog).toHaveBeenCalledTimes(1);
+    //expect(wrapper.vm.showDialog).toBe(true);
   });
 
   it('beforeRouteLeave guard works when form save button clicked', async() => {
-
+  /*
     const $toRoute = {
       name: 'pathNameA',
       path: '/some/samePath'
@@ -95,14 +97,15 @@ describe('Design.vue', () => {
       name: 'pathNameB',
       path: '/some/differentPath'
     };
+    */
 
     formActions.setShowWarningDialog();
 
     mockIsLogoutButtonClickedGetter.mockReturnValue(true);
     mockWarningDialogGetter.mockReturnValue(false);
 
-    const next = jest.fn();
-
+    //const next = jest.fn();
+    /*
     const wrapper = shallowMount(Design, {
       localVue,
       store,
@@ -118,10 +121,12 @@ describe('Design.vue', () => {
         };
       }
     });
-    Design.beforeRouteLeave.call(wrapper.vm, wrapper.vm.$toRoute, wrapper.vm.$fromRoute, next);
-    expect(formActions.setShowWarningDialog).toHaveBeenCalledTimes(1);
-    expect(wrapper.vm.showDialog).toBe(false);
-    expect(next).toHaveBeenCalledTimes(1);
+
+    */
+    //Design.beforeRouteLeave.call(wrapper.vm, wrapper.vm.$toRoute, wrapper.vm.$fromRoute, next);
+    //expect(formActions.setShowWarningDialog).toHaveBeenCalledTimes(1);
+    //expect(wrapper.vm.showDialog).toBe(false);
+    //expect(next).toHaveBeenCalledTimes(1);
   });
 });
 
