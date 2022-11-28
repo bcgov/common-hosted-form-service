77a618ae6589c3ed72b6c9538aa080560423387a
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-11-28 02:09:45 -0700

    resolve lint error

--- app/src/forms/bcgeoaddress/routes.js
+++ app/src/forms/bcgeoaddress/routes.js
@@ -3,7 +3,6 @@ const routes = require('express').Router();
 const controller = require('./controller');
 
 routes.post('/address', async (req, res, next) => {
-  console.log(req);
   await controller.searchBCGeoAddress(req, res, next);
 });
 
