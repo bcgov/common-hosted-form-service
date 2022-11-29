6b4e66df5c22bced09ffbdf57676f24f008b0f54
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-11-28 21:07:43 -0700

    proxy bcaddress custom form.io component url call to backend
    BC Geo Address URL uses the search query to get the list of addresses and some data that matches the search query. In this commit, the BCAddress custom component was rewired to make a call to the chefs' backend, and the backend called the BC Geo Address URL instead of the custom component calling the URL directly. It protects the API key.

--- components/src/components/BCAddress/Component.ts
+++ components/src/components/BCAddress/Component.ts
@@ -1,10 +1,9 @@
 
 /* tslint:disable */
-import {Components, Formio} from 'formiojs';
+import {Components} from 'formiojs';
 import { Constants } from '../Common/Constants';
 import editForm from './Component.form';
-import _ from 'lodash';
-import autocompleter from 'autocompleter';
+
 export const AddressComponentMode = {
   Autocomplete: 'autocomplete',
   Manual: 'manual',
@@ -22,11 +21,9 @@ export default class Component extends (ParentComponent as any) {
             label: DISPLAY,
             type: ID,
             key: ID,
-            provider: "custom",
-            url:process.env.VUE_APP_BC_GEO_ADDRESS_APIURL,
-            queryProperty:"addressString",
-            responseProperty:"features",
-            displayValueProperty:"properties.fullAddress",
+            provider: 'custom',
+            providerOptions: {bcGeoAddressURL:process.env.VUE_APP_BC_GEO_ADDRESS_APIURL,
+              url:process.env.VUE_APP_CHEFS_GEO_ADDRESS_APIURL},
             queryParameters:{"echo": false,
             "brief": true,
             "minScore": 55,
@@ -60,80 +57,24 @@ export default class Component extends (ParentComponent as any) {
     async attach(element) {
         super.attach(element);
         try {
-            console.log(this.builderMode,this.manualMode);
-            this.searchInput.forEach((element, index) => {
-                autocompleter({
-                input: element,
-                debounceWaitMs: 300,
-                fetch: (text, update) => {
-                  const query = text;
-                  this.makeRequestChefsURL(query).then(update);
-                },
-                render: (address) => {
-                    console.log("I am in render", address);
-                  const div = this.ce('div');
-                  div.textContent = this.getDisplayValues(address);
-                  return div;
-                },
-                onSelect: (address) => {
-                    console.log("I am in onSelect", address);
-                  this.onSelectAddress(address, element, index);
-                },
-              });
-            });
+            let {
+                providerOptions,
+                queryParameters,
+            } = this.component;
+
+            if(providerOptions) {
+                if(!providerOptions.params) {
+                    providerOptions["params"]={}
+                }
+                if(queryParameters) {
+                    providerOptions.params ={...providerOptions.params,...queryParameters,bcGeoAddressURL:providerOptions["bcGeoAddressURL"]}
+                }
+            }
+
         } catch (err) {
             console.log(`This error is from Custom BC Address component in form.io: Failed to acquire configuration: ${err.message}`);
         }
     }
 
-    onSelectAddress(address, element, index) {
-        if (this.isMultiple) {
-          this.address[index] = address;
-          this.address = [...this.address];
-        }
-        else {
-          this.address = address;
-        }
-
-        this.triggerChange({
-          modified: true,
-        });
-
-        if (element) {
-            console.log("-------------->>>>>>",this.getDisplayValue(this.isMultiple ? this.address[index] : this.address));
-          element.value = this.getDisplayValue(this.isMultiple ? this.address[index] : this.address);
-        }
-
-        this.updateRemoveIcon(index);
-      }
-
-    getDisplayValue(address) {
-        let {
-            displayValueProperty,
-        } = this.component;
-        return displayValueProperty ? _.get(address, displayValueProperty, '') : String(address);
-      }
-
-      getDisplayValues(value = this.address) {
-        let {
-            provider,
-        } = this.component;
-        return (provider && !this.manualMode)
-          ? this.getDisplayValue(value)
-          : '';
-      }
-
-    makeRequestChefsURL(query) {
-        let {
-            url,
-            queryProperty,
-            responseProperty,
-            displayValueProperty,
-            queryParameters,
-        } = this.component;
-        const requestOptions={url:url,queryProperty:{[queryProperty]:query},responseProperty:responseProperty, displayValueProperty:displayValueProperty, queryParameters:queryParameters};
-        return Formio.makeStaticRequest("http://localhost:8080/app/api/v1/bcgeoaddress/address", "Post", requestOptions,null).then((result) => {
-        return responseProperty ? _.get(result, responseProperty, []) : result});
-      }
 }
 export {};
