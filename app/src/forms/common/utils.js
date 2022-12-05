a3c19650561b22226d5dbb4ad4848f4b1c81fb37
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2022-12-05 12:14:02 -0700

    fix lint error

--- app/src/forms/common/utils.js
+++ app/src/forms/common/utils.js
@@ -43,13 +43,13 @@ const queryUtils = {
 
 const unCamelCase = (str) => {
   return str
-      // insert a space between lower & upper
-      .replace(/([a-z])([A-Z])/g, '$1 $2')
-      // space before last upper in a sequence followed by lower
-      .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
-      // uppercase the first character
-      .replace(/^./, function(str){ return str.toUpperCase(); })
-}
+  // insert a space between lower & upper
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+  // space before last upper in a sequence followed by lower
+    .replace(/\b([A-Z]+)([A-Z])([a-z])/, '$1 $2$3')
+  // uppercase the first character
+    .replace(/^./, function(str){ return str.toUpperCase(); });
+};
 
 
 const reArrangeFormSubmissionJSon = (obj) => {
