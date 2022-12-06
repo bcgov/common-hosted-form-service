1ad84201664caaccaeed9bc4408f96bf45cc707d
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2022-12-05 16:36:25 -0800

    Removed idpUsername
    idpUsername will continue to be the username column. Users that log in will begin to populate the idp_userid column.

--- app/src/db/migrations/20221205215308_025-add-idp-guids.js
+++ app/src/db/migrations/20221205215308_025-add-idp-guids.js
@@ -2,7 +2,6 @@ exports.up = function(knex) {
   return Promise.resolve()
     .then(() => knex.schema.alterTable('user', table => {
       table.string('idpUserId').comment('The unique identifier provided by the identity provider');
-      table.string('idpUsername').comment('The username provided by the identity provider');
     }));
 };
 
@@ -10,6 +9,5 @@ exports.down = function(knex) {
   return Promise.resolve()
     .then(() => knex.schema.alterTable('user', table => {
       table.dropColumn('idpUserId');
-      table.dropColumn('idpUsername');
     }));
 };
