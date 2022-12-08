b5065bc8de069df5f3d73a99f632841070af13e8
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2022-12-07 17:47:34 -0800

    Replace keycloakId with idpUserId
    Replaced instances of keycloakId with idpUserId which is more reliable

--- app/frontend/src/store/modules/auth.js
+++ app/frontend/src/store/modules/auth.js
@@ -47,6 +47,8 @@ export default {
     isUser: (_state, getters) => getters.hasResourceRoles('chefs', ['user']),
     keycloakReady: () => Vue.prototype.$keycloak.ready,
     keycloakSubject: () => Vue.prototype.$keycloak.subject,
+    identityProviderIdentity: () =>
+      Vue.prototype.$keycloak.tokenParsed.idp_userid,
     moduleLoaded: () => !!Vue.prototype.$keycloak,
     realmAccess: () => Vue.prototype.$keycloak.tokenParsed.realm_access,
     redirectUri: (state) => state.redirectUri,
@@ -65,8 +67,8 @@ export default {
         public: !getters.authenticated
       };
       if (getters.authenticated) {
-        if (getters.tokenParsed.identity_provider_identity) {
-          user.username = getters.tokenParsed.identity_provider_identity;
+        if (getters.tokenParsed.idp_username) {
+          user.username = getters.tokenParsed.idp_username;
         } else {
           user.username = getters.tokenParsed.preferred_username;
         }
