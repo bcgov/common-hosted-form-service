9c20fd5ce3197536f5b53a4a289a1faa6ee30ac5
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2023-03-01 13:30:20 -0700

    fix response_headers_multiple_content_disposition error
    User reported this bug when downloading files with a comma in the file name.

--- app/src/forms/file/controller.js
+++ app/src/forms/file/controller.js
@@ -1,6 +1,7 @@
 const service = require('./service');
 const storageService = require('./storage/storageService');
-const {escapeSpecialCharacters} = require('../common/utils');
+
+const escapeCharacter = require('../common/escape-delimiters')('"',',',false);
 const _trim = (r) => {
   if (r) {
     // don't want storage information going over the wire...
@@ -37,9 +38,7 @@ module.exports = {
       stream.on('error', function error(err) {
         throw (err);
       });
-
-
-      res.setHeader('Content-Disposition', `attachment; filename=${escapeSpecialCharacters(fileStorage.originalName)}`);
+      res.setHeader('Content-Disposition', `attachment; filename=${escapeCharacter(fileStorage.originalName)}`);
       res.set('Content-Type', fileStorage.mimeType);
       res.set('Content-Length', fileStorage.size);
       res.set('Last-Modified', fileStorage.updatedAt);
