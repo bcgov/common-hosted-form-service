51eb8a2798212b6886aa82ed4e3600b05ac528a5
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2023-03-06 09:33:05 -0800

    fixed json versioning
    json export versioning included for front end users. documentation includes version. for json exports, if version is not specified or if version is 0 then it will export all versions.

---
+++ app/src/forms/common/models/views/submissionData.js
@@ -0,0 +1,41 @@
+const { Model } = require('objection');
+
+class SubmissionData extends Model {
+  static get tableName() {
+    return 'submissions_data_vw';
+  }
+
+  static get modifiers() {
+    return {
+      filterVersion(query, value) {
+        if (value && value !== 0) {
+          query.where('version', value);
+        }
+      },
+      filterCreatedAt(query, minDate, maxDate) {
+        if (minDate && maxDate) {
+          query.whereBetween('createdAt', [minDate, maxDate]);
+        } else if (minDate) {
+          query.where('createdAt', '>=', minDate);
+        } else if (maxDate) {
+          query.where('createdAt', '<=', maxDate);
+        }
+      },
+      filterDeleted(query, value) {
+        if (!value) {
+          query.where('deleted', false);
+        }
+      },
+      filterDrafts(query, value) {
+        if (!value) {
+          query.where('draft', false);
+        }
+      },
+      orderDefault(builder) {
+        builder.orderBy('createdAt', 'DESC');
+      },
+    };
+  }
+}
+
+module.exports = SubmissionData;
