ef8ee8959f6d8b6cec07c7509a44bf2833f7f76e
Sergey Fedyanin <s.fedyanin.bc@gmail.com>
2023-04-12 10:21:55 -0700

    fix: trying to fix unit tests

--- app/jest.config.js
+++ app/jest.config.js
@@ -9,4 +9,5 @@ module.exports = {
   testMatch: ['**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'],
   testPathIgnorePatterns: ['frontend'],
   testURL: 'http://localhost/',
+  transformIgnorePatterns: ['/node_modules/@json2csv'],
 };
