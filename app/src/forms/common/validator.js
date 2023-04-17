07d53d67f9bb8c073ae71e2a9f7465344a5eac7d
ptugger <prabhjot.tugger@cgi.com>
2023-04-17 16:55:16 -0400

    Added Validation to backend for Multi draft upload

---
+++ app/src/forms/common/validator.js
@@ -0,0 +1,18 @@
+const Validator = require('formio/src/resources/Validator.js');
+
+/**
+ * This function will be used to validate submission data
+ */
+
+function validate(data, schema) {
+  return new Promise((resolve, reject) => {
+    const validator = new Validator(schema);
+    validator.validate({ data }, (err) => {
+        resolve(err);
+    });
+  });
+}
+
+module.exports = {
+  validate
+};
