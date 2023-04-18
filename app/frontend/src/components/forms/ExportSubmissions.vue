da44a52d85afd9fac2e5470369e0dc19a10c9e86
Sergey Fedyanin <s.fedyanin.bc@gmail.com>
2023-04-18 14:53:04 -0700

    fix: console.log removed

--- app/frontend/src/components/forms/ExportSubmissions.vue
+++ app/frontend/src/components/forms/ExportSubmissions.vue
@@ -320,7 +320,6 @@ export default {
             : undefined;
 
         let emailExport = false;
-        console.log(this.formFields.length);
         if (
           this.submissionList.length > ExportLargeData.MAX_RECORDS ||
           this.formFields.length > ExportLargeData.MAX_FIELDS
