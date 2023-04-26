8a2557266d9637b39d029a9edb3c4c17275a1b7c
Sergey Fedyanin <s.fedyanin.bc@gmail.com>
2023-04-26 08:15:19 -0700

    feat: add message queue to file upload service

--- app/src/forms/file/service.js
+++ app/src/forms/file/service.js
@@ -1,39 +1,69 @@
 const config = require('config');
 const { v4: uuidv4 } = require('uuid');
 
+const Queue = require('bull');
+
 const { FileStorage } = require('../common/models');
 const storageService = require('./storage/storageService');
 
 const PERMANENT_STORAGE = config.get('files.permanent');
 
+// create our job queue
+const queue = new Queue('Upload files queue', { redis: { password: 'redispass' } });
+
 const service = {
   create: async (data, currentUser) => {
-    let trx;
-    try {
-      trx = await FileStorage.startTransaction();
+    const job = await queue.add('Upload files job', {
+      fileData: data,
+      user: currentUser,
+    });
 
-      const obj = {};
-      obj.id = uuidv4();
-      obj.storage = 'uploads';
-      obj.originalName = data.originalname;
-      obj.mimeType = data.mimetype;
-      obj.size = data.size;
-      obj.path = data.path;
-      obj.createdBy = currentUser.usernameIdp;
+    queue
+      .on('completed', function () {
+        console.log('Upload file Job completed...');
+      })
+      .on('failed', function () {
+        console.log('Upload file Job failed');
+      })
+      .on('progress', function (job, progress) {
+        console.log('\rUpload file job #' + job.id + ' ' + progress + '% complete');
+      });
 
-      const uploadResult = await storageService.upload(obj);
-      obj.path = uploadResult.path;
-      obj.storage = uploadResult.storage;
+    // process upload file jobs, 1 at a time.
+    queue.process('Upload files job', 1, async (job, done) => {
+      const data = job.data.fileData;
+      const currentUser = job.data.user;
 
-      await FileStorage.query(trx).insert(obj);
+      job.progress(0);
+      let trx;
+      try {
+        trx = await FileStorage.startTransaction();
 
-      await trx.commit();
-      const result = await service.read(obj.id);
-      return result;
-    } catch (err) {
-      if (trx) await trx.rollback();
-      throw err;
-    }
+        const obj = {};
+        obj.id = uuidv4();
+        obj.storage = 'uploads';
+        obj.originalName = data.originalname;
+        obj.mimeType = data.mimetype;
+        obj.size = data.size;
+        obj.path = data.path;
+        obj.createdBy = currentUser.usernameIdp;
+
+        const uploadResult = await storageService.upload(obj);
+        obj.path = uploadResult.path;
+        obj.storage = uploadResult.storage;
+
+        await FileStorage.query(trx).insert(obj);
+
+        await trx.commit();
+        const result = await service.read(obj.id);
+        job.progress(100);
+        return done(null, result);
+      } catch (err) {
+        if (trx) await trx.rollback();
+        return done(new Error(err));
+      }
+    });
+    console.log(job);
   },
 
   read: async (id) => {
