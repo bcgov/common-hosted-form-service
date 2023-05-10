2ff9222c0ac61c49ae2f36d2fe618d4c9dbd4e1c
Walter Moar <Walter.Moar@gov.bc.ca>
2023-05-10 14:21:08 -0700

    feat: FORMS-583 added metrics tab to admin page

--- app/frontend/src/components/admin/AdminPage.vue
+++ app/frontend/src/components/admin/AdminPage.vue
@@ -4,23 +4,33 @@
     <v-tab>Users</v-tab>
     <v-tab>Developer</v-tab>
     <v-tab data-cy="infoLinks">Info Links</v-tab>
+    <v-tab v-if="adminDashboardUrl">Metrics</v-tab>
 
     <v-tab-item> <AdminFormsTable /> </v-tab-item>
     <v-tab-item> <AdminUsersTable /> </v-tab-item>
     <v-tab-item> <Developer /> </v-tab-item>
     <v-tab-item> <FormComponentsProactiveHelp /> </v-tab-item>
+    <v-tab-item> <Dashboard :url="adminDashboardUrl" /> </v-tab-item>
   </v-tabs>
 </template>
 
 <script>
+import Vue from 'vue';
+
 export default {
   name: 'AdminPage',
   components: {
     AdminFormsTable: () => import('@/components/admin/AdminFormsTable.vue'),
     AdminUsersTable: () => import('@/components/admin/AdminUsersTable.vue'),
+    Dashboard: () => import('@/components/admin/Dashboard.vue'),
     Developer: () => import('@/components/admin/Developer.vue'),
     FormComponentsProactiveHelp: () =>
       import('@/components/admin/FormComponentsProactiveHelp.vue'),
+  },
+  data() {
+    return {
+      adminDashboardUrl: Vue.prototype.$config.adminDashboardUrl,
+    };
   },
 };
 </script>
