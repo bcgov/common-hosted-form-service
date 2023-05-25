c153f70cfdf8d91a62d209deff8dc785a4dd0169
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2023-05-25 13:55:49 -0700

    minor bug fixes and optimization
    removed console.log
    fixed tests

--- app/frontend/src/components/forms/PrintOptions.vue
+++ app/frontend/src/components/forms/PrintOptions.vue
@@ -185,7 +185,6 @@ export default {
             template: body,
             submission: this.submission,
           };
-          console.log(draftData);
           response = await utilsService.draftDocGen(draftData);
         }
 
