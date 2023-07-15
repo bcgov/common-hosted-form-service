31cdd85f7d1bfd5356ad2316fa2da04ad66b16a6
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2023-07-14 21:43:03 -0700

    working on alignment issue for rtl support

--- components/src/components/BCAddress/Component.form.ts
+++ components/src/components/BCAddress/Component.form.ts
@@ -1,5 +1,8 @@
 import baseEditForm from 'formiojs/components/_classes/component/Component.form';
 import AddressEditProvider from './editForm/Address.edit.provider';
+import AddressEditData from './editForm/Address.edit.data';
+import AddressEditDisplay from './editForm/Address.edit.display';
+
 
 export default function(...extend) {
     return baseEditForm([
@@ -8,6 +11,18 @@ export default function(...extend) {
             key: 'provider',
             weight: 150,
             components: AddressEditProvider,
+          },
+          {
+            label: 'Data',
+            key: 'data',
+            weight: 160,
+            components: AddressEditData,
+          },
+          {
+            label: 'Display',
+            key: 'display',
+            weight: 170,
+            components: AddressEditDisplay,
           },
     ], ...extend);
 }
