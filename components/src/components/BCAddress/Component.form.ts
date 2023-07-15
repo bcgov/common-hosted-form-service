d9995733c512acf69a3aa25127ca1a6bd110ccb6
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2023-07-14 21:51:44 -0700

    remove codes that I should not have commited

--- components/src/components/BCAddress/Component.form.ts
+++ components/src/components/BCAddress/Component.form.ts
@@ -1,8 +1,5 @@
 import baseEditForm from 'formiojs/components/_classes/component/Component.form';
 import AddressEditProvider from './editForm/Address.edit.provider';
-import AddressEditData from './editForm/Address.edit.data';
-import AddressEditDisplay from './editForm/Address.edit.display';
-
 
 export default function(...extend) {
     return baseEditForm([
@@ -11,18 +8,6 @@ export default function(...extend) {
             key: 'provider',
             weight: 150,
             components: AddressEditProvider,
-          },
-          {
-            label: 'Data',
-            key: 'data',
-            weight: 160,
-            components: AddressEditData,
-          },
-          {
-            label: 'Display',
-            key: 'display',
-            weight: 170,
-            components: AddressEditDisplay,
           },
     ], ...extend);
 }
