600173d08ce6df4af718f2ad5d751bc943a658b0
Ayobami O. Idowu <ayobami.o.idowu@gmail.com>
2023-08-07 20:16:07 -0700

    fix an error in the translation files

--- app/frontend/src/views/NotFound.vue
+++ app/frontend/src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="text-center">
-    <h1 class="my-6" :lang="lang">{{ $t('trans.notFound.pageNotFound') }}(</h1>
+    <h1 class="my-6" :lang="lang">{{ $t('trans.notFound.pageNotFound') }}</h1>
     <router-link :to="{ name: 'About' }">
       <v-btn color="primary" large>
         <v-icon left>home</v-icon>
