07fbeaa0d1d55530352f7fec0e0e0648a9dde2e5
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2023-10-30 13:28:05 -0700

    added support for GET /files

--- app/src/forms/file/routes.js
+++ app/src/forms/file/routes.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const controller = require('./controller');
+const apiAccess = require('../auth/middleware/apiAccess');
 
 const P = require('../common/constants').Permissions;
 const { currentFileRecord, hasFileCreate, hasFilePermissions } = require('./middleware/filePermissions');
@@ -13,7 +14,7 @@ routes.post('/', middleware.publicRateLimiter, hasFileCreate, fileUpload.upload,
   await controller.create(req, res, next);
 });
 
-routes.get('/:id', currentFileRecord, hasFilePermissions(P.SUBMISSION_READ), async (req, res, next) => {
+routes.get('/:id', apiAccess, currentFileRecord, hasFilePermissions(P.SUBMISSION_READ), async (req, res, next) => {
   await controller.read(req, res, next);
 });
 
