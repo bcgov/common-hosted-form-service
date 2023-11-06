7d6297cae1cd17687a53293c8536b4fbc9d6aeb4
Walter Moar <Walter.Moar@gov.bc.ca>
2023-11-06 10:17:48 -0800

    fix: FORMS-907 rate limit all api-key calls (#1139)
    * fix: FORMS-907 rate limit all api-key calls
    
    * fix: FORMS-907 remove unauthed rate limiting
    
    * fix: FORMS-907 restrict rate limiting to api
    
    * fix: FORMS-907 windowMs bug; headers version

--- app/src/forms/common/middleware/rateLimiter.js
+++ app/src/forms/common/middleware/rateLimiter.js
@@ -2,12 +2,18 @@ const config = require('config');
 const rateLimit = require('express-rate-limit');
 
 const apiKeyRateLimiter = rateLimit({
+  // Instead of legacy headers use the standardHeaders version defined below.
+  legacyHeaders: false,
+
   limit: config.get('server.rateLimit.public.max'),
 
   // Skip Bearer token auth so that CHEFS app users are not limited.
   skip: (req) => req.headers && req.headers.authorization && !req.headers.authorization.startsWith('Basic '),
 
-  windowMs: config.get('server.rateLimit.public.windowMs'),
+  // Use the latest draft of the IETF standard for rate limiting headers.
+  standardHeaders: 'draft-7',
+
+  windowMs: parseInt(config.get('server.rateLimit.public.windowMs')),
 });
 
 module.exports.apiKeyRateLimiter = apiKeyRateLimiter;
