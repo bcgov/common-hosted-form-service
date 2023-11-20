0c996eb1ea1ba9a2656927e1f7343f9ed9f12312
Sai-CITZ <sai.bannur@gov.bc.ca>
2023-11-20 14:16:18 -0800

    layout of file profile

---
+++ app/frontend/src/components/designer/FormProfile.vue
@@ -0,0 +1,32 @@
+<script>
+import { mapState } from 'pinia';
+import FormProductionLevel from '~/components/designer/profile/FormProductionLevel.vue';
+import BasePanel from '~/components/base/BasePanel.vue';
+import { useFormStore } from '~/store/form';
+
+export default {
+  components: {
+    BasePanel,
+    FormProductionLevel,
+  },
+  computed: {
+    ...mapState(useFormStore, ['isRTL', 'lang']),
+  },
+};
+</script>
+
+<template>
+  <BasePanel class="my-6">
+    <template #title>
+      <span :lang="lang">{{ $t('trans.create.formProfile') }}</span>
+    </template>
+
+    <v-container class="px-0" :class="{ 'dir-rtl': isRTL }">
+      <v-row>
+        <v-col cols="12" md="6">
+          <FormProductionLevel />
+        </v-col>
+      </v-row>
+    </v-container>
+  </BasePanel>
+</template>
---
+++ app/frontend/src/components/designer/profile/FormProductionLevel.vue
@@ -0,0 +1,30 @@
+<script>
+import { mapState } from 'pinia';
+import BasePanel from '~/components/base/BasePanel.vue';
+import { useFormStore } from '~/store/form';
+
+export default {
+  components: {
+    BasePanel,
+  },
+  computed: {
+    ...mapState(useFormStore, ['lang']),
+  },
+};
+</script>
+
+<template>
+  <BasePanel class="fill-height">
+    <template #title
+      ><span :lang="lang">{{
+        $t('trans.fileProfile.productionLevel')
+      }}</span></template
+    >
+
+    <v-radio-group>
+      <v-radio label="Development" value="Development"></v-radio>
+      <v-radio label="Test" value="Test"></v-radio>
+      <v-radio label="Production" value="Production"></v-radio>
+    </v-radio-group>
+  </BasePanel>
+</template>
