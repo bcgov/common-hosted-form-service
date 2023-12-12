47e09690ae941e35c70f1a18ad76251af93b5321
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2023-12-12 08:45:19 -0800

    Added BasicAuth and rate limiting
    Added BasicAuth and rate limiting to the document template render endpoint.
    
    Resolves: FORMS-937

--- app/src/forms/utils/routes.js
+++ app/src/forms/utils/routes.js
@@ -1,11 +1,13 @@
 const routes = require('express').Router();
 
+const apiAccess = require('../auth/middleware/apiAccess');
 const controller = require('../submission/controller');
 const { currentUser } = require('../auth/middleware/userAccess');
+const rateLimiter = require('../common/middleware').apiKeyRateLimiter;
 
 routes.use(currentUser);
 
-routes.post('/template/render', async (req, res, next) => {
+routes.post('/template/render', rateLimiter, apiAccess, async (req, res, next) => {
   await controller.draftTemplateUploadAndRender(req, res, next);
 });
 
