051c7207a0234856d915bfca33719794ed22d48b
Walter Moar <Walter.Moar@gov.bc.ca>
2024-01-09 14:48:12 -0800

    fix: FORMS-954 express update to handle proxied ip addresses (#1220)
    * fix: FORMS-954 express handle proxied ip addresses
    
    * fix: FORMS-954 express handle proxied ip addresses

--- app/app.js
+++ app/app.js
@@ -27,6 +27,11 @@ app.use(compression());
 app.use(express.json({ limit: config.get('server.bodyLimit') }));
 app.use(express.urlencoded({ extended: true }));
 
+// Express needs to know about the OpenShift proxy. With this setting Express
+// pulls the IP address from the headers, rather than use the proxy IP address.
+// See https://express-rate-limit.github.io/ERR_ERL_UNEXPECTED_X_FORWARDED_FOR
+app.set('trust proxy', 1);
+
 // Skip if running tests
 if (process.env.NODE_ENV !== 'test') {
   // Initialize connections and exit if unsuccessful
