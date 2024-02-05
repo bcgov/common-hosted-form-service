bc49c4488f86977f14c1684f1c1a8db3bb67431f
nimya-aot <nimya.john@aot-technologies.com>
2024-02-05 10:22:08 -0800

    #980 modification to cypress config

--- tests/functional/cypress.config.js
+++ tests/functional/cypress.config.js
@@ -1,7 +1,31 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  env: {
+    depEnv: 'app',
+    auth_base_url: 'http://localhost:8082',
+    auth_realm: 'chefs',
+    auth_client_id: 'chefs-frontend',
+    keycloakUrl: 'http://localhost:8082',
+    keycloakRealm: 'chefs',
+    keycloakClientId: 'chefs-frontend',
+    keycloakUsername: 'testuser@gmail.com',
+    keycloakPassword: 'testuser',
   },
-};
+  chromeWebSecurity: false,
+  video: false,
+  fixturesFolder: 'cypress/fixtures',
+  screenshotsFolder: 'screenshots',
+  videosFolder: 'videos',
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    //setupNodeEvents(on, config) {
+     // return require('./plugins/index.js')(on, config)
+    
+    baseUrl: 'http://localhost:5173',
+    specPattern: 'cypress/specs/*.cy.{js,jsx,ts,tsx}',
+    //supportFile: 'support/index.js',
+  }
+  },
+)
