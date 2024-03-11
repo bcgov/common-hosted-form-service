1d40fbabec7dff00bafb66162eba33ff3413ae37
Walter Moar <Walter.Moar@gov.bc.ca>
2024-03-11 16:26:28 +0000

    feat: FORMS-1042 new document templates backend

--- app/src/forms/form/service.js
+++ app/src/forms/form/service.js
@@ -5,6 +5,7 @@ const { EmailTypes } = require('../common/constants');
 const eventService = require('../event/eventService');
 const moment = require('moment');
 const {
+  DocumentTemplate,
   FileStorage,
   Form,
   FormApiKey,
@@ -260,6 +261,89 @@ const service = {
         form.schedule = checkIsFormExpired(form.schedule);
         return form;
       });
+  },
+
+  /**
+   * Creates a document template that can be used to generate a document from
+   * a form's submission data.
+   *
+   * @param {uuid} formId the identifier for the form.
+   * @param {object} data the data for the document template.
+   * @param {string} currentUsername the currently logged in user's username.
+   * @returns the created object.
+   */
+  documentTemplateCreate: async (formId, data, currentUsername) => {
+    let trx;
+
+    try {
+      const documentTemplate = {
+        id: uuidv4(),
+        formId: formId,
+        filename: data.filename,
+        template: data.template,
+        createdBy: currentUsername,
+      };
+
+      trx = await DocumentTemplate.startTransaction();
+      await DocumentTemplate.query(trx).insert(documentTemplate);
+      await trx.commit();
+
+      const result = await service.documentTemplateRead(documentTemplate.id);
+
+      return result;
+    } catch (error) {
+      if (trx) {
+        await trx.rollback();
+      }
+
+      throw error;
+    }
+  },
+
+  /**
+   * Deletes an active document template given its ID.
+   *
+   * @param {uuid} documentTemplateId the id of the document template.
+   * @param {string} currentUsername the currently logged in user's username.
+   * @throws an Error if the document template does not exist.
+   */
+  documentTemplateDelete: async (documentTemplateId, currentUsername) => {
+    let trx;
+    try {
+      trx = await DocumentTemplate.startTransaction();
+      await DocumentTemplate.query(trx).patchAndFetchById(documentTemplateId, {
+        active: false,
+        updatedBy: currentUsername,
+      });
+      await trx.commit();
+    } catch (error) {
+      if (trx) {
+        await trx.rollback();
+      }
+
+      throw error;
+    }
+  },
+
+  /**
+   * Gets the active document templates for a form.
+   *
+   * @param {uuid} formId the identifier for the form.
+   * @returns a Promise for the document templates belonging to a form.
+   */
+  documentTemplateList: (formId) => {
+    return DocumentTemplate.query().modify('filterFormId', formId).modify('filterActive', true);
+  },
+
+  /**
+   * Reads an active document template given its ID.
+   *
+   * @param {uuid} documentTemplateId the id of the document template.
+   * @returns a Promise for the document template.
+   * @throws an Error if the document template does not exist.
+   */
+  documentTemplateRead: (documentTemplateId) => {
+    return DocumentTemplate.query().findById(documentTemplateId).modify('filterActive', true).throwIfNotFound();
   },
 
   listFormSubmissions: async (formId, params) => {
