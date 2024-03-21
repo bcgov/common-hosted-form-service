d1edf098636fb5313b65d370fb0a765ad0d55a54
nimya-aot <nimya.john@aot-technologies.com>
2024-03-21 13:41:18 -0700

    #1059 updated the base url

--- tests/functional/cypress/e2e/about.cy.js
+++ tests/functional/cypress/e2e/about.cy.js
@@ -4,7 +4,8 @@ const baseUrl = Cypress.env('baseUrl');
 
 describe('Application About Page', () => {
   it('Visits the app about page', () => {
-    cy.visit(`/${depEnv}`);
+    //cy.visit(`/${depEnv}`);
+    cy.visit('https://chefs-dev.apps.silver.devops.gov.bc.ca/app/');
     cy.contains('h1', 'Create, publish forms, and receive submissions with the Common Hosted Forms Service.');
   });
 });
