bc987aff012b498d2a3187fc5b9d78c3b5a56070
Nimya John <78105113+nimya-aot@users.noreply.github.com>
2024-04-17 16:13:36 -0700

    Update form-design-basicfields.cy.js

--- tests/functional/cypress/e2e/form-design-basicfields.cy.js
+++ tests/functional/cypress/e2e/form-design-basicfields.cy.js
@@ -1,10 +1,6 @@
 import 'cypress-keycloak-commands';
 import 'cypress-drag-drop';
-<<<<<<< HEAD
-
-=======
 import { formsettings } from '../support/login.js';
->>>>>>> test/forms-1011
 
 const depEnv = Cypress.env('depEnv');
 
@@ -45,75 +41,9 @@ describe('Form Designer', () => {
   });
     
 // Verifying fields in the form settings page
-<<<<<<< HEAD
- it('Visits the form settings page', () => {
-  
-    cy.visit(`/${depEnv}/form/create`);
-    cy.location('pathname').should('eq', `/${depEnv}/form/create`);
-    //cy.contains('h1', 'Form Settings');
-    cy.get('.v-row > :nth-child(1) > .v-card > .v-card-title > span').contains('Form Title');
-
-    let title="title" + Math.random().toString(16).slice(2);
-
-    
-    cy.get('#input-15').type(title);
-    cy.get('#input-17').type('test description');
-    cy.get('#input-22').click();
-    cy.get('.v-selection-control-group > .v-card').should('be.visible');
-    cy.get('#input-23').click();
-    //cy.get('.v-selection-control-group > .v-card').should('not.be.visible');
-    //cy.get('#input-91').should('be.visible');
-    cy.get('.v-row > .v-input > .v-input__control > .v-selection-control-group > :nth-child(1) > .v-label > span').contains('IDIR');
-    cy.get('span').contains('Basic BCeID');
-    
-    cy.get(':nth-child(2) > .v-card > .v-card-text > .v-input--error > :nth-child(2)').contains('Please select 1 log-in type');
-    //cy.get('#input-92').should('be.visible');
-    //cy.get('#input-93').should('be.visible');
-
-
-    cy.get('#input-24').click();
-    
-
-    cy.get('#checkbox-25').click();
-    cy.get('#checkbox-28').click();
-    cy.get('#checkbox-38').click();
-    cy.get('#checkbox-50').click();
-    cy.get('#input-88').click();
-    cy.get('#input-88').type('abc@gmail.com');
-   
-   
-    
-   cy.get('#input-54').click();
-   cy.contains("Citizens' Services (CITZ)").click();
-   
-   cy.get('#input-58').click();
-
-   
-   cy.get('.v-list').should('contain','Applications that will be evaluated followed');
-   cy.get('.v-list').should('contain','Collection of Datasets, data submission');
-   cy.get('.v-list').should('contain','Registrations or Sign up - no evaluation');
-   cy.get('.v-list').should('contain','Reporting usually on a repeating schedule or event driven like follow-ups');
-   cy.get('.v-list').should('contain','Feedback Form to determine satisfaction, agreement, likelihood, or other qualitative questions');
-   cy.contains('Reporting usually on a repeating schedule or event driven like follow-ups').click();
-   cy.get('#input-64').click();
-   cy.get('#input-70').click();
-   cy.get('.mt-3 > .mdi-help-circle-outline').should('be.visible')
-   cy.get('.mt-3 > .mdi-help-circle-outline').click();
-   cy.get('.d-flex > .v-input > .v-input__control > .v-field > .v-field__field > .v-field__input').click();
-   cy.get('.d-flex > .v-input > .v-input__control > .v-field > .v-field__field > .v-field__input').type('test label');
-   cy.get('#checkbox-76').click();
-   cy.get('button').contains('Continue').click();
-
- 
-  // Form design page with simple textbox components
-
-  //cy.get('button').contains('Basic Layout').click();
-  
-=======
   it('DesignTextbox components', () => {
     cy.viewport(1000, 1100);
     cy.get('button').contains('Basic Fields').click();
->>>>>>> test/forms-1011
     let textFields = ["First Name", "Middle Name", "Last Name"];
 
     for(let i=0; i<textFields.length; i++) {
@@ -312,15 +242,7 @@ describe('Form Designer', () => {
         //cy.get('p').contains('Multi-line Text Component');
         cy.get('input[name="data[label]"]').clear().type('ID Number');
         cy.get('button').contains('Save').click();
-<<<<<<< HEAD
-    });
-    //cy.get('[name="data[middleName]"]').realHover('mouse');
-      //
-      //removeComponent "middle Name" 
-
-=======
       });
->>>>>>> test/forms-1011
       cy.get('[ref=removeComponent]').then($el => {
 
         const rem=$el[11];
