66e24e67da70314956b6e55e08ae220109ca9985
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2024-04-23 11:42:26 -0700

    updated approver role
    approver role has been added to the identity_provider table

--- app/src/db/migrations/20240423183912_045-add-approver-role.js
+++ app/src/db/migrations/20240423183912_045-add-approver-role.js
@@ -1,7 +1,7 @@
 const { v4: uuidv4 } = require('uuid');
 const { Permissions, Roles } = require('../../forms/common/constants');
 
-const CREATED_BY = 'migration-045';
+const USER = 'migration-045';
 
 /**
  * @param { import("knex").Knex } knex
@@ -12,7 +12,7 @@ exports.up = function (knex) {
     .then(() => {
       const items = [
         {
-          createdBy: CREATED_BY,
+          createdBy: USER,
           code: Roles.SUBMISSION_APPROVER,
           display: 'Approver',
           description: "Can review all form submissions but can't edit the submission.",
@@ -23,7 +23,7 @@ exports.up = function (knex) {
     .then(() => {
       const items = [
         {
-          createdBy: CREATED_BY,
+          createdBy: USER,
           code: Permissions.SUBMISSION_REVIEW,
           display: 'Submission Review',
           description: 'Can add/read notes and update the status of a submission.',
@@ -39,7 +39,7 @@ exports.up = function (knex) {
       [Permissions.FORM_READ, Permissions.SUBMISSION_READ, Permissions.SUBMISSION_REVIEW, Permissions.TEAM_READ].forEach((p) => {
         const item = {
           id: uuidv4(),
-          createdBy: CREATED_BY,
+          createdBy: USER,
           role: Roles.SUBMISSION_APPROVER,
           permission: p,
         };
@@ -52,7 +52,7 @@ exports.up = function (knex) {
     .then(() => {
       const rolePermssion = {
         id: uuidv4(),
-        createdBy: CREATED_BY,
+        createdBy: USER,
         role: Roles.OWNER,
         permission: Permissions.SUBMISSION_REVIEW,
       };
@@ -62,7 +62,7 @@ exports.up = function (knex) {
     .then(() => {
       const rolePermssion = {
         id: uuidv4(),
-        createdBy: CREATED_BY,
+        createdBy: USER,
         role: Roles.SUBMISSION_REVIEWER,
         permission: Permissions.SUBMISSION_REVIEW,
       };
@@ -71,14 +71,16 @@ exports.up = function (knex) {
     })
     .then(() => {
       const roles = [Roles.OWNER, Roles.TEAM_MANAGER, Roles.FORM_DESIGNER, Roles.SUBMISSION_REVIEWER, Roles.FORM_SUBMITTER, Roles.SUBMISSION_APPROVER];
-      knex('identity_provider').where({ code: 'idir' }).update({
+      return knex('identity_provider').where({ code: 'idir' }).update({
         roles: roles,
+        updatedBy: USER,
       });
     })
     .then(() => {
       const roles = [Roles.TEAM_MANAGER, Roles.SUBMISSION_REVIEWER, Roles.FORM_SUBMITTER, Roles.SUBMISSION_APPROVER];
-      knex('identity_provider').where({ code: 'bceid-business' }).update({
+      return knex('identity_provider').where({ code: 'bceid-business' }).update({
         roles: roles,
+        updatedBy: USER,
       });
     });
 };
@@ -93,32 +95,34 @@ exports.down = function (knex) {
       const roles = [Roles.OWNER, Roles.TEAM_MANAGER, Roles.FORM_DESIGNER, Roles.SUBMISSION_REVIEWER, Roles.FORM_SUBMITTER];
       knex('identity_provider').where({ code: 'idir' }).update({
         roles: roles,
+        updatedBy: USER,
       });
     })
     .then(() => {
       const roles = [Roles.TEAM_MANAGER, Roles.SUBMISSION_REVIEWER, Roles.FORM_SUBMITTER];
       knex('identity_provider').where({ code: 'bceid-business' }).update({
         roles: roles,
+        updatedBy: USER,
       });
     })
     .then(() => {
       knex('role_permission')
         .where({
-          createdBy: CREATED_BY,
+          createdBy: USER,
         })
         .del();
     })
     .then(() => {
       knex('permission')
         .where({
-          createdBy: CREATED_BY,
+          createdBy: USER,
         })
         .del();
     })
     .then(() => {
       knex('role')
         .where({
-          createdBy: CREATED_BY,
+          createdBy: USER,
         })
         .del();
     });
