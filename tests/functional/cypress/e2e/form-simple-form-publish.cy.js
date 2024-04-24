9cd4bff10346ce94b1127be1f09b0157ad86d85b
nimya-aot <nimya.john@aot-technologies.com>
2024-04-24 11:05:25 -0700

    Modifies test scripts

--- tests/functional/cypress/e2e/form-simple-form-publish.cy.js
+++ tests/functional/cypress/e2e/form-simple-form-publish.cy.js
@@ -1,5 +1,6 @@
 import 'cypress-keycloak-commands';
 import 'cypress-drag-drop';
+import { formsettings } from '../support/login.js';
 
 const depEnv = Cypress.env('depEnv');
 
@@ -16,10 +17,7 @@ describe('Form Designer', () => {
 
   beforeEach(()=>{
     
-    cy.viewport(1000, 1800);
-    cy.waitForLoad();
-    cy.kcLogout();
-    cy.kcLogin("user");
+    
     
     cy.on('uncaught:exception', (err, runnable) => {
       // Form.io throws an uncaught exception for missing projectid
@@ -28,70 +26,21 @@ describe('Form Designer', () => {
       return false;
     });
   });
+  it('Visits the form settings page', () => {
     
+    
+    cy.viewport(1000, 1100);
+    cy.waitForLoad();
+    
+    formsettings();
+    
+
+  });  
 // Verifying fields in the form settings page
- it('Visits the form settings page', () => {
-   
-    cy.visit(`/${depEnv}/form/create`);
-    cy.location('pathname').should('eq', `/${depEnv}/form/create`);
-    cy.contains('h1', 'Form Settings');
-    cy.get('.v-row > :nth-child(1) > .v-card > .v-card-title > span').contains('Form Title');
-
-    let title="title" + Math.random().toString(16).slice(2);
-
-    
-    cy.get('#input-15').type(title);
-    cy.get('#input-17').type('test description');
-    cy.get('#input-22').click();
-    cy.get('.v-selection-control-group > .v-card').should('be.visible');
-    cy.get('#input-23').click();
-    //cy.get('.v-selection-control-group > .v-card').should('not.be.visible');
-    //cy.get('#input-91').should('be.visible');
-    cy.get('.v-row > .v-input > .v-input__control > .v-selection-control-group > :nth-child(1) > .v-label > span').contains('IDIR');
-    cy.get('span').contains('Basic BCeID');
-    
-    cy.get(':nth-child(2) > .v-card > .v-card-text > .v-input--error > :nth-child(2)').contains('Please select 1 log-in type');
-    //cy.get('#input-92').should('be.visible');
-    //cy.get('#input-93').should('be.visible');
-
-
-    cy.get('#input-24').click();
-    
-
-    cy.get('#checkbox-25').click();
-    cy.get('#checkbox-28').click();
-    cy.get('#checkbox-38').click();
-    cy.get('#checkbox-50').click();
-    cy.get('#input-88').click();
-    cy.get('#input-88').type('abc@gmail.com');
-   
-   
-    
-   cy.get('#input-54').click();
-   cy.contains("Citizens' Services (CITZ)").click();
-   
-   cy.get('#input-58').click();
-
-   
-   cy.get('.v-list').should('contain','Applications that will be evaluated followed');
-   cy.get('.v-list').should('contain','Collection of Datasets, data submission');
-   cy.get('.v-list').should('contain','Registrations or Sign up - no evaluation');
-   cy.get('.v-list').should('contain','Reporting usually on a repeating schedule or event driven like follow-ups');
-   cy.get('.v-list').should('contain','Feedback Form to determine satisfaction, agreement, likelihood, or other qualitative questions');
-   cy.contains('Reporting usually on a repeating schedule or event driven like follow-ups').click();
-   cy.get('#input-64').click();
-   cy.get('#input-70').click();
-   cy.get('.mt-3 > .mdi-help-circle-outline').should('be.visible')
-   cy.get('.mt-3 > .mdi-help-circle-outline').click();
-   cy.get('.d-flex > .v-input > .v-input__control > .v-field > .v-field__field > .v-field__input').click();
-   cy.get('.d-flex > .v-input > .v-input__control > .v-field > .v-field__field > .v-field__input').type('test label');
-   cy.get('#checkbox-76').click();
-   cy.get('button').contains('Continue').click();
-
- 
-  // Form design page with simple textbox components
-
- 
+ it('Simple form submission', () => {
+    cy.viewport(1000, 1100);
+    cy.waitForLoad();
+    // Form design page with simple textbox components
     let textFields = ["First Name", "Middle Name", "Last Name"];
 
     for(let i=0; i<textFields.length; i++) {
