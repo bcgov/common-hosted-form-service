271e5c36f5d8722b4022f0b652bd5974d7cef9dd
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2024-05-13 10:31:04 -0700

    refactor: Forms 1118 bcgov composition api (#1344)
    * Update BCGovAlertBanner
    
    BCGovAlertBanner updated to the composition API and tests are fixed, added 100% coverage
    
    * Update BCGovFooter
    
    BCGovFooter updated to composition API. Was getting an infinite recursion issue caused by having a computed value inside of the BCGovFooter so I moved the formSubmitMode value to the parent template "App.vue".
    
    * Update BCGovHeader
    
    BCGovHeader has been updated to the composition API and the computed values were moved to its parent template "App.vue" to fix an infinite recursion issue.
    
    * Update BCGovNavBar
    
    BCGovNavBar has been updated to the composition API. Removed the hideNavBar computed method as it's no different from formSubmitMode. Passed it in just like the BCGovHeader and BCGovFooter because of infinite recursion.
    
    * Update BCGovHeader.spec.js
    
    BCGovHeader test has been fixed, it checks for the app title. The app title is passed to it from App.js
    Therefore, App.spec.js has been updated to test for it. Coverage is at 100% for both BCGovHeader and App.
    
    ---------
    
    Co-authored-by: Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>

--- app/frontend/tests/unit/components/bcgov/BCGovHeader.spec.js
+++ app/frontend/tests/unit/components/bcgov/BCGovHeader.spec.js
@@ -1,28 +1,27 @@
 import { setActivePinia, createPinia } from 'pinia';
 import { mount } from '@vue/test-utils';
 import { describe, expect, it } from 'vitest';
-import { createRouter, createWebHistory } from 'vue-router';
 
 import BCGovHeader from '~/components/bcgov/BCGovHeader.vue';
-import getRouter from '~/router';
 import { useAuthStore } from '~/store/auth';
 
 describe('BCGovHeader.vue', () => {
   const pinia = createPinia();
   setActivePinia(pinia);
   const authStore = useAuthStore();
-  const router = createRouter({
-    history: createWebHistory(),
-    routes: getRouter().getRoutes(),
-  });
 
-  it('renders', () => {
+  it('if formSubmitMode is false then show the app title', () => {
+    const APP_TITLE = 'This is an app title';
     // set keycloak ready to true, required for BaseAuthButton
     // to be visible
     authStore.ready = true;
     const wrapper = mount(BCGovHeader, {
+      props: {
+        formSubmitMode: false,
+        appTitle: APP_TITLE,
+      },
       global: {
-        plugins: [router, pinia],
+        plugins: [pinia],
         stubs: {
           BaseAuthButton: true,
           BaseInternationalization: true,
@@ -32,11 +31,35 @@ describe('BCGovHeader.vue', () => {
 
     const btnHeaderTitle = wrapper.find('[data-test="btn-header-title"]');
     expect(btnHeaderTitle.exists()).toBeTruthy();
-    expect(btnHeaderTitle.text()).toEqual(
-      router?.currentRoute?.value?.meta?.title
-        ? router.currentRoute.value.meta.title
-        : ''
-    );
+    expect(btnHeaderTitle.text()).toContain(APP_TITLE);
+    expect(wrapper.find('[data-test="base-auth-btn"]').exists()).toBeTruthy();
+    expect(
+      wrapper.find('[data-test="base-internationalization"]').exists()
+    ).toBeTruthy();
+  });
+
+  it('if formSubmitMode is true then do not show the app title', () => {
+    const APP_TITLE = 'This is an app title';
+    // set keycloak ready to true, required for BaseAuthButton
+    // to be visible
+    authStore.ready = true;
+    const wrapper = mount(BCGovHeader, {
+      props: {
+        formSubmitMode: true,
+        appTitle: APP_TITLE,
+      },
+      global: {
+        plugins: [pinia],
+        stubs: {
+          BaseAuthButton: true,
+          BaseInternationalization: true,
+        },
+      },
+    });
+
+    const btnHeaderTitle = wrapper.find('[data-test="btn-header-title"]');
+    expect(btnHeaderTitle.exists()).toBeFalsy();
+    expect(wrapper.html()).not.toContain(APP_TITLE);
     expect(wrapper.find('[data-test="base-auth-btn"]').exists()).toBeTruthy();
     expect(
       wrapper.find('[data-test="base-internationalization"]').exists()
