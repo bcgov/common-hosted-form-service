746ce72730b48b776b513117fae29a162705460c
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2024-05-13 10:58:10 -0700

    refactor: Forms 1119 profile settings composition api (#1343)
    * Update FormLabelProfile
    
    FormLabelProfile has some test coverage
    
    * Update FormAccessSettings
    
    FormAccessSettings has been updated to the composition API and a test file has been created, giving it 100% coverage
    
    * Update FormFunctionalitySettings
    
    FormFunctionalitySettings has been updated to the composition API and has an added test file with 100% coverage
    
    * Update FormGeneralSettings.vue
    
    FormGeneralSettings has been updated to the composition API
    
    * Update FormSubmissionSettings
    
    FormSubmissionSettings has been updated to the composition API.
    
    * Update FormScheduleSettings
    
    FormScheduleSettings has been updated to the composition API and a test file has been created for it. It is close to 100% coverage, it just needs a few cases for AVAILABLE_DATES and AVAILABLE_PERIOD_OPTIONS
    
    * Fix getAvailableDates
    
    The transformUtils function getAvailableDates that is supposed to grab available dates for a submission period would not always return the same value, as created tests showed. I've rewritten the functions with better documentation and a refactored input. It should still output the same values but it is unknown as the previous documentation for this feature does not actually specify the inputs/outputs nor were there any tests.
    
    ---------
    
    Co-authored-by: Vijaivir Dhaliwal <91633223+vijaivir@users.noreply.github.com>

---
+++ app/frontend/tests/unit/components/designer/settings/FormFunctionalitySettings.spec.js
@@ -0,0 +1,224 @@
+import { createTestingPinia } from '@pinia/testing';
+import { flushPromises, mount } from '@vue/test-utils';
+import { setActivePinia } from 'pinia';
+import { beforeEach, describe, expect, it } from 'vitest';
+import { ref } from 'vue';
+
+import { useAuthStore } from '~/store/auth';
+import { useFormStore } from '~/store/form';
+import { useIdpStore } from '~/store/identityProviders';
+import FormFunctionalitySettings from '~/components/designer/settings/FormFunctionalitySettings.vue';
+import { FormRoleCodes, AppPermissions } from '~/utils/constants';
+
+const IDIR = {
+  active: true,
+  login: true,
+  code: 'idir',
+  display: 'IDIR',
+  idp: 'idir',
+  permissions: [
+    AppPermissions.VIEWS_FORM_STEPPER,
+    AppPermissions.VIEWS_ADMIN,
+    AppPermissions.VIEWS_FILE_DOWNLOAD,
+    AppPermissions.VIEWS_FORM_EMAILS,
+    AppPermissions.VIEWS_FORM_EXPORT,
+    AppPermissions.VIEWS_FORM_MANAGE,
+    AppPermissions.VIEWS_FORM_PREVIEW,
+    AppPermissions.VIEWS_FORM_SUBMISSIONS,
+    AppPermissions.VIEWS_FORM_TEAMS,
+    AppPermissions.VIEWS_FORM_VIEW,
+    AppPermissions.VIEWS_USER_SUBMISSIONS,
+  ],
+  primary: true,
+  roles: [
+    FormRoleCodes.OWNER,
+    FormRoleCodes.TEAM_MANAGER,
+    FormRoleCodes.FORM_DESIGNER,
+    FormRoleCodes.SUBMISSION_REVIEWER,
+    FormRoleCodes.FORM_SUBMITTER,
+  ],
+  tokenmap: {
+    idp: 'identity_provider',
+    email: 'email',
+    fullName: 'name',
+    lastName: 'family_name',
+    username: 'idir_username',
+    firstName: 'given_name',
+    idpUserId: 'idir_user_guid',
+    keycloakId: 'idir_user_guid',
+  },
+};
+
+const BCEIDBASIC = {
+  active: true,
+  login: true,
+  code: 'bceid-basic',
+  display: 'Basic BCeID',
+  idp: 'bceidbasic',
+  permissions: [AppPermissions.VIEWS_USER_SUBMISSIONS],
+  primary: false,
+  roles: [FormRoleCodes.FORM_SUBMITTER],
+  tokenmap: {
+    idp: 'identity_provider',
+    email: 'email',
+    fullName: 'name',
+    lastName: null,
+    username: 'bceid_username',
+    firstName: null,
+    idpUserId: 'bceid_user_guid',
+    keycloakId: 'bceid_user_guid',
+  },
+};
+
+const BCEIDBUSINESS = {
+  active: true,
+  login: true,
+  code: 'bceid-business',
+  display: 'Business BCeID',
+  idp: 'bceidbusiness',
+  permissions: [
+    AppPermissions.VIEWS_FORM_EXPORT,
+    AppPermissions.VIEWS_FORM_MANAGE,
+    AppPermissions.VIEWS_FORM_SUBMISSIONS,
+    AppPermissions.VIEWS_FORM_TEAMS,
+    AppPermissions.VIEWS_FORM_VIEW,
+    AppPermissions.VIEWS_USER_SUBMISSIONS,
+  ],
+  primary: false,
+  roles: [
+    FormRoleCodes.TEAM_MANAGER,
+    FormRoleCodes.SUBMISSION_REVIEWER,
+    FormRoleCodes.FORM_SUBMITTER,
+  ],
+  tokenmap: {
+    idp: 'identity_provider',
+    email: 'email',
+    fullName: 'name',
+    lastName: null,
+    username: 'bceid_username',
+    firstName: null,
+    idpUserId: 'bceid_user_guid',
+    keycloakId: 'bceid_user_guid',
+  },
+};
+
+describe('FormFunctionalitySettings.vue', () => {
+  const pinia = createTestingPinia();
+  setActivePinia(pinia);
+
+  const authStore = useAuthStore(pinia);
+  const formStore = useFormStore(pinia);
+  const idpStore = useIdpStore(pinia);
+
+  beforeEach(() => {
+    authStore.$reset();
+    formStore.$reset();
+    idpStore.$reset();
+
+    authStore.identityProvider = ref({
+      code: 'idir',
+    });
+    formStore.form = ref({
+      schedule: {
+        enabled: false,
+      },
+      subscribe: {
+        enabled: false,
+      },
+      enableSubmitterDraft: false,
+      allowSubmitterToUploadFile: true,
+    });
+    idpStore.providers = [IDIR, BCEIDBASIC, BCEIDBUSINESS];
+  });
+
+  it('renders and displays form functionality settings', () => {
+    const wrapper = mount(FormFunctionalitySettings, {
+      global: {
+        plugins: [pinia],
+        stubs: {
+          BaseInfoCard: {
+            name: 'BaseInfoCard',
+            template: '<div class="base-info-card-stub"><slot /></div>',
+          },
+          BasePanel: {
+            name: 'BasePanel',
+            template: '<div class="base-panel-stub"><slot /></div>',
+          },
+        },
+      },
+    });
+
+    expect(wrapper.text()).toMatch(
+      'trans.formSettings.canSaveAndEditDraftLabel'
+    );
+    expect(wrapper.text()).toMatch(
+      'trans.formSettings.canUpdateStatusAsReviewer'
+    );
+    expect(wrapper.text()).toMatch('trans.formSettings.allowMultiDraft');
+    expect(wrapper.text()).toMatch(
+      'trans.formSettings.formSubmissinScheduleMsg'
+    );
+    expect(wrapper.text()).toMatch(
+      'trans.formSettings.submitterCanCopyExistingSubmissn'
+    );
+    expect(wrapper.text()).toMatch('trans.formSettings.allowEventSubscription');
+    expect(wrapper.text()).toMatch('trans.formSettings.wideFormLayout');
+  });
+
+  it('submitters can not upload files if drafts are disabled', async () => {
+    formStore.form.enableSubmitterDraft = true;
+    const wrapper = mount(FormFunctionalitySettings, {
+      global: {
+        plugins: [pinia],
+        stubs: {
+          BaseInfoCard: {
+            name: 'BaseInfoCard',
+            template: '<div class="base-info-card-stub"><slot /></div>',
+          },
+          BasePanel: {
+            name: 'BasePanel',
+            template: '<div class="base-panel-stub"><slot /></div>',
+          },
+        },
+      },
+    });
+
+    expect(formStore.form.enableSubmitterDraft).toBeTruthy();
+    expect(formStore.form.allowSubmitterToUploadFile).toBeTruthy();
+
+    formStore.form.enableSubmitterDraft = false;
+
+    wrapper.vm.enableSubmitterDraftChanged();
+
+    expect(formStore.form.allowSubmitterToUploadFile).toBeFalsy();
+  });
+
+  it('if the submitter is allowed to upload files and drafts are disabled then enable drafts', async () => {
+    formStore.form.enableSubmitterDraft = true;
+    const wrapper = mount(FormFunctionalitySettings, {
+      global: {
+        plugins: [pinia],
+        stubs: {
+          BaseInfoCard: {
+            name: 'BaseInfoCard',
+            template: '<div class="base-info-card-stub"><slot /></div>',
+          },
+          BasePanel: {
+            name: 'BasePanel',
+            template: '<div class="base-panel-stub"><slot /></div>',
+          },
+        },
+      },
+    });
+
+    expect(formStore.form.enableSubmitterDraft).toBeTruthy();
+    expect(formStore.form.allowSubmitterToUploadFile).toBeTruthy();
+
+    formStore.form.enableSubmitterDraft = false;
+
+    wrapper.vm.allowSubmitterToUploadFileChanged();
+
+    expect(formStore.form.enableSubmitterDraft).toBeTruthy();
+    expect(formStore.form.allowSubmitterToUploadFile).toBeTruthy();
+  });
+});
