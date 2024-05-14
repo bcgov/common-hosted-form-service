58b270ee4352eb1b5653d407febff8a8ca63474c
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2024-05-14 16:47:59 -0700

    fixed unpublish issue

--- app/frontend/src/components/forms/manage/ManageVersions.vue
+++ app/frontend/src/components/forms/manage/ManageVersions.vue
@@ -164,6 +164,10 @@ export default {
               id: item.id,
               isDraft: item.isDraft,
             };
+            const idx = this.drafts.map((d) => d.id).indexOf(item.id);
+            if (idx !== -1) {
+              this.drafts[idx].published = true;
+            }
             document.documentElement.style.overflow = 'hidden';
             this.showPublishDialog = true;
           }
