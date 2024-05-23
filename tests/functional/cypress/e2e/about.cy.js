c82dfff5bf7878fbdedfcd08b2ed448aa60c351a
nimya-aot <nimya.john@aot-technologies.com>
2024-05-23 09:15:23 -0700

    Updated scripts according to PR review

--- tests/functional/cypress/e2e/about.cy.js
+++ tests/functional/cypress/e2e/about.cy.js
@@ -4,8 +4,7 @@ const baseUrl = Cypress.env('baseUrl');
 
 describe('Application About Page', () => {
   it('Visits the app about page', () => {
-    //cy.visit(`/${depEnv}`);
-    cy.visit('https://chefs-dev.apps.silver.devops.gov.bc.ca/app/');
+    cy.visit(`/${depEnv}`);
     cy.contains('h1', 'Create, publish forms, and receive submissions with the Common Hosted Forms Service.');
   });
 });
