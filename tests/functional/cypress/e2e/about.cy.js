e95b1441d9dcc401ab20c31b13d4c9ad7bf6eb1e
nimya-aot <nimya.john@aot-technologies.com>
2024-05-27 09:37:10 -0700

    Updated to take default env variable

--- tests/functional/cypress/e2e/about.cy.js
+++ tests/functional/cypress/e2e/about.cy.js
@@ -2,10 +2,20 @@
 const depEnv = Cypress.env('depEnv');
 const baseUrl = Cypress.env('baseUrl');
 
+
 describe('Application About Page', () => {
   it('Visits the app about page', () => {
-    //cy.visit(`/${depEnv}`);
-    cy.visit('https://chefs-dev.apps.silver.devops.gov.bc.ca/app/');
-    cy.contains('h1', 'Create, publish forms, and receive submissions with the Common Hosted Forms Service.');
+    
+    if(depEnv=="")
+    {
+        cy.visit(`/app`);
+    }
+    else
+    {
+
+      cy.visit(`/pr-${depEnv}`);
+      cy.contains('h1', 'Create, publish forms, and receive submissions with the Common Hosted Forms Service.');
+    }
+    
   });
 });
