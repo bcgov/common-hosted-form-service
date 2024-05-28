38ca74aa3877c347d6b6c3d35e669697f6e61b79
nimya-aot <nimya.john@aot-technologies.com>
2024-05-27 23:33:35 -0700

    updated

--- tests/functional/cypress/e2e/about.cy.js
+++ tests/functional/cypress/e2e/about.cy.js
@@ -19,7 +19,7 @@ describe('Application About Page', () => {
       
       cy.visit(`/${depEnv}`);
       cy.contains('Create, publish forms, and receive submissions with the Common Hosted Forms Service.').should('be.visible');
-      
+      cy.get('[data-test="base-auth-btn"] > .v-btn > .v-btn__content > span').click();
     }
     
   });
