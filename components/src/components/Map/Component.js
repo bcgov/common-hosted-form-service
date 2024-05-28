9c7733e19b48b6c5a320ed7d0771fca7d10a88e5
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2024-05-28 15:16:24 -0700

    Init Leaflet Component

--- components/src/components/Map/Component.js
+++ components/src/components/Map/Component.js
@@ -1,5 +1,11 @@
 import { Components } from 'formiojs';
 const FieldComponent = Components.components.field;
+import L from "leaflet"
+import "leaflet-draw"
+import 'leaflet/dist/leaflet.css';
+import "leaflet-draw/dist/leaflet.draw-src.css";
+
+const CENTER = [48.41938669910753, -123.37030649185182]
 
 class MapComponent extends FieldComponent {
     static schema(...extend) {
@@ -24,28 +30,39 @@ class MapComponent extends FieldComponent {
     render() {
         return super.render(        
         `
-        <div>
-            <script>
-                SMK.INIT( {
-                    containerSel: '#smk-map-frame'
-                } )
-            </script>
-        </div>
+        <div id="mapContainer" style="height:400px;"></div>
         
         `
         )
     }
     attach(element) {
-        super.attach(element);
+        const superAttach = super.attach(element);
         this.loadMap();
+        return superAttach
     }
     loadMap() {
-        const mapContainer = document.getElementById('map');
-        if (mapContainer) {
-            console.log('inside Map');
-            //   SMK.init({ container: 'map' }).then((smk) => {
-            //     // Additional SMK configuration goes here
-            //   });
+        const mapContainer = document.getElementById("mapContainer");
+        if(mapContainer){
+            const map = L.map(mapContainer,{drawControl:true}).setView(CENTER, 13);
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+              }).addTo(map);
+            var drawnItems = new L.FeatureGroup()
+            var drawControl = new L.Control.Draw({
+                edit:{
+                    featureGroup: drawnItems
+                }
+            })
+            map.addControl(drawControl)
+            
+            //event listener
+            map.on('draw:created', function(e){
+                console.log(e)
+                var type = e.layerType
+                var layer = e.layer
+
+                drawnItems.addLayer(layer)
+            })
         }
     }
 
