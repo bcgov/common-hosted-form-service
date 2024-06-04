bc951742c16875b27643cbe78fcae75e0a7d086b
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2024-06-04 00:28:01 -0700

    EmailTemplate updated
    EmailTemplate has been updated to the composition API and tests have been created for it

---
+++ app/frontend/tests/unit/components/forms/manage/EmailTemplate.spec.js
@@ -0,0 +1,102 @@
+import { createTestingPinia } from '@pinia/testing';
+import { flushPromises, mount } from '@vue/test-utils';
+import { setActivePinia } from 'pinia';
+import { beforeEach, vi } from 'vitest';
+import { ref } from 'vue';
+
+import EmailTemplate from '~/components/forms/manage/EmailTemplate.vue';
+import { useFormStore } from '~/store/form';
+import { useNotificationStore } from '~/store/notification';
+
+const STUBS = {};
+
+describe('EmailTemplate.vue', () => {
+  let pinia = createTestingPinia();
+  setActivePinia(pinia);
+
+  const formStore = useFormStore(pinia);
+  const notificationStore = useNotificationStore(pinia);
+
+  beforeEach(() => {
+    formStore.$reset();
+
+    formStore.emailTemplates = ref([
+      {
+        body: 'Thank you for your {{ form.name }} submission. You can view your submission details by visiting the following links:',
+        formId: '8e436682-35be-4a93-82bc-08408d8fa606',
+        subject: '{{ form.name }} Accepted',
+        title: '{{ form.name }} Accepted',
+        type: 'submissionConfirmation',
+      },
+    ]);
+  });
+
+  it('renders', () => {
+    const wrapper = mount(EmailTemplate, {
+      props: {
+        title: 'Test title',
+        type: 'submissionConfirmation',
+      },
+      global: {
+        plugins: [pinia],
+        stubs: STUBS,
+      },
+    });
+
+    expect(wrapper.text()).toContain('Test title');
+    expect(wrapper.text()).toContain('trans.emailTemplate.subject');
+    expect(wrapper.text()).toContain('trans.emailTemplate.title');
+    expect(wrapper.text()).toContain('trans.emailTemplate.body');
+    expect(wrapper.text()).toContain('trans.emailTemplate.save');
+  });
+
+  it('saveEmailTemplate succeeds', async () => {
+    const addNotificationSpy = vi.spyOn(notificationStore, 'addNotification');
+    const updateEmailTemplateSpy = vi.spyOn(formStore, 'updateEmailTemplate');
+    updateEmailTemplateSpy.mockImplementation(async () => {
+      return {};
+    });
+    const wrapper = mount(EmailTemplate, {
+      props: {
+        title: 'Test title',
+        type: 'submissionConfirmation',
+      },
+      global: {
+        plugins: [pinia],
+        stubs: STUBS,
+      },
+    });
+
+    await wrapper.vm.saveEmailTemplate();
+
+    await flushPromises();
+
+    expect(updateEmailTemplateSpy).toHaveBeenCalledTimes(1);
+    expect(addNotificationSpy).toHaveBeenCalledTimes(0);
+  });
+
+  it('saveEmailTemplate shows a notification on failure', async () => {
+    const addNotificationSpy = vi.spyOn(notificationStore, 'addNotification');
+    const updateEmailTemplateSpy = vi.spyOn(formStore, 'updateEmailTemplate');
+    updateEmailTemplateSpy.mockImplementation(async () => {
+      throw new Error();
+    });
+    const wrapper = mount(EmailTemplate, {
+      props: {
+        title: 'Test title',
+        type: 'submissionConfirmation',
+      },
+      global: {
+        plugins: [pinia],
+        stubs: STUBS,
+      },
+    });
+
+    await wrapper.vm.saveEmailTemplate();
+
+    await flushPromises();
+
+    expect(updateEmailTemplateSpy).toHaveBeenCalledTimes(1);
+    expect(addNotificationSpy).toHaveBeenCalledTimes(1);
+  });
+});
