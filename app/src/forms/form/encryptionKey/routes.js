56ef27f52dc018f8d4e41fa82ea55abe27e662a2
Jason Sherman <tools@usingtechnolo.gy>
2024-08-15 12:07:02 -0700

    sigh - cut/paste error... fix
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- app/src/forms/form/encryptionKey/routes.js
+++ app/src/forms/form/encryptionKey/routes.js
@@ -2,8 +2,8 @@ const routes = require('express').Router();
 const { currentUser, hasFormPermissions } = require('../../auth/middleware/userAccess');
 const validateParameter = require('../../common/middleware/validateParameter');
 const { featureFlags } = require('../../../components/featureFlags');
-const apiAccess = require('../auth/middleware/apiAccess');
-const rateLimiter = require('../common/middleware').apiKeyRateLimiter;
+const apiAccess = require('../../auth/middleware/apiAccess');
+const rateLimiter = require('../../common/middleware').apiKeyRateLimiter;
 const P = require('../../common/constants').Permissions;
 
 const controller = require('./controller');
