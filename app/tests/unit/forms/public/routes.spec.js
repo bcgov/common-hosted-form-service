3b27eb98289533548420010023fe7cad249982d5
Walter Moar <Walter.Moar@gov.bc.ca>
2024-08-20 22:25:02 +0000

    fix: FORMS-1448 new middleware for reminder route

--- app/tests/unit/forms/public/routes.spec.js
+++ app/tests/unit/forms/public/routes.spec.js
@@ -4,6 +4,16 @@ const uuid = require('uuid');
 const { expressHelper } = require('../../../common/helper');
 
 const controller = require('../../../../src/forms/public/controller');
+const apiAccess = require('../../../../src/forms/public/middleware/apiAccess');
+
+//
+// Mock out all the middleware - we're testing that the routes are set up
+// correctly, not the functionality of the middleware.
+//
+
+apiAccess.checkApiKey = jest.fn((_req, _res, next) => {
+  next();
+});
 
 //
 // Create the router and a simple Express server.
@@ -18,10 +28,6 @@ afterEach(() => {
   jest.clearAllMocks();
 });
 
-// TODO - move code out of the route and into middleware or the controller. The
-// tests should continue to pass, but should themselves be moved into the
-// middleware or controller tests.
-
 describe(`${basePath}/reminder`, () => {
   const path = `${basePath}/reminder`;
 
@@ -32,46 +38,9 @@ describe(`${basePath}/reminder`, () => {
   it('200s when the APITOKEN matches apiKey', async () => {
     process.env.APITOKEN = uuid.v4();
 
-    const response = await appRequest.get(path).set({ apikey: process.env.APITOKEN });
+    await appRequest.get(path).set({ apikey: process.env.APITOKEN });
 
+    expect(apiAccess.checkApiKey).toBeCalledTimes(1);
     expect(controller.sendReminderToSubmitter).toBeCalledTimes(1);
-    expect(response.status).toBe(200);
-  });
-
-  // This is testing some strange code. Refactor the code and get rid of this.
-  it('401s when there is no APITOKEN or apiKey', async () => {
-    process.env.APITOKEN = '';
-
-    const response = await appRequest.get(path).set({ apikey: process.env.APITOKEN });
-
-    expect(controller.sendReminderToSubmitter).toBeCalledTimes(0);
-    expect(response.status).toBe(401);
-  });
-
-  it('401s when the apiKey is empty', async () => {
-    process.env.APITOKEN = uuid.v4();
-
-    const response = await appRequest.get(path).set({ apikey: '' });
-
-    expect(controller.sendReminderToSubmitter).toBeCalledTimes(0);
-    expect(response.status).toBe(401);
-  });
-
-  it('401s when the apiKey exists but does not match', async () => {
-    process.env.APITOKEN = uuid.v4();
-
-    const response = await appRequest.get(path).set({ apikey: uuid.v4() });
-
-    expect(controller.sendReminderToSubmitter).toBeCalledTimes(0);
-    expect(response.status).toBe(401);
-  });
-
-  // Ideally the code should be refactored to not create the 404 and let Express
-  // do it. Then this test should still pass.
-  it('404s on a non-GET', async () => {
-    const response = await appRequest.post(path);
-
-    expect(controller.sendReminderToSubmitter).toBeCalledTimes(0);
-    expect(response.status).toBe(404);
   });
 });
