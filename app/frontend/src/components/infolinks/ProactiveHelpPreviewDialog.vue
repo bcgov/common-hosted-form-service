fdcad3db078058b839e3a8d210defc8d33db2825
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2024-09-03 13:05:03 -0700

    Update ProactiveHelpPreviewDialog.vue
    Cleaned up the ProactiveHelpPreviewDialog to use more of the vuetify components if we can.

--- app/frontend/src/components/infolinks/ProactiveHelpPreviewDialog.vue
+++ app/frontend/src/components/infolinks/ProactiveHelpPreviewDialog.vue
@@ -29,111 +29,69 @@ defineExpose({ onCloseDialog });
 </script>
 
 <template>
-  <v-row justify="center" class="mb-5">
-    <v-dialog
-      v-model="dialog"
-      :width="fcProactiveHelpImageUrl ? '70%' : '40%'"
-      @click:outside="onCloseDialog"
-    >
-      <v-card>
-        <v-container class="overflow-auto">
-          <v-row>
-            <v-col class="d-flex justify-space-between headerWrapper pa-4">
-              <div class="align-self-center">
-                {{ component && component.componentName }}
-              </div>
-              <div class="align-self-center cursor">
-                <v-icon icon="mdi:mdi-close" @click="onCloseDialog" />
-              </div>
-            </v-col>
-          </v-row>
-          <v-row v-if="fcProactiveHelpImageUrl" class="mt-6">
-            <v-col md="6">
-              <div
-                ref="preview_text_field"
-                class="text"
-                data-cy="preview_text_field"
-              >
-                {{ component && component.description }}
-              </div>
-            </v-col>
-            <v-col md="6">
+  <v-dialog
+    v-model="dialog"
+    :width="fcProactiveHelpImageUrl ? '70%' : '40%'"
+    @click:outside="onCloseDialog"
+  >
+    <template #default>
+      <div class="content">
+        <v-toolbar density="compact" color="primary">
+          <v-toolbar-title :lang="locale">{{
+            component && component.componentName
+          }}</v-toolbar-title>
+          <v-btn icon
+            ><v-icon icon="mdi:mdi-close" @click="onCloseDialog"></v-icon
+          ></v-btn>
+        </v-toolbar>
+        <v-row class="flex-nowrap">
+          <v-col
+            class="flex-grow-1 flex-shrink-0 ma-2"
+            style="min-width: 100px; max-width: 100%"
+            cols="1"
+            ><div
+              ref="preview_text_field"
+              class="ma-2 pa-2"
+              data-cy="preview_text_field"
+            >
+              {{ component && component.description }}
+            </div></v-col
+          >
+          <v-col
+            v-if="fcProactiveHelpImageUrl"
+            class="flex-grow-0 flex-shrink-0"
+            cols="7"
+            ><div class="ma-2 pa-2">
               <v-img
                 data-cy="preview_image_field"
                 :src="fcProactiveHelpImageUrl"
               ></v-img>
-            </v-col>
-          </v-row>
-          <v-row v-else class="mt-6">
-            <v-col cols="12">
-              <div
-                ref="preview_text_field"
-                class="text"
-                data-cy="preview_text_field"
-              >
-                {{ component && component.description }}
-              </div>
-            </v-col>
-          </v-row>
-          <v-row v-if="component && component.isLinkEnabled">
-            <v-col
-              class="d-flex flex-row align-center text-decoration-underline linkWrapper"
+            </div>
+          </v-col>
+        </v-row>
+        <v-row style="margin: 0">
+          <v-col class="ma-2">
+            <a
+              :href="component && component.externalLink"
+              :target="'_blank'"
+              :class="{
+                disabledLink: component && component.moreHelpInfoLink === '',
+              }"
+              style="padding: 0"
             >
-              <a
-                :href="component && component.externalLink"
-                class="preview_info_link_field"
-                :target="'_blank'"
-                :class="{
-                  disabledLink: component && component.moreHelpInfoLink === '',
-                }"
-              >
-                <div class="mr-1 cursor" :lang="locale">
-                  {{ $t('trans.proactiveHelpPreviewDialog.learnMore') }}
-                  <v-icon icon="mdi:mdi-arrow-top-right-bold-box" /></div
-              ></a>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-card>
-    </v-dialog>
-  </v-row>
+              <div :lang="locale">
+                {{ $t('trans.proactiveHelpPreviewDialog.learnMore') }}
+                <v-icon icon="mdi:mdi-arrow-top-right-bold-box" /></div
+            ></a>
+          </v-col>
+        </v-row>
+      </div>
+    </template>
+  </v-dialog>
 </template>
-<style lang="scss" scoped>
-.cursor {
-  cursor: pointer !important;
-}
-.text {
-  font-style: normal !important;
-  font-weight: normal !important;
-  font-size: 18px !important;
-  font-family: BCSans !important;
-  letter-spacing: 0px !important;
-  color: #000000 !important;
-  opacity: 1 !important;
-}
-.linkWrapper {
-  text-align: left !important;
-  text-decoration: underline !important;
-  font-weight: normal !important;
-  font-style: normal !important;
-  font-family: BCSans !important;
-  font-size: 18px !important;
-  letter-spacing: 0px !important;
-  color: #1a5a96 !important;
-}
-.headerWrapper {
-  height: 40px !important;
-  background: #1a5a96 0% 0% no-repeat padding-box !important;
-  text-align: left !important;
-  font-weight: normal !important;
-  font-style: normal !important;
-  font-family: BCSans !important;
-  font-size: 25px !important;
-  letter-spacing: 0px !important;
-  color: #f2f2f2 !important;
-  text-transform: capitalize !important;
-}
-.disabledLink {
-  pointer-events: none;
+
+<style lang="scss">
+.content {
+  background: white;
 }
 </style>
