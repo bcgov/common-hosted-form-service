f1bd619aee1001b02b2385d7624e407122710abb
Vijaivir Dhaliwal <vijaivirdhaliwal@gmail.com>
2024-09-05 13:19:13 -0700

    removed comment

--- app/frontend/src/services/formService.js
+++ app/frontend/src/services/formService.js
@@ -544,18 +544,6 @@ export default {
     );
   },
 
-  // /**
-  //  * @function deleteEmailRecipients
-  //  * Delete the email recipients for the submission
-  //  * @param {string} submissionId The form submission identifier
-  //  * @returns {Promise} An axios response
-  //  */
-  // deleteEmailRecipients(submissionId) {
-  //   return appAxios().delete(
-  //     `${ApiRoutes.SUBMISSION}/${submissionId}/emailRecipients`
-  //   );
-  // },
-
   //
   // Email
   //
