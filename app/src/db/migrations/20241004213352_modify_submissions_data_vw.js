6caeb336dffd6a2e2d2e3a80b41b569db3cfd157
“bhuvan-aot” <bhuvanesh.p@aot-technologies.com>
2024-10-08 09:25:39 -0700

    changes to add submittedAt to the export API

--- app/src/db/migrations/20241004213352_modify_submissions_data_vw.js
+++ app/src/db/migrations/20241004213352_modify_submissions_data_vw.js
@@ -1,7 +1,6 @@
 exports.up = function (knex) {
   return Promise.resolve()
     .then(() => knex.schema.dropViewIfExists('submissions_data_vw'))
-    .then(() => knex.schema.dropViewIfExists('submissions_vw'))
     .then(() =>
       knex.schema.raw(`CREATE OR REPLACE VIEW public.submissions_data_vw
       AS SELECT s."confirmationId",
@@ -32,13 +31,13 @@ exports.up = function (knex) {
     s."formSubmissionStatusCode" AS status,
     s."formSubmissionAssignedToFullName" AS assignee,
     s."formSubmissionAssignedToEmail" AS "assigneeEmail",
-    fss."createdAt" AS submittedDate
+    fss."createdAt" AS "submittedAt"
    FROM submissions_vw s
      JOIN form_submission fs ON s."submissionId" = fs.id
      LEFT JOIN form_submission_user fsu ON s."submissionId" = fsu."formSubmissionId" AND fsu.permission::text = 'submission_create'::text
      LEFT JOIN "user" u ON fsu."userId" = u.id
      JOIN (
-    SELECT *, ROW_NUMBER() OVER (PARTITION BY "submissionId" ORDER BY "createdAt" DESC) AS rn
+    SELECT form_submission_status."submissionId", form_submission_status."createdAt", ROW_NUMBER() OVER (PARTITION BY form_submission_status."submissionId" ORDER BY form_submission_status."createdAt" DESC) AS rn
     FROM form_submission_status
 ) fss
 ON s."submissionId" = fss."submissionId" WHERE fss.rn = 1
@@ -49,7 +48,6 @@ ON s."submissionId" = fss."submissionId" WHERE fss.rn = 1
 exports.down = function (knex) {
   return Promise.resolve()
     .then(() => knex.schema.dropViewIfExists('submissions_data_vw'))
-    .then(() => knex.schema.dropViewIfExists('submissions_vw'))
     .then(() =>
       knex.schema.raw(
         `CREATE OR REPLACE VIEW public.submissions_data_vw
