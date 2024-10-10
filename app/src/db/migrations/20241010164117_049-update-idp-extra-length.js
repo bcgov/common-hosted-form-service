3c9c480ead56dab59f76b6228fa5ac7dbf7cec27
jasonchung1871 <101672465+jasonchung1871@users.noreply.github.com>
2024-10-10 10:06:15 -0700

    Update 20241010164117_049-update-idp-extra-length.js
    some code clean up

--- app/src/db/migrations/20241010164117_049-update-idp-extra-length.js
+++ app/src/db/migrations/20241010164117_049-update-idp-extra-length.js
@@ -1,15 +1,5 @@
-const BCEID_EXTRAS_OLD = {
+const BCEID_EXTRAS = {
   formAccessSettings: 'idim',
-  addTeamMemberSearch: {
-    text: {
-      minLength: 6,
-      message: 'trans.manageSubmissionUsers.searchInputLength',
-    },
-    email: {
-      exact: true,
-      message: 'trans.manageSubmissionUsers.exactBCEIDSearch',
-    },
-  },
   userSearch: {
     filters: [
       { name: 'filterIdpUserId', param: 'idpUserId', required: 0 },
@@ -25,8 +15,22 @@ const BCEID_EXTRAS_OLD = {
   },
 };
 
-const BCEID_EXTRAS = {
-  formAccessSettings: 'idim',
+const BCEID_EXTRAS_OLD = {
+  ...BCEID_EXTRAS,
+  addTeamMemberSearch: {
+    text: {
+      minLength: 6,
+      message: 'trans.manageSubmissionUsers.searchInputLength',
+    },
+    email: {
+      exact: true,
+      message: 'trans.manageSubmissionUsers.exactBCEIDSearch',
+    },
+  },
+};
+
+const BCEID_EXTRAS_NEW = {
+  ...BCEID_EXTRAS,
   addTeamMemberSearch: {
     text: {
       minLength: 4,
@@ -36,19 +40,6 @@ const BCEID_EXTRAS = {
       exact: true,
       message: 'trans.manageSubmissionUsers.exactBCEIDSearch',
     },
-  },
-  userSearch: {
-    filters: [
-      { name: 'filterIdpUserId', param: 'idpUserId', required: 0 },
-      { name: 'filterIdpCode', param: 'idpCode', required: 0 },
-      { name: 'filterUsername', param: 'username', required: 2, exact: true },
-      { name: 'filterFullName', param: 'fullName', required: 0 },
-      { name: 'filterFirstName', param: 'firstName', required: 0 },
-      { name: 'filterLastName', param: 'lastName', required: 0 },
-      { name: 'filterEmail', param: 'email', required: 2, exact: true },
-      { name: 'filterSearch', param: 'search', required: 0 },
-    ],
-    detail: 'Could not retrieve BCeID users. Invalid options provided.',
   },
 };
 
@@ -61,12 +52,12 @@ exports.up = function (knex) {
     knex.schema
       .then(() =>
         knex('identity_provider').where({ code: 'bceid-business' }).update({
-          extra: BCEID_EXTRAS,
+          extra: BCEID_EXTRAS_NEW,
         })
       )
       .then(() =>
         knex('identity_provider').where({ code: 'bceid-basic' }).update({
-          extra: BCEID_EXTRAS,
+          extra: BCEID_EXTRAS_NEW,
         })
       )
   );
