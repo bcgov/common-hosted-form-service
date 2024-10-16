95435f0fb4bab60dd1d84f0233a966afa82f4613
nimya-aot <nimya.john@aot-technologies.com>
2024-10-16 08:52:10 -0700

    #1527 Added print template functionalities

--- tests/functional/cypress/e2e/form-submission-export.cy.js
+++ tests/functional/cypress/e2e/form-submission-export.cy.js
@@ -90,16 +90,13 @@ describe("Form Designer", () => {
       cy.get("label").contains("Text Field").should("be.visible");
       cy.get("label").contains("Text Field").should("be.visible");
       cy.location("pathname").should("eq", `/${depEnv}/form/success`);
-
       cy.contains("h1", "Your form has been submitted successfully");
       cy.visit(`/${depEnv}/form/manage?f=${arrayValues[0]}`);
       cy.waitForLoad();
       cy.waitForLoad();
       cy.waitForLoad();
       cy.visit(`/${depEnv}/form/submit?f=${arrayValues[0]}`);
-      cy.waitForLoad();
-      cy.waitForLoad();
-      cy.waitForLoad();
+      cy.wait(2000);
       cy.get("button").contains("Submit").should("be.visible");
       cy.waitForLoad();
       cy.contains("Text Field").click();
@@ -109,13 +106,12 @@ describe("Form Designer", () => {
       cy.get('[data-test="continue-btn-continue"]').should("be.visible");
       cy.get('[data-test="continue-btn-continue"]').should("exist");
       cy.get('[data-test="continue-btn-continue"]').click({ force: true });
-      cy.waitForLoad();
-      cy.waitForLoad();
+      cy.location("pathname").should("eq", `/${depEnv}/form/success`);
+      cy.contains("h1", "Your form has been submitted successfully");
       cy.waitForLoad();
       //view submission
       cy.visit(`/${depEnv}/form/manage?f=${arrayValues[0]}`);
-      cy.waitForLoad();
-      cy.waitForLoad();
+      cy.wait(2000);
     });
   });
   it("Verify export submission", () => {
@@ -181,18 +177,75 @@ describe("Form Designer", () => {
       });
     //verify export button is enabled
     cy.get('.mb-5').should('be.enabled');
+  });
+
+  it("Verify print template functionality", () => {
+    cy.viewport(1000, 1100);
+    cy.waitForLoad();
     cy.get(".mdi-list-box-outline").click();
-    //print option
-    cy.get('a > .v-btn').click();
+    
+   
+    cy.get(':nth-child(1) > :nth-child(6) > a > .v-btn').click();
+     //print option
+    cy.get('.mdi-printer').click();
+    cy.get('.flex-container > .v-btn--elevated').should('be.enabled');
+    cy.get('.text-textLink').should('be.enabled');
+    cy.get('.text-textLink').click();
+    cy.get('.v-overlay__content > .v-card').should('not.exist');
+    cy.get('.mdi-printer').click();
+    cy.get('.v-label > span').contains('Expand text fields');
+    cy.get('.v-slide-group-item--active > .v-btn__content').contains('Browser Print');
+    cy.get('[tabindex="-1"] > .v-btn__content').contains('Template Print');
+    cy.get('input[type="checkbox"]').should('not.be.checked');
+    cy.get('input[type="checkbox"]').click();
+    cy.get('input[type="checkbox"]').should('be.checked');
+
+    cy.get('.v-window-item--active > .flex-container > .more-info-link').should('exist');
+
+    //print option for submission files
+    cy.get('[tabindex="-1"] > .v-btn__content').click();
+    let fileUploadInputField = cy.get('input[type=file]');
+    cy.get('input[type=file]').should('not.to.be.null');
+    fileUploadInputField.attachFile('add1.png');
+    cy.waitForLoad();
+    //cy.get('label').contains('Upload template file').click({multiple:true,force:true});
+    //cy.get('.v-messages__message').contains('The template must use one of the following extentions: .txt, .docx, .html, .odt, .pptx, .xlsx');
+    cy.get('#file-input-submit').should('not.be.enabled');
+    //
+    cy.waitForLoad();
+    //Upload print template
+    cy.get('[tabindex="-1"] > .v-btn__content').click();
+    cy.get('.v-slide-group__content > [tabindex="-1"]').click();
+    cy.waitForLoad();
+    cy.get('.mdi-close-circle').click();
+    cy.get('input[type=file]').attachFile('test.docx');
+    cy.waitForLoad();
+    //cy.get('.v-selection-control-group > .v-input--dirty > .v-input__control > .v-field > .v-field__append-inner > .mdi-menu-down').click();
+    cy.get('.v-selection-control-group > .v-text-field > .v-input__control > .v-field').click();
+    cy.contains('pdf').should('be.visible');
+    //cy.get('span').contains('docx').should('exist');
+    cy.contains('pdf').click();
+    cy.get('#file-input-submit').should('be.enabled');
+    cy.get('.v-card-actions > .flex-container > .text-textLink').should('be.enabled');
+    cy.get('.v-card-actions > .flex-container > .text-textLink').click();
+    cy.get('.mdi-printer').click();
+    cy.get('#file-input-submit').click();
+    // Verify cdogs template uplaod success message
+    cy.get('body').click(0,0);
+    cy.wait(2000);
+    cy.get('.v-alert__content').contains('Document generated successfully').should('be.visible');
+
     //Delete form after test run
-    /*
-        cy.visit(`/${depEnv}/form/manage?f=${arrayValues[0]}`);
-        cy.waitForLoad();
-        cy.waitForLoad();
-        cy.get('.mdi-delete').click();
-        cy.get('[data-test="continue-btn-continue"]').click();
-        cy.get('#logoutButton > .v-btn__content > span').click();
+    cy.get('.mdi-list-box-outline').click();
+    cy.location("search").then((search) => {
+      let arr = search.split("=");
+    cy.visit(`/${depEnv}/form/manage?f=${arr[1]}`);
+    cy.waitForLoad();
+    cy.get('.mdi-delete').click();
+    cy.get('[data-test="continue-btn-continue"]').click();
+    cy.get('#logoutButton > .v-btn__content > span').click();
+    })
         
-        */
+      
   });
 });
