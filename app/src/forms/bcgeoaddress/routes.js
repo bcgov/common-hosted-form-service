3a9ee87ac0526c4ce3a214a56ccfbf0c9f27d816
Jason Sherman <tools@usingtechnolo.gy>
2024-11-28 13:51:15 -0800

    remove cors verify in openshift

--- app/src/forms/bcgeoaddress/routes.js
+++ app/src/forms/bcgeoaddress/routes.js
@@ -1,12 +1,19 @@
+const cors = require('cors');
 const routes = require('express').Router();
+const { Development } = require('../common/constants');
 
 const controller = require('./controller');
 
-routes.get('/address', async (req, res, next) => {
+// need to allow cors for OPTIONS call (localhost only)
+// formio component will call OPTIONS pre-flight
+routes.options('/address', cors({ origin: Development.LOCALHOST_ORIGIN }));
+routes.options('/advance/address', cors({ origin: Development.LOCALHOST_ORIGIN }));
+
+routes.get('/address', cors({ origin: Development.LOCALHOST_ORIGIN }), async (req, res, next) => {
   await controller.searchBCGeoAddress(req, res, next);
 });
 
-routes.get('/advance/address', async (req, res, next) => {
+routes.get('/advance/address', cors({ origin: Development.LOCALHOST_ORIGIN }), async (req, res, next) => {
   await controller.advanceSearchBCGeoAddress(req, res, next);
 });
 
