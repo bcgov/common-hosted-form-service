262dc966c3c782ee2b6ab0bed4f624ee5b0006f4
nimya-aot <nimya.john@aot-technologies.com>
2024-12-18 13:39:17 -0800

    #1621 Test scripts for Map component display tab

---
+++ tests/functional/cypress/e2e/form-design-map.cy.js
@@ -0,0 +1,120 @@
+import 'cypress-keycloak-commands';
+import 'cypress-drag-drop';
+import { formsettings } from '../support/login.js';
+//import { should } from 'chai';
+
+const depEnv = Cypress.env('depEnv');
+
+
+Cypress.Commands.add('waitForLoad', () => {
+  const loaderTimeout = 60000;
+
+  cy.get('.nprogress-busy', { timeout: loaderTimeout }).should('not.exist');
+});
+
+
+
+describe('Form Designer', () => {
+
+  beforeEach(()=>{
+    
+    
+    
+    cy.on('uncaught:exception', (err, runnable) => {
+      // Form.io throws an uncaught exception for missing projectid
+      // Cypress catches it as undefined: undefined so we can't get the text
+      console.log(err);
+      return false;
+    });
+  });
+  it('Visits the form settings page', () => {
+    
+    
+    cy.viewport(1000, 1100);
+    cy.waitForLoad();
+    
+    formsettings();
+    
+
+  });  
+// Publish a simple form with Simplebc Address component
+ it('Checks simplebcaddress and form submission', () => {
+    cy.viewport(1000, 1100);
+    cy.waitForLoad();
+    
+    cy.get('button').contains('BC Government').click();
+    cy.get('div.formio-builder-form').then($el => {
+      const coords = $el[0].getBoundingClientRect();
+      cy.get('[data-type="map"]')
+      .trigger('mousedown', { which: 1}, { force: true })
+      .trigger('mousemove', coords.x, -550, { force: true })
+        //.trigger('mousemove', coords.y, +100, { force: true })
+      .trigger('mouseup', { force: true });
+      cy.waitForLoad();
+      cy.get('input[name="data[label]"]').type('s'); 
+      cy.get('textarea[name="data[description]"]').should("have.attr","placeholder","This will appear below the map");
+      cy.get('textarea[name="data[description]"]').type('Map location above');
+      cy.wait(2000);
+      cy.contains('Map location above').should('exist');
+      cy.get('textarea[placeholder="Add a tooltip beside the label"]').type('Add your desired location');
+      cy.wait(2000);
+      cy.get('i[ref="tooltip"]').should('exist');
+      
+      cy.waitForLoad();
+      cy.get('button').contains('Save').click();
+
+
+      });
+    
+  // Form saving
+    let savedButton = cy.get('[data-cy=saveButton]');
+    expect(savedButton).to.not.be.null;
+    savedButton.trigger('click');
+    cy.wait(2000);
+
+
+  
+    
+  // Filter the newly created form
+    cy.location('search').then(search => {
+      //let pathName = fullUrl.pathname
+      let arr = search.split('=');
+      let arrayValues = arr[1].split('&');
+      cy.log(arrayValues[0]);
+      cy.visit(`/${depEnv}/form/manage?f=${arrayValues[0]}`);
+      cy.waitForLoad();
+      
+   
+    //Publish the form
+    cy.get('.v-label > span').click();
+
+    cy.get('span').contains('Publish Version 1');
+
+    cy.contains('Continue').should('be.visible');
+    cy.contains('Continue').trigger('click');
+    //Share link verification
+    let shareFormButton = cy.get('[data-cy=shareFormButton]');
+    expect(shareFormButton).to.not.be.null;
+    shareFormButton.trigger('click').then(()=>{
+      //let shareFormLinkButton = cy.get('[data-cy=shareFormLinkButtonss]');
+      let shareFormLinkButton=cy.get('.mx-2');
+      expect(shareFormLinkButton).to.not.be.null;
+      shareFormLinkButton.trigger('click');
+      cy.get('.mx-2 > .v-btn').click();
+    })
+      cy.visit(`/${depEnv}`);
+      cy.get('[data-cy="userFormsLinks"]').click();
+      cy.visit(`/${depEnv}/form/manage?f=${arrayValues[0]}`);
+      cy.waitForLoad();
+      //Delete form after test run
+      //cy.get('.mdi-delete').click();
+      cy.get(':nth-child(5) > .v-btn > .v-btn__content > .mdi-delete').click();
+      cy.get('[data-test="continue-btn-continue"]').click();
+      cy.get('#logoutButton > .v-btn__content > span').click();
+   
+
+    });
+
+  });
+    
+});
