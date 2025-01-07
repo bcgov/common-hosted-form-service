5d0ba59541e07a7a7b568665e320485e99515a75
usingtechnology <39388115+usingtechnology@users.noreply.github.com>
2025-01-07 08:54:24 -0800

    FORMS-1563: auto-approve new ext api when same ministry/url already a… (#1543)
    * FORMS-1563: auto-approve new ext api when same ministry/url already approved.
    
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>
    
    * limit usage of raw sql
    
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>
    
    * add a sql injection block
    
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>
    
    * Show sendApiKey fo admins before approving an api
    
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>
    
    ---------
    
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- app/src/forms/admin/service.js
+++ app/src/forms/admin/service.js
@@ -1,3 +1,4 @@
+const { ExternalAPIStatuses } = require('../common/constants');
 const { Form, FormVersion, User, UserFormAccess, FormComponentsProactiveHelp, AdminExternalAPI, ExternalAPI, ExternalAPIStatusCode } = require('../common/models');
 const { queryUtils } = require('../common/utils');
 const { v4: uuidv4 } = require('uuid');
@@ -148,13 +149,39 @@ const service = {
       upd['userTokenHeader'] = null;
       upd['userTokenBearer'] = false;
     }
-
-    await ExternalAPI.query().patchAndFetchById(id, upd);
-
-    return ExternalAPI.query().findById(id);
+    let trx;
+    try {
+      trx = await ExternalAPI.startTransaction();
+      await ExternalAPI.query(trx).patchAndFetchById(id, upd);
+      await service._approveMany(id, data, trx);
+      await trx.commit();
+      return ExternalAPI.query().findById(id);
+    } catch (err) {
+      if (trx) await trx.rollback();
+      throw err;
+    }
   },
   getExternalAPIStatusCodes: async () => {
     return ExternalAPIStatusCode.query();
+  },
+  _approveMany: async (id, data, trx) => {
+    // if we are setting to approved, approve all similar endpoints.
+    // same ministry, same base url...
+    if (data.code === ExternalAPIStatuses.APPROVED) {
+      const adminExternalAPI = await AdminExternalAPI.query(trx).findById(id);
+      const regex = /^[A-Z]{2,4}$/; // Ministry constants are in the Frontend, they are 2,3,or 4 Capital chars
+      if (regex.test(adminExternalAPI.ministry)) {
+        // this will protect from sql injection.
+        // this should be removed when form API and db are updated to restrict form Ministry values.
+        const delimiter = '?';
+        const baseUrl = data.endpointUrl.split(delimiter)[0];
+        await ExternalAPI.query(trx)
+          .patch({ code: ExternalAPIStatuses.APPROVED })
+          .whereRaw(`"formId" in (select id from form where ministry = '${adminExternalAPI.ministry}')`)
+          .andWhere('endpointUrl', 'ilike', `${baseUrl}%`)
+          .andWhere('code', ExternalAPIStatuses.SUBMITTED);
+      }
+    }
   },
 
   /**
