7da3d9fdd068cb03c05f3b9ab2d22db765e1f730
Walter Moar <Walter.Moar@gov.bc.ca>
2025-01-28 15:35:19 -0800

    fix: FORMS-1776 remove role and permission routes (#1587)
    The roles and permissions both have routes for create (POST) and update (PUT). We do not use these routes, and should not use these routes - changes to the roles and permissions should be done through Knex migrations to ensure consistency across environments.

--- app/src/forms/role/controller.js
+++ app/src/forms/role/controller.js
@@ -9,25 +9,10 @@ module.exports = {
       next(error);
     }
   },
-  create: async (req, res, next) => {
-    try {
-      const response = await service.create(req.body, req.currentUser);
-      res.status(201).json(response);
-    } catch (error) {
-      next(error);
-    }
-  },
+
   read: async (req, res, next) => {
     try {
       const response = await service.read(req.params.code);
-      res.status(200).json(response);
-    } catch (error) {
-      next(error);
-    }
-  },
-  update: async (req, res, next) => {
-    try {
-      const response = await service.update(req.params.code, req.body, req.currentUser);
       res.status(200).json(response);
     } catch (error) {
       next(error);
