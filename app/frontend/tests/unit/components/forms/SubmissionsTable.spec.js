2234c595272942c02ef04570cd78ac3eb35963bc
revanth-banala <revanth.banala@gov.bc.ca>
2025-02-13 14:14:08 -0800

    Update SubmissionsTable.spec.js

--- app/frontend/tests/unit/components/forms/SubmissionsTable.spec.js
+++ app/frontend/tests/unit/components/forms/SubmissionsTable.spec.js
@@ -788,7 +788,7 @@ describe('SubmissionsTable.vue', () => {
       itemsPerPage: 2,
       sortBy: [{ key: 'something', order: 'desc' }],
     });
-    it('should not refresh submissions if firstDataLoad is true', () => {
+  it('should not refresh submissions if firstDataLoad is true', () => {
     vi.clearAllMocks();  // Reset all mocks before the test
     // should not refresh submissions
     wrapper.vm.firstDataLoad = true;
