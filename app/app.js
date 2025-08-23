f31d376b96b9782f1826a0d20df9b31bd8a64b2b
Jason Sherman <tools@usingtechnolo.gy>
2025-08-23 10:16:49 -0700

    routes for static files in PR
    Signed-off-by: Jason Sherman <tools@usingtechnolo.gy>

--- app/app.js
+++ app/app.js
@@ -113,6 +113,9 @@ app.use('/favicon.ico', (_req, res) => {
 });
 app.use(staticFilesPath, express.static(path.join(__dirname, 'frontend/dist')));
 
+// Host embed assets (CSS and JS files) separately to avoid Vue router conflicts
+app.use(`${config.get('server.basePath')}/embed`, express.static(path.join(__dirname, 'frontend/dist')));
+
 // Handle 500
 // eslint-disable-next-line no-unused-vars
 app.use((err, _req, res, _next) => {
