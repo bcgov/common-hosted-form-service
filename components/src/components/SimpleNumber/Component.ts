222227491711decd8407394b57aa4247849acce3
RyanBirtch-aot <ryan.birtch@aot-technologies.com>
2025-09-24 18:30:33 +0000

    Sonarqube fixes

--- components/src/components/SimpleNumber/Component.ts
+++ components/src/components/SimpleNumber/Component.ts
@@ -11,57 +11,65 @@ const DISPLAY = 'Number';
 
 // Apply the mixin to create the final component class
 export default class Component extends (ParentComponent as any) {
-    static schema(...extend: any[]) {
-        const baseSchema = ParentComponent.schema({
-            type: ID,
-            label: DISPLAY,
-            key: ID,
-            validate: {
-                min: '',
-                max: '',
-                step: 'any',
-                integer: ''
-            }
-        }, ...extend);
+  static schema(...extend: any[]) {
+    const baseSchema = ParentComponent.schema(
+      {
+        type: ID,
+        label: DISPLAY,
+        key: ID,
+        validate: {
+          min: '',
+          max: '',
+          step: 'any',
+          integer: '',
+        },
+      },
+      ...extend
+    );
 
-        return addRoundingToSchema(baseSchema);
-    }
+    return addRoundingToSchema(baseSchema);
+  }
 
-    public static editForm = editForm;
+  public static editForm = editForm;
 
-    static get builderInfo() {
-        return {
-            title: DISPLAY,
-            group: 'simple',
-            icon: 'hashtag',
-            weight: 10,
-            documentation: Constants.DEFAULT_HELP_LINK,
-            schema: Component.schema(),
-        };
-    }
+  static get builderInfo() {
+    return {
+      title: DISPLAY,
+      group: 'simple',
+      icon: 'hashtag',
+      weight: 10,
+      documentation: Constants.DEFAULT_HELP_LINK,
+      schema: Component.schema(),
+    };
+  }
 
-    formatValue(value: any) {
-        if (this.component.requireDecimal && value !== null && value !== undefined && value !== '') {
-            const decimalPlaces = this.component.decimalLimit || 2;
-            const multiplier = Math.pow(10, decimalPlaces);
-            let numValue = parseFloat(value);
-            if (!isNaN(numValue)) {
-                switch (this.component.rounding.method) {
-                    case 'floor':
-                        numValue = Math.floor(numValue * multiplier) / multiplier;
-                        break;
-                    case 'ceil':
-                        numValue = Math.ceil(numValue * multiplier) / multiplier;
-                        break;
-                    case 'round':
-                    default:
-                        numValue = Math.round(numValue * multiplier) / multiplier;
-                        break;
-                }
-                return numValue.toFixed(decimalPlaces);
-            }
+  formatValue(value: any) {
+    if (
+      this.component.requireDecimal &&
+      value !== null &&
+      value !== undefined &&
+      value !== ''
+    ) {
+      const decimalPlaces = this.component.decimalLimit || 2;
+      const multiplier = Math.pow(10, decimalPlaces);
+      let numValue = Number.parseFloat(value);
+      if (!Number.isNaN(numValue)) {
+        switch (this.component.rounding.method) {
+          case 'floor':
+            numValue = Math.floor(numValue * multiplier) / multiplier;
+            break;
+          case 'ceil':
+            numValue = Math.ceil(numValue * multiplier) / multiplier;
+            break;
+          case 'round':
+          default:
+            numValue = Math.round(numValue * multiplier) / multiplier;
+            break;
         }
-
-        return super.formatValue(value);
+        return numValue.toFixed(decimalPlaces);
+      }
     }
+
+    return super.formatValue(value);
+  }
 }
