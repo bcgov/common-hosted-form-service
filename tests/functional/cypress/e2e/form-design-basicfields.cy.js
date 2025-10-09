7891510a9ddc0cdd44a8652d4710f3dfde3814b0
Nimya John <78105113+nimya-aot@users.noreply.github.com>
2025-10-09 10:07:04 -0700

    test: FORMS- 2711Cypress test scripts for adding new form design version from previous base version  (#1764)
    * Added Submitter revision form check
    
    * Added submission recall ability
    
    * Added recall functionality
    
    * Added public form check
    
    * Added validations for BC SC card login type on form settings
    
    * Added scripts to reuse formid in multiple tests
    
    * updated public form to access formid
    
    * Updated other tests to reuse same formid
    
    * Updated
    
    * Added updated scripts for Flating button changes on designer page
    
    * Updated
    
    * Added validations for updating design versions
    
    * Added cypress tests for permanent delete feature settings
    
    * Update form-apikey-cdogs.cy.js
    
    * Updated with latest changes
    
    * Updated scripts

--- tests/functional/cypress/e2e/form-design-basicfields.cy.js
+++ tests/functional/cypress/e2e/form-design-basicfields.cy.js
@@ -129,6 +129,7 @@ describe('Form Designer', () => {
         .trigger('mouseup', { force: true });
         cy.get('.btn-success').click();
     });
+    cy.wait(1000);
     //Phone Number
     cy.get('div.formio-builder-form').then($el => {
         const coords = $el[0].getBoundingClientRect();
@@ -137,10 +138,6 @@ describe('Form Designer', () => {
         .trigger('mousedown', { which: 1}, { force: true })
         .trigger('mousemove', coords.x, -50, { force: true })
         .trigger('mouseup', { force: true });
-        //cy.get('p').contains('Multi-line Text Component');
-        cy.get('input[name="data[label]"]').clear();
-        cy.get('input[name="data[label]"]').clear();
-        cy.get('input[name="data[label]"]').type('Phone Number');
         cy.get('.btn-success').click();
     });
     //Email
