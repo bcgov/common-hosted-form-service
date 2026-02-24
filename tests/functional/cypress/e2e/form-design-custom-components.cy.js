9a21699cf29715921ff4e84976dabd0b7aedb9ef
nimya-aot <nimya.john@aot-technologies.com>
2026-02-24 15:22:22 -0800

    Added fix to axe-report

--- tests/functional/cypress/e2e/form-design-custom-components.cy.js
+++ tests/functional/cypress/e2e/form-design-custom-components.cy.js
@@ -72,8 +72,8 @@ describe("Form Designer", () => {
     cy.get('input[type="radio"][value="default"]').should('be.checked');
     cy.get('input[type="radio"][aria-label="Direct Print"]').should('not.be.checked');
     cy.get('input[type="radio"][aria-label="Direct Print"]').click();
-    cy.wait(500);
-    cy.get('.pl-12 > .v-text-field > .v-input__control > .v-field > .v-field__append-inner').click();
+    cy.wait(1000);
+    cy.get('.pl-12 > .v-select > .v-input__control > .v-field > .v-field__append-inner > .mdi-menu-down').click();
     cy.wait(1000);
     cy.get('div.v-list-item-title').contains('test.docx').click();
     cy.get('input[type="radio"][value="formName"]').should('be.checked');
