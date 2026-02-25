33fe9c91575d9ad70c92f8f05dc7a4c4d196c7a9
nimya-aot <nimya.john@aot-technologies.com>
2026-02-25 08:31:23 -0800

    Fixed axe issues

--- tests/functional/cypress/support/commands.js
+++ tests/functional/cypress/support/commands.js
@@ -1,37 +1,28 @@
 import 'cypress-file-upload';
 import 'cypress-axe';
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This is will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('checkA11yPage', () => {
   cy.get('body', { timeout: 10000 }).should('be.visible');
   cy.injectAxe();
 
-  cy.checkA11y(null, {
-    includedImpacts: ['critical', 'serious', 'moderate', 'minor'],
-    skipFailures: true,
-  }, cy.cypressAxeReporterCallBack, true);
+  cy.checkA11y(
+    null,
+    {
+      includedImpacts: ['critical', 'serious', 'moderate', 'minor'],
+      skipFailures: true,
+    },
+    (violations) => {
+      if (!violations) {
+        console.warn('No axe violations found');
+        return;
+      }
+
+      // Call task to write axe report
+      cy.task('writeAxeReport', {
+        results: { violations }, // ✅ wrap in results object
+        filePath: 'cypress/axe-reports/report.json',
+      });
+    },
+    true
+  );
 });
