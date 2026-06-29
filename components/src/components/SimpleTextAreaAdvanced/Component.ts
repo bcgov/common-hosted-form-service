1bb52b5f7d0dd12f9d82cded2801e5a4f8cad962
usingtechnology <39388115+usingtechnology@users.noreply.github.com>
2026-06-29 11:39:14 -0700

    ckeditor errors fix?

--- components/src/components/SimpleTextAreaAdvanced/Component.ts
+++ components/src/components/SimpleTextAreaAdvanced/Component.ts
@@ -9,34 +9,55 @@ const ID = 'simpletextareaadvanced';
 const DISPLAY = 'Text Area';
 
 export default class Component extends (ParentComponent as any) {
-    static schema(...extend) {
-        return ParentComponent.schema({
-            type: ID,
-            label: DISPLAY,
-            key: ID,
-            rows: 3,
-            wysiwyg: false,
-            editor: '',
-            spellcheck: true,
-            fixedSize: true,
-            inputFormat: 'plain',
-            validate: {
-                minWords: '',
-                maxWords: ''
-            }
-        }, ...extend);
-    }
+  static schema(...extend) {
+    return ParentComponent.schema(
+      {
+        type: ID,
+        label: DISPLAY,
+        key: ID,
+        rows: 3,
+        wysiwyg: false,
+        editor: '',
+        spellcheck: true,
+        fixedSize: true,
+        inputFormat: 'plain',
+        validate: {
+          minWords: '',
+          maxWords: '',
+        },
+      },
+      ...extend,
+    );
+  }
 
-    public static editForm = editForm;
+  public static editForm = editForm;
 
-    static get builderInfo() {
-        return {
-            title: DISPLAY,
-            group: 'advanced',
-            icon: 'font',
-            weight: 730,
-            documentation: Constants.DEFAULT_HELP_LINK,
-            schema: Component.schema()
-        };
-    }
+  static get builderInfo() {
+    return {
+      title: DISPLAY,
+      group: 'advanced',
+      icon: 'font',
+      weight: 730,
+      documentation: Constants.DEFAULT_HELP_LINK,
+      schema: Component.schema(),
+    };
+  }
+
+    detach() {
+        // CKEditor 5 destroy() blows up when the editor instance is wrapped in a
+        // Vue 3 reactive Proxy (_events is non-configurable). Unwrap via __v_raw
+        // and swallow any remaining proxy errors since the DOM is going away anyway.
+        this.editors.forEach((editor) => {
+      if (editor.destroy) {
+        try {
+          const rawEditor = (editor as any).__v_raw ?? editor;
+          rawEditor.destroy();
+        } catch (_err) {
+          // Suppressed: component is being unmounted and DOM is removed
+        }
+      }
+    });
+    this.editors = [];
+    super.detach();
+  }
 }
