55a21a1317c731deeaa3953ed8daa2cd9faa3433
nimya-aot <nimya.john@aot-technologies.com>
2026-08-12 09:56:16 -0700

    Added Offline form feature

--- tests/functional/cypress/e2e/form-design-custom-components.cy.js
+++ tests/functional/cypress/e2e/form-design-custom-components.cy.js
@@ -35,13 +35,21 @@ describe("Form Designer", () => {
     cy.get("div.formio-builder-form").then(($el) => {
       const coords = $el[0].getBoundingClientRect();
       cy.get('[data-type="idirusers"]')
-
       .trigger('mousedown', { which: 1}, { force: true })
       .trigger('mousemove', coords.x, -600, { force: true })
       .trigger('mouseup', { force: true });
       cy.get('.btn-success').click();
       cy.wait(1000);
-  });
+    });
+    cy.get('button').contains('Basic Fields').click();
+    cy.get('div.formio-builder-form').then($el => {
+      const coords = $el[0].getBoundingClientRect();
+      cy.get('span.btn').contains('Text Field')
+      .trigger('mousedown', { which: 1}, { force: true })
+      .trigger('mousemove', coords.x, -110, { force: true })
+      .trigger('mouseup', { force: true });
+      cy.get('.btn-success').click();
+    });
   });
   it('Verify Direct Print Configuration', () => {
     let savedButton = cy.get('[data-cy=saveButton]');
@@ -63,7 +71,11 @@ describe("Form Designer", () => {
     cy.contains('Continue').trigger('click');
     // Update Form settings for offline forms
     cy.get('[lang="en"] > .v-btn > .v-btn__content > .mdi-pencil').click();
-    cy.get('[data-test="enableOfflineSubmissionCheckbox)').click();
+    cy.wait(1000);
+    let enableOfflineSubmissionCheckbox = cy.get('[data-test="enableOfflineSubmissionCheckbox"] input[type="checkbox"]');
+    enableOfflineSubmissionCheckbox.check({ force: true });
+    //Update form settings for offline forms
+    cy.get('[data-test="canEditForm"]').click();
     //codogs file upload
     cy.get(':nth-child(3) > .v-expansion-panel > .v-expansion-panel-title > .v-expansion-panel-title__overlay').click();
     let fileUploadInputField = cy.get('input[type=file]');
@@ -92,6 +104,60 @@ describe("Form Designer", () => {
     cy.get('button[title="Save Configuration"]').click();
     cy.get('.v-alert__content').contains('Print configuration saved successfully').should('be.visible');
     cy.wait(500);
+    //Offline simulator check
+    cy.visit(`/${depEnv}/form/submit?f=${arrayValues[0]}&simulateOffline=1`);
+    cy.wait
+    cy.get('[data-test="onlineBadge"]').should('be.visible');
+    cy.get('[data-test="onlineBadge"]').should('be.visible').and('contain', 'ONLINE');
+    cy.get('[data-test="simulateOfflineToggle"]').click();
+    cy.get('[data-test="simulatingOfflineBadge"]').should('be.visible').and('contain', 'OFFLINE');
+    cy.wait(1000);
+    cy.contains('Text Field').click();
+    cy.contains('Text Field').type('Smith');
+    cy.get('button').contains('Submit').click();
+    cy.get('input[placeholder="A short label to help you find this submission later"]').type('My offline Submission');
+    cy.get('[data-test="queue-confirm-submit"]').click();
+    cy.get('.v-alert__content').contains('Submission saved. It will be sent when you are back online.').should('be.visible');
+    cy.contains('h1', 'Submission Saved').should('be.visible');
+    cy.contains('span', '1').should('be.visible');
+    cy.contains('button', 'Return to Form').should('be.visible');
+    cy.get('.v-badge__badge > span').should('contain', '1');
+    cy.get('.v-badge__badge').click();
+    cy.contains('.pending-row-title', 'My offline Submission').should('be.visible');
+    //Delete an offline submission from submission queue
+    cy.get('button[title="Discard"]').click();
+    cy.get('[data-test="pending-discard-confirm"]').should('be.visible');
+    cy.get('[data-test="pending-discard-cancel"]').should('be.visible');
+    cy.get('[data-test="pending-discard-confirm"]').click();
+    //Confirms it is deleted from the submission queue
+    cy.contains('.pending-row-title', 'My offline Submission').should('not.exist');
+    cy.contains('span', 'Close').should('be.visible').click();
+    cy.contains('span', '1').should('not.exist');
+    cy.contains('button', 'Return to Form').should('be.visible').click();
+    //Checks whether it goes back to original form
+    cy.get('input[placeholder="Search by email"]').should('be.visible');
+    cy.contains('Text Field').should('be.visible');
+    cy.contains('Text Field').type('Smith');
+    cy.get('button').contains('Submit').click();
+    //Submit again with offline toggle
+    cy.get('input[placeholder="A short label to help you find this submission later"]').type('My offline Submission');
+    cy.get('[data-test="queue-confirm-submit"]').click();
+    cy.get('.v-alert__content').contains('Submission saved. It will be sent when you are back online.').should('be.visible');
+    cy.contains('h1', 'Submission Saved').should('be.visible');
+    cy.contains('span', '1').should('be.visible');
+    cy.contains('button', 'Return to Form').should('be.visible');
+    cy.get('.v-badge__badge > span').should('contain', '1');
+    //Enable online toggle and check if submission is sent
+    cy.get('.mdi-cloud-check-outline').click({ force: true });
+    cy.wait(2000);
+    cy.contains('button', 'Return to Form').click();
+    //Check on submission table if submission is sent and received
+    cy.get('button[title="View My Drafts/Submissions"]').click();
+    cy.get('[data-test="saveddelete-btn-cancel"] > .v-btn__content > span').click();
+    cy.wait(1000);
+    cy.get('.v-data-table__tr > :nth-child(4)').contains('SUBMITTED').should('be.visible');
+    //Validate only one submission is present in the submission table
+    cy.get('.v-data-table__tr').should('have.length', 1);
     //Submit the form
     cy.visit(`/${depEnv}/form/submit?f=${arrayValues[0]}`);
     cy.wait(1000);
@@ -114,14 +180,16 @@ describe("Form Designer", () => {
     cy.get('.col-md-12 > .btn').click();
     cy.get(':nth-child(4) > .btn').click();
     cy.get('.selected-user-info > :nth-child(1)').contains(' CHEFS Testing (CHEFSTST) CITZ:EX').should('be.visible');
+    //exit offline toggle
+    cy.get('[data-test="simulateOfflineToggle"]').click();
+    cy.wait(500);
+    //Validate online badge is visible back
+    cy.get('[data-test="onlineBadge"]').should('be.visible').and('contain', 'ONLINE');
+    cy.wait(1000);
     cy.get('button').contains('Submit').click();
     cy.get('[data-test="continue-btn-continue"]').click({ force: true });
     cy.wait(1000);
     cy.get('.selected-user-view > :nth-child(1)').contains('CHEFS Testing (CHEFSTST) CITZ:EX').should('be.visible');
-    cy.get('[data-test="onlineBadge"]').should('be.visible');
-    //cy.get('[data-test="offlineBadge"]').should('be.visible');
-    cy.get('.v-alert__content').contains('Submission created successfully').should('be.visible');
-    
     //Direct Print Verification
     cy.wait(2000);
     cy.get('.mdi-printer').should('be.visible').click();
